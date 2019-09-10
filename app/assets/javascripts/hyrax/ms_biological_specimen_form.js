64d2694e432aa018496c429c41a84a8df2b83257
Simon Choy <simon.choy@duke.edu>
2019-09-10 11:32:06 -0400

    show/hide embedded and current institution div

--- app/assets/javascripts/hyrax/ms_biological_specimen_form.js
+++ app/assets/javascripts/hyrax/ms_biological_specimen_form.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function() {
   if ($('form[id*="biological_specimen"]').length) { // if BSO form page
 
-    setupEmbeddedWorkForm('new_taxonomy');
-    setupEmbeddedWorkForm('new_institution');
+    setupEmbeddedWorkForm('taxonomy', 'new');
+    setupEmbeddedWorkForm('institution', 'new');
 
 	  $('.tooltip-icon').tooltip({ 
 	    title: function(){
--- app/assets/javascripts/hyrax/ms_helpers.js
+++ app/assets/javascripts/hyrax/ms_helpers.js
@@ -13,19 +13,23 @@ function hide_fields(field_array, clear = true) {
 }
 
 // setup embedded work form, when to load the form, submit and close handling
-function setupEmbeddedWorkForm(work_name) {
-	var this_btn = "#btn_" + work_name;
-	var this_div = "#embedded_div_" + work_name;
-	var this_form = "form#" + work_name;
-	var this_path = "/submissions/" + work_name;
+function setupEmbeddedWorkForm(work_name, action) {
+	var this_btn = "#btn_" + action + '_' + work_name;
+	var this_div = "#embedded_div_" + action + '_' + work_name;
+	var this_form = "form#" + action + '_' + work_name;
+	var this_path = "/submissions/" + action + '_' + work_name;
+	var detail_div = '#' + work_name + '_details';
 
   $(document).on("click", this_btn, function() {		
-  	// load the new work form	
-		$(this_div).show();
-		$(this_div).addClass('ui-loading');
+  	// the new work form button has been clicked	
+		$(this_div).addClass('ui-loading').show();
+		// hide detail section if any
+		$(detail_div).hide();
+
 		$.ajax({
 		  url: this_path,
 		  success: function(result) {
+		  	// new work form has been retrieved in the request
 		  	try {
 			    $(this_div).html(result);
 		  	} catch (e) {
@@ -39,35 +43,45 @@ function setupEmbeddedWorkForm(work_name) {
 	});
 
   $(this_div).on("submit", this_form, function() {
+  	// the new work form's save button has been clicked
 		// replace with ajax form post to trigger other actions
 		$.post($(this_form).attr('action'),
 	  $(this_form).serialize(), function(data, status){
+	    // got the data back after the new work has been created
 	    console.log(data.message, data);
 			var relationship_element = $(this_div).data("relationship-control");
 	  	var relationship_input = $(relationship_element).find('input[name*="[find_parent_work]"]');
 			$(relationship_input).val(data.work.id);
-			var new_data = {
-				id: data.work.id, 
-				text: data.work.title,
-				institution_code: data.work.institution_code,
-        description: data.work.description,
-        address: data.work.address, 
-        city: data.work.city, 
-        state_province: data.work.state_province,
-        country: data.work.country
-			};
+			if (work_name == 'institution') {
+				var new_data = {
+					id: data.work.id, 
+					text: data.work.title,
+					institution_code: data.work.institution_code,
+	        description: data.work.description,
+	        address: data.work.address, 
+	        city: data.work.city, 
+	        state_province: data.work.state_province,
+	        country: data.work.country
+				};
+			} else {
+				var new_data = {
+					id: data.work.id, 
+					text: data.work.title
+				};
+			}
 	    $(relationship_element).data('new-work-created', new_data);
 			var relationship_add_btn = $(this_div).data("add-button");
 			$(relationship_add_btn).trigger("click");
 
 			// perform any work specifc function
-			switch(work_name) {
+			switch(action + '_' + work_name) {
 				case 'new_institution':
 					$('#biological_specimen_institution_code').val(data.work.institution_code);
 					break;
 				default:
 			}
 			$(this_div).hide();
+			$(detail_div).show();
 	  });
 	  
 		$(this_div).addClass('ui-loading');
@@ -78,5 +92,6 @@ function setupEmbeddedWorkForm(work_name) {
   $(this_div).on("click", "button.cancel", function() {
 		$(this_div).hide();
    	$(this_div).html('');
+		$(detail_div).show();
   });
 }

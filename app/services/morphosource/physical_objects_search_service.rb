696f567a37f6c46acf41993a8204cedbeec94682
Ryan Baumann <ryan.baumann@gmail.com>
2019-10-29 11:17:07 -0400

    MR-616: add genus and species taxonomy search fields

--- app/services/morphosource/physical_objects_search_service.rb
+++ app/services/morphosource/physical_objects_search_service.rb
@@ -1,7 +1,7 @@
 module Morphosource
   class PhysicalObjectsSearchService
 
-    attr_reader :model, :params
+    attr_reader :taxonomy_genus, :taxonomy_species, :model, :params
 
     SORTABLE_TITLE_FIELD = Solrizer.solr_name('title', :stored_sortable)
 
@@ -11,12 +11,15 @@ module Morphosource
 
     def initialize(model, params={})
       @model = model
+      @taxonomy_genus = params.delete('taxonomy_genus')
+      @taxonomy_species = params.delete('taxonomy_species')
       @params = params
     end
 
     def call
       qry = assemble_query
       hits = search_solr(qry)
+      hits = filter_on_taxonomy(hits) if (taxonomy_genus.present? || taxonomy_species.present?)
       hits.map { |hit| SolrDocument.new(hit) }
     end
 
@@ -25,6 +28,24 @@ module Morphosource
     def assemble_query
       query_clauses = [ model_clause ] + param_clauses
       query_clauses.join(' AND ')
+    end
+
+    def filter_on_taxonomy(hits)
+      tax_doc = taxonomy_doc
+      taxonomy_member_ids = tax_doc[Solrizer.solr_name('member_ids', :symbol)]
+      if taxonomy_member_ids.present?
+        hits.select { |hit| taxonomy_member_ids.include?(hit.id) }
+      else
+        []
+      end
+    end
+
+    def taxonomy_doc
+      taxonomy_query_clauses = [ "#{Solrizer.solr_name('has_model', :symbol)}:Taxonomy" ]
+      taxonomy_query_clauses << "#{Solrizer.solr_name('taxonomy_genus', :stored_searchable)}:(#{taxonomy_genus})" if taxonomy_genus.present?
+      taxonomy_query_clauses << "#{Solrizer.solr_name('taxonomy_species', :stored_searchable)}:(#{taxonomy_species})" if taxonomy_species.present?
+      taxonomy_query = taxonomy_query_clauses.join(' AND ')
+      SolrDocument.new(ActiveFedora::SolrService.query(taxonomy_query, rows: 999999).first)
     end
 
     def model_name

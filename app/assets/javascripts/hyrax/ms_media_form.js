5247955527fba4f1821c4ba90ae4091382fe4abe
Simon Choy <simon.choy@duke.edu>
2019-11-05 10:12:19 -0500

    file obj details, fixed error when changing media type

--- app/assets/javascripts/hyrax/ms_media_form.js
+++ app/assets/javascripts/hyrax/ms_media_form.js
@@ -1,13 +1,174 @@
 $(document).on('turbolinks:load', function() {
 
-  if ($('form[id*="media"]').length) { // if media form page
+  if ($('form[id*="media"]').length) { // if media form page (add/edit)
+
+    var form = $('form[id*="media"]')[0];
+
+    // Hidden Default Scale Bar Field
+    var scaleBarGroup = document.querySelector('div.media_scale_bar');
+    var scaleBarGroupUl = scaleBarGroup.querySelector("ul");
+    var concatScaleBars = scaleBarGroup.querySelectorAll("input");
+    var concatScaleBarCount = (scaleBarGroup.querySelectorAll("input").length) - 1;
+
+    // Three part scale bar entry
+    var scaleBarWrapper = document.getElementById("media_scale_bar_wrapper");
+    var scaleBarWrapperUl = scaleBarWrapper.querySelector('ul');
+    var scaleBarWrapperLi = scaleBarWrapper.querySelector('li');
+
+    // concatenate rights holder name, type to rights holder
+    // Check the rightsHolder Field (will be hidden)
+    var targetGroup = document.querySelector('div.media_rights_holder');
+    var targetGroupUl = targetGroup.querySelector("ul");
+    var concatFields = targetGroup.querySelectorAll("input");
+    var concatFieldCount = (targetGroup.querySelectorAll("input").length) - 1;
+
+    // Two part rightsHolder entry
+    var targetWrapper = document.getElementById("media_rights_holder_wrapper");
+    var targetWrapperUl = targetWrapper.querySelector('ul');
+    var targetWrapperLi = targetWrapper.querySelector('li');
+
   	hide_fields(['.media_number_of_images_in_set','.media_scale_bar']);
   	adjust_form_media_type();
     setupRightsHolder();    
+    setupScaleBar();
 
     $(document).on('change', '#media_media_type', function() {
       adjust_form_media_type();
     });
+
+    // On submit, name and type fields are concatenated and inserted into hidden default rights holder field.
+    form.addEventListener("submit", function() {
+
+      $(targetGroupUl).empty(); // remove all items and re-build 
+      var rightsHolderCount = $('select[name="media[rights_holder_type][]"]').length;
+      for (i = 0; i < rightsHolderCount; i++) {
+
+        var rightsHolderName = $('input[name="media[rights_holder_name][]"]')[i].value || '';
+        var rightsHolderType = $('select[name="media[rights_holder_type][]"]')[i].value || '';
+
+        // As long as at least one input is filled out, proceed with creating a rightsHolder string. Otherwise, create an empty string.
+        if ((rightsHolderType != '') || (rightsHolderName != '')) {
+          var rightsHolder = "Name: " + rightsHolderName + ", Type: " + rightsHolderType;
+        } else {
+          var rightsHolder = '';
+        }
+        //console.log('rightsHolder: '+rightsHolder);
+        buildTargetRightHolderField(rightsHolder, targetGroupUl);
+      }
+
+      // If media type = photogrammetry, submit scale bar fields. Otherwise, submit an empty scale bar.
+      if($('#media_media_type').val() == 'PhotogrammetryImageSeries') {
+
+        var scaleBarCount = document.getElementsByClassName("media_scale_bar_target_type").length;
+
+        for (i = 0; i < scaleBarCount; i++) {
+
+          var targetType = document.getElementsByClassName("media_scale_bar_target_type")[i].value || '';
+          var scaleBarDistance = document.getElementsByClassName("media_scale_bar_distance")[i].value || '';
+          var scaleBarUnits = document.getElementsByClassName("media_scale_bar_units")[i].value || '';
+
+          // As long as at least one input is filled out, proceed with creating a scale bar string. Otherwise, create an empty string, which will not result in a new scale bar triple.
+          if ((targetType != '') || (scaleBarDistance != '') || (scaleBarUnits != '')) {
+            var scaleBar = "Type: " + targetType + ", Distance: " + scaleBarDistance + ", Units: " +  scaleBarUnits;
+          }
+          else {
+            var scaleBar = ''
+          }
+          buildScaleBar(scaleBar, scaleBarGroupUl);
+        }
+      }
+      else {
+        buildScaleBar('', scaleBarGroupUl);
+      }
+
+    }); // /on submit
+
+
+    // Puts concatenated values into scale bar on submit.
+    function buildScaleBar(scaleBar, scaleBarGroupUl) {
+      var li = document.createElement('li');
+
+      var input = document.createElement('input');
+      input.className = 'string multi_value optional media_scale_bar form-control multi-text-field';
+      input.setAttribute("id", "media_scale_bar");
+      input.setAttribute("name", "media[scale_bar][]");
+      input.value = scaleBar
+
+      li.appendChild(input);
+      scaleBarGroupUl.appendChild(li);
+    }
+
+    function setupScaleBar() {
+
+      // When editing a record, this populates the three-part scale bar fields with previously saved metadata.
+      for (i = 0; i < concatScaleBarCount; i++) {
+        var concatScaleBarValue = concatScaleBars[i].value
+        var type = concatScaleBarValue.match(/Type: (.*?), Distance: /)[1];
+        var distance = concatScaleBarValue.match(/, Distance: (.*?), Units: /)[1];
+        var units = concatScaleBarValue.match(/Units: (.*)/)[1];
+
+        // Fill in values for first line
+        if (i == 0) {
+          scaleBarWrapperLi.querySelectorAll('input')[0].value = type;
+          scaleBarWrapperLi.querySelectorAll('input')[1].value = distance;
+          scaleBarWrapperLi.querySelectorAll('input')[2].value = units;
+        }
+        // Add a new line for each additional scalebar
+        else {
+          // Assemble new type, distance, and units inputs
+          var li = document.createElement('li');
+          li.className = 'field-wrapper input-group input-append';
+          //li.setAttribute('style', "display:flex; flex-direction:row; justify-content:space-evenly;");
+
+          var typeInput = document.createElement('input');
+          typeInput.className = "string multi_value optional form-control media_scale_bar_target_type form-control multi-text-field";
+          typeInput.setAttribute("id", "media_scale_bar_target_type");
+          typeInput.setAttribute("name", "media[scale_bar_target_type][]");
+          //typeInput.setAttribute("style", "margin:5px; width:30%; border-radius:5px;");
+          typeInput.value = type;
+          li.appendChild(typeInput);
+
+          var distanceInput = document.createElement('input');
+          distanceInput.className = "string multi_value optional form-control media_scale_bar_distance form-control multi-text-field";
+          distanceInput.setAttribute("id", "media_scale_bar_distance");
+          distanceInput.setAttribute("name", "media[scale_bar_distance][]");
+          //distanceInput.setAttribute("style", "margin:5px; width:30%; border-radius:5px;");
+
+          distanceInput.value = distance;
+          li.appendChild(distanceInput);
+
+          var unitsInput = document.createElement('input');
+          unitsInput.className = "string multi_value optional form-control media_scale_bar_units form-control multi-text-field";
+          unitsInput.setAttribute("id", "media_scale_bar_units");
+          unitsInput.setAttribute("name", "media[scale_bar_units][]");
+          //unitsInput.setAttribute("style", "margin:5px; width:30%; border-radius:5px;");
+
+          unitsInput.value = units;
+          li.appendChild(unitsInput);
+
+          var span = document.createElement('span');
+          span.className = "input-group-btn field-controls";
+          span.innerHTML = `<button type="button" class="btn btn-link remove">
+                              <span class="glyphicon glyphicon-remove"></span>
+                              <span class="controls-remove-text">Remove</span>
+                              <span class="sr-only"> previous
+                                <span class="controls-field-name-text"> Scale Bar</span>
+                              </span>
+                            </button>`
+          span.innerHTML = '<button type="button" class="btn btn-link remove" data-index="' + i.toString() + '"><i class="fa fa-times-circle" aria-hidden="true"></i></button><button type="button" class="btn btn-link add"><i class="fa fa-plus-circle" aria-hidden="true"></i></button>'
+
+          li.appendChild(span);
+
+          scaleBarWrapperUl.appendChild(li);
+
+        }
+      }
+
+      // Clear default scale bar fields when done.
+      scaleBarGroupUl.innerHTML = '';
+
+
+    } // /setupScaleBar
 
     function adjust_form_media_type() {
       if ($('#media_media_type').val() == 'CTImageSeries') {
@@ -25,19 +186,6 @@ $(document).on('turbolinks:load', function() {
     }
 
     function setupRightsHolder() {
-      // concatenate rights holder name, type to rights holder
-      var form = $('form[id*="media"]')[0];
-
-      // Check the rightsHolder Field (will be hidden)
-      var targetGroup = document.querySelector('div.media_rights_holder');
-      var targetGroupUl = targetGroup.querySelector("ul");
-      var concatFields = targetGroup.querySelectorAll("input");
-      var concatFieldCount = (targetGroup.querySelectorAll("input").length) - 1;
-
-      // Two part rightsHolder entry
-      var targetWrapper = document.getElementById("media_rights_holder_wrapper");
-      var targetWrapperUl = targetWrapper.querySelector('ul');
-      var targetWrapperLi = targetWrapper.querySelector('li');
 
       // When editing a record, this populates the rightsHolder fields with previously saved metadata.
       for (i = 0; i < concatFieldCount; i++) {
@@ -86,7 +234,7 @@ $(document).on('turbolinks:load', function() {
 
           var span = document.createElement('span');
           span.className = "input-group-btn field-controls";
-          span.innerHTML = '<button type="button" class="btn btn-link remove remove-rights-holder" data-index="' + i.toString() + '"><i class="fa fa-times-circle" aria-hidden="true"></i></button><button type="button" class="btn btn-link add"><i class="fa fa-plus-circle" aria-hidden="true"></i></button>'
+          span.innerHTML = '<button type="button" class="btn btn-link remove" data-index="' + i.toString() + '"><i class="fa fa-times-circle" aria-hidden="true"></i></button><button type="button" class="btn btn-link add"><i class="fa fa-plus-circle" aria-hidden="true"></i></button>'
 
           li.appendChild(span);
           targetWrapperUl.appendChild(li);
@@ -97,26 +245,6 @@ $(document).on('turbolinks:load', function() {
       // Clear default rightsHolder fields when done.
       targetGroupUl.innerHTML = '';
       $(targetGroup).hide(); // hide the field label and add button
-
-      // On submit, name and type fields are concatenated and inserted into hidden default rights holder field.
-      form.addEventListener("submit", function() {
-        $(targetGroupUl).empty(); // remove all items and re-build 
-        var rightsHolderCount = $('select[name="media[rights_holder_type][]"]').length;
-        for (i = 0; i < rightsHolderCount; i++) {
-
-          var rightsHolderName = $('input[name="media[rights_holder_name][]"]')[i].value || '';
-          var rightsHolderType = $('select[name="media[rights_holder_type][]"]')[i].value || '';
-
-          // As long as at least one input is filled out, proceed with creating a rightsHolder string. Otherwise, create an empty string.
-          if ((rightsHolderType != '') || (rightsHolderName != '')) {
-            var rightsHolder = "Name: " + rightsHolderName + ", Type: " + rightsHolderType;
-          } else {
-            var rightsHolder = '';
-          }
-          //console.log('rightsHolder: '+rightsHolder);
-          buildTargetRightHolderField(rightsHolder, targetGroupUl);
-        }
-      });
 
     } // /setupRightsHolder
 
--- app/controllers/hyrax/media_controller.rb
+++ app/controllers/hyrax/media_controller.rb
@@ -30,6 +30,8 @@ module Hyrax
                  'morphosource_2_columns'
                when 'edit'
                  'morphosource_2_columns'
+               when 'update'
+                 'morphosource_2_columns'
                else
                  'dashboard'
                end
@@ -110,7 +112,17 @@ module Hyrax
         respond_to do |wants|
           wants.html do
             build_form
-            render 'edit', status: :unprocessable_entity
+            #render 'edit', status: :unprocessable_entity
+            # todo: make sure to handle error when changing media type
+            @presenter = show_presenter.new(curation_concern_from_search_results, current_ability, request)
+            @presenter.get_showcase_data
+
+
+            @new_organization_submit_submissions_url = '/submissions/new_organization_submit'
+            @new_organization_form = Hyrax::WorkFormService.build(::Organization.new, current_ability, self)
+            @countries_service = Morphosource::CountriesService.new
+            render '/hyrax/media/edit', presenter: @presenter, status: :unprocessable_entity
+
           end
           wants.json { render_json_response(response_type: :unprocessable_entity, options: { errors: curation_concern.errors }) }
         end

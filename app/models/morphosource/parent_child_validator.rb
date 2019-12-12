afdc1a4646b751e47916b0e249ecdc27f301ca4d
Simon Choy <simon.choy@duke.edu>
2019-12-12 17:49:50 -0500

    revert back parent child validator (show error if invalid)

--- app/models/morphosource/parent_child_validator.rb
+++ app/models/morphosource/parent_child_validator.rb
@@ -5,21 +5,13 @@ module Morphosource
 
     def validate(record)
       invalid_children = []
+
       record.works.each do |child|
         invalid_children << child.class unless record.valid_child_concerns.include? child.class
       end
 
-      # delete the child if invalid instead of giving an error (this is a workaround when saving Device
-      # in showcase media edit page)
-      # todo: might need to investigate why media is found as a child when saving device
-      #record.works.delete_if do |child|
-      #  unless record.valid_child_concerns.include? child.class
-      #    true # Make sure the if statement returns true, so it gets marked for deletion
-      #  end
-      #end
-      return
-      #return if invalid_children.empty?
-      #record.errors[:works] << "valid children for #{record.class} do not include #{invalid_children.uniq.join(', ')}."
+      return if invalid_children.empty?
+      record.errors[:works] << "valid children for #{record.class} do not include #{invalid_children.uniq.join(', ')}."
     end
 
   end

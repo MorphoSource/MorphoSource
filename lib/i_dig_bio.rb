de194085b92b040c48945d328f2aba196cd87378
Ryan Baumann <ryan.baumann@gmail.com>
2019-12-12 14:03:18 -0500

    MR-90: Fix IDigBio.search

--- lib/i_dig_bio.rb
+++ lib/i_dig_bio.rb
@@ -5,9 +5,10 @@ module IDigBio
   extend ActiveSupport::Autoload
 
   API_ENDPOINT = 'https://search.idigbio.org/v2'
+  ::RestClient.log = Rails.logger
 
   def self.search(search_query, limit = 500, fields = nil)
-    response = RestClient.get "#{API_ENDPOINT}/search/records/", {params: {rq: search_query, limit: limit}}
+    response = RestClient.post "#{API_ENDPOINT}/search/records/", {rq: search_query, limit: limit}.to_json, {content_type: :json, accept: :json}
     JSON.parse(response.body)['items'] if response
   end
 end

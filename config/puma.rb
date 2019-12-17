13fb6d4054670eeb7d83bef9fac84e5bd2e8b9aa
Julie Winchester <julia.m.winchester@gmail.com>
2019-12-17 21:29:53 +0000

    Update puma config to always use 0.0.0.0 as IP binding in development environment

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,9 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
+# Specify the default IP binding, otherwise defaults to localhost which Vagrant doesn't like
+set_default_host '0.0.0.0'
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }

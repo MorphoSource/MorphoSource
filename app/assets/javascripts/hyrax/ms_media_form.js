8331f8652f63dff1dde22b4324f3968127d24c4a
Simon Choy <simon.choy@duke.edu>
2019-12-18 14:58:20 -0500

    remove download and cart buttons, fix work relationship validation in IE controller, handle new PO button with modal

--- app/assets/javascripts/hyrax/ms_media_form.js
+++ app/assets/javascripts/hyrax/ms_media_form.js
@@ -14,6 +14,7 @@ $(document).on('turbolinks:load', function() {
 
     setupEmbeddedWorkForm('device', 'new', updateMediaTitle);
     setupEmbeddedWorkForm('organization', 'new', updateDevice);
+    setupEmbeddedWorkForm('biological_specimen', 'new');
     setupTooltip();
     removeLastRepeatable();
 

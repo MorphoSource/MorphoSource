5919e4afdd5325382815544f6831730bf122a5bc
Simon Choy <simon.choy@duke.edu>
2020-02-21 13:35:04 -0500

    format date in IE details, add datepicker in PE form

--- app/helpers/morphosource_helper.rb
+++ app/helpers/morphosource_helper.rb
@@ -245,4 +245,33 @@ module MorphosourceHelper
     url
   end
 
+  def display_date(value)
+    # first try to parse and format the date generated from datepicker (YYYY-MM-DD)
+    # for other format, e.g. "04/26/2019", it will throw invalid date exception
+    # in thac case, use DateTime.strptime instead
+    begin
+      Date.parse(value).to_formatted_s(:long)
+    rescue StandardError => e
+      if e.message == 'invalid date'
+        
+        begin
+          # attempt to parse the date
+          parsed_date = Date.strptime(value, "%m/%d/%Y")
+          if parsed_date.present?
+            parsed_date.to_formatted_s(:long)
+          else
+            value # just return the string as it is
+          end
+        rescue StandardError => e
+          value # just return the string as it is              
+        end
+
+      else
+        # if landed here. check e.message for the exception message
+        '(Error)'
+      end
+    end
+
+  end
+
 end
--- app/renderers/hyrax/renderers/showcase_date_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/showcase_date_attribute_renderer.rb
@@ -6,31 +6,7 @@ module Hyrax
       private
 
         def attribute_value_to_html(value)
-          begin
-            # TODO: currently Date.parse is expecting formats e.g. "26/04/2019", "2019-04-26"
-            # "04/26/2019" will throw invalid date exception
-            # Depending on the date picker on the form, we might need to use DateTime.strptime instead
-            Date.parse(value).to_formatted_s(:long)
-          rescue StandardError => e
-            if e.message == 'invalid date'
-              
-              begin
-                # attempt to parse the date
-                parsed_date = Date.strptime(value, "%m/%d/%Y")
-                if parsed_date.present?
-                  parsed_date.to_formatted_s(:long)
-                else
-                  value # just return the string as it is
-                end
-              rescue StandardError => e
-                value # just return the string as it is              
-              end
-
-            else
-              # if landed here. check e.message for the exception message
-              '(Error)'
-            end
-          end
+          display_date(value) # use the method from morphosource helper
         end
 
     end

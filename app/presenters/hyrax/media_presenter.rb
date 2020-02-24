f9c09700d6d99c8038c09e475fc3424de1624742
Simon Choy <simon.choy@duke.edu>
2020-02-24 18:23:17 -0500

    redirect after delete, rename presenter methods to snake case

--- app/presenters/hyrax/media_presenter.rb
+++ app/presenters/hyrax/media_presenter.rb
@@ -80,19 +80,19 @@ module Hyrax
       end
     end
 
-    def isPublished?
+    def is_published?
       @download_permission.include?('Publish')
     end
 
-    def hasChildMedia?
+    def has_child_media?
       @child_media_id_list.any?
     end
 
-    def hasProcessingEvents?
+    def has_processing_events?
       @processing_events.present?
     end
 
-    def hasImagingEvents?
+    def has_imaging_events?
       @imaging_event.present?
     end
 

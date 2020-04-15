0179383fff678631daca3b9922a590ba34e7eb29
Simon Choy <simon.choy@duke.edu>
2020-04-15 18:10:32 -0400

    batch add to cart controller method and form changes

--- app/assets/javascripts/hyrax/showcase.js
+++ app/assets/javascripts/hyrax/showcase.js
@@ -1,67 +1,6 @@
 $(document).on('turbolinks:load', function() {
 
   if ($('div[class*="showcase"]').length) { // check if the page is showcase page
-
-    var mediaTable = $('#datatable-media-list').DataTable({
-      responsive: true,
-      columnDefs: [
-        { responsivePriority: 1, targets: 0 },
-        { responsivePriority: 2, targets: 1 },
-        { responsivePriority: 3, targets: 2 },
-        { responsivePriority: 4, targets: -1 } // rightmost column
-      ],
-      pageLength: 10,
-      bPaginate: true,
-      bDestroy: true,
-      bLengthChange: false, // hide the show number of entries dropdown
-      bFilter: false // hide search box
-    });
-    var bsoTable = $('#datatable-bso-list').DataTable({
-      responsive: true,
-      columnDefs: [
-        { responsivePriority: 1, targets: 0 },
-        { responsivePriority: 3, targets: 1 },
-        { responsivePriority: 4, targets: 2 }
-      ],
-      pageLength: 10,
-      bPaginate: true,
-      bDestroy: true,
-      bLengthChange: false, // hide the show number of entries dropdown
-      bFilter: false // hide search box
-    });
-    var choTable = $('#datatable-cho-list').DataTable({
-      responsive: true,
-      columnDefs: [
-        { responsivePriority: 1, targets: 0 },
-        { responsivePriority: 3, targets: 1 },
-        { responsivePriority: 4, targets: 2 }
-      ],
-      pageLength: 10,
-      bPaginate: true,
-      bDestroy: true,
-      bLengthChange: false, // hide the show number of entries dropdown
-      bFilter: false // hide search box
-    });
-
-    // Toggle the visibility of table column
-    $('.choose-columns-media .toggle-vis').on( 'click', function (e) {
-      //e.preventDefault();
-      var column = mediaTable.column( $(this).attr('data-column') );
-      column.visible( ! column.visible() );
-    });
-    $('.choose-columns-bso .toggle-vis').on( 'click', function (e) {
-      var column = bsoTable.column( $(this).attr('data-column') );
-      column.visible( ! column.visible() );
-    });
-    $('.choose-columns-cho .toggle-vis').on( 'click', function (e) {
-      var column = choTable.column( $(this).attr('data-column') );
-      column.visible( ! column.visible() );
-    });
-
-    // keep dropdown menu open 
-    $(document).on('click', '.choose-columns .dropdown-menu', function (e) {
-      e.stopPropagation();
-    });
     
     // switching icons and button labels for expand / collapse
     $(".collapse-block").not(".glyphicon-only").on("hide.bs.collapse", function(){
@@ -82,6 +21,72 @@ $(document).on('turbolinks:load', function() {
       var thisId = $(this).attr('id');
       $("span."+thisId).removeClass("glyphicon-triangle-bottom").addClass("glyphicon-triangle-top")
     });
+
+
+    if ($('div[class*="teams"]').length) { // check if the page is teams/projects show page
+
+      var mediaTable = $('#datatable-media-list').DataTable({
+        responsive: true,
+        columnDefs: [
+          { responsivePriority: 1, targets: 0 },
+          { responsivePriority: 2, targets: 1 },
+          { responsivePriority: 3, targets: 2 },
+          { responsivePriority: 4, targets: -1 } // rightmost column
+        ],
+        pageLength: 10,
+        bPaginate: true,
+        bDestroy: true,
+        bLengthChange: false, // hide the show number of entries dropdown
+        bFilter: false // hide search box
+      });
+      var bsoTable = $('#datatable-bso-list').DataTable({
+        responsive: true,
+        columnDefs: [
+          { responsivePriority: 1, targets: 0 },
+          { responsivePriority: 3, targets: 1 },
+          { responsivePriority: 4, targets: 2 }
+        ],
+        pageLength: 10,
+        bPaginate: true,
+        bDestroy: true,
+        bLengthChange: false, // hide the show number of entries dropdown
+        bFilter: false // hide search box
+      });
+      var choTable = $('#datatable-cho-list').DataTable({
+        responsive: true,
+        columnDefs: [
+          { responsivePriority: 1, targets: 0 },
+          { responsivePriority: 3, targets: 1 },
+          { responsivePriority: 4, targets: 2 }
+        ],
+        pageLength: 10,
+        bPaginate: true,
+        bDestroy: true,
+        bLengthChange: false, // hide the show number of entries dropdown
+        bFilter: false // hide search box
+      });
+
+      // Toggle the visibility of table column
+      $('.choose-columns-media .toggle-vis').on( 'click', function (e) {
+        //e.preventDefault();
+        var column = mediaTable.column( $(this).attr('data-column') );
+        column.visible( ! column.visible() );
+      });
+      $('.choose-columns-bso .toggle-vis').on( 'click', function (e) {
+        var column = bsoTable.column( $(this).attr('data-column') );
+        column.visible( ! column.visible() );
+      });
+      $('.choose-columns-cho .toggle-vis').on( 'click', function (e) {
+        var column = choTable.column( $(this).attr('data-column') );
+        column.visible( ! column.visible() );
+      });
+
+      // keep dropdown menu open 
+      $(document).on('click', '.choose-columns .dropdown-menu', function (e) {
+        e.stopPropagation();
+      });
+
+    } // / end if teams and projects show page
 
   } // end if the page is showcase page 
 

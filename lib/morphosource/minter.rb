22bf48ec59a0278e0d89c5bd28e4ff03c2bf6d63
Ryan Baumann <ryan.baumann@gmail.com>
2020-05-07 12:17:25 -0400

    MR-746: gracefully handle errors when Morphosource::Minter is instantiated before database initialization, e.g. during initial setup

--- lib/morphosource/minter.rb
+++ lib/morphosource/minter.rb
@@ -7,13 +7,17 @@ module Morphosource
       Rails.logger.info("Morphosource::Minter noid-rails namespace: #{::Noid::Rails.config.namespace}")
       Rails.logger.info("Morphosource::Minter noid-rails template: #{::Noid::Rails.config.template}")
 
-      minter = ::Noid::Rails::Service.new.minter.read
-      Rails.logger.info("Morphosource::Minter seq: #{minter[:seq]}")
-      Rails.logger.info("Morphosource::Minter noid_sequence_start: #{Rails.configuration.noid_sequence_start}")
-      if minter[:seq] && (minter[:seq] < Rails.configuration.noid_sequence_start)
-        minter[:seq] = Rails.configuration.noid_sequence_start
-        ::Noid::Rails::Service.new.minter.write!(minter)
-        Rails.logger.info("Morphosource::Minter updated seq: #{minter[:seq]}")
+      begin
+        minter = ::Noid::Rails::Service.new.minter.read
+        Rails.logger.info("Morphosource::Minter seq: #{minter[:seq]}")
+        Rails.logger.info("Morphosource::Minter noid_sequence_start: #{Rails.configuration.noid_sequence_start}")
+        if minter[:seq] && (minter[:seq] < Rails.configuration.noid_sequence_start)
+          minter[:seq] = Rails.configuration.noid_sequence_start
+          ::Noid::Rails::Service.new.minter.write!(minter)
+          Rails.logger.info("Morphosource::Minter updated seq: #{minter[:seq]}")
+        end
+      rescue StandardError => e
+        Rails.logger.error("Morphosource::Minter error: #{e.inspect}")
       end
     end
   end

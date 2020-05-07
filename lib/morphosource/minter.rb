ab4b0f4675d238a20c6a7b315dad3ed14fbb140c
Ryan Baumann <ryan.baumann@gmail.com>
2020-05-07 13:11:19 -0400

    MR-746: slower but more robust mechanism for setting noid sequence start

--- lib/morphosource/minter.rb
+++ lib/morphosource/minter.rb
@@ -12,12 +12,13 @@ module Morphosource
         Rails.logger.info("Morphosource::Minter seq: #{minter[:seq]}")
         Rails.logger.info("Morphosource::Minter noid_sequence_start: #{Rails.configuration.noid_sequence_start}")
         if minter[:seq] && (minter[:seq] < Rails.configuration.noid_sequence_start)
-          minter[:seq] = Rails.configuration.noid_sequence_start
-          ::Noid::Rails::Service.new.minter.write!(minter)
-          Rails.logger.info("Morphosource::Minter updated seq: #{minter[:seq]}")
+          (Rails.configuration.noid_sequence_start - minter[:seq]).times do
+            ::Noid::Rails::Service.new.minter.mint
+          end
+          Rails.logger.info("Morphosource::Minter updated seq: #{::Noid::Rails::Service.new.minter.read[:seq]}")
         end
       rescue StandardError => e
-        Rails.logger.error("Morphosource::Minter error: #{e.inspect}")
+        Rails.logger.error("Morphosource::Minter error: #{e.inspect} #{e.backtrace}")
       end
     end
   end

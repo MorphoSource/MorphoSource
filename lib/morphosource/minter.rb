18e19036e973e5d1ecc1b9fb2a68f79cbd8684eb
Ryan Baumann <ryan.baumann@gmail.com>
2020-06-09 10:41:28 -0400

    MR-746: fast & robust minter initialization

--- lib/morphosource/minter.rb
+++ lib/morphosource/minter.rb
@@ -6,11 +6,20 @@ module Morphosource
       Rails.logger.info("Morphosource::Minter loaded")
       Rails.logger.info("Morphosource::Minter noid-rails namespace: #{::Noid::Rails.config.namespace}")
       Rails.logger.info("Morphosource::Minter noid-rails template: #{::Noid::Rails.config.template}")
+      Rails.logger.info("Morphosource::Minter noid-rails minter class: #{::Noid::Rails.config.minter_class.to_s}")
+      Rails.logger.info("Morphosource::Minter noid_sequence_start: #{Rails.configuration.noid_sequence_start}")
 
       begin
+        if (::Noid::Rails.config.minter_class == Noid::Rails::Minter::Db) && (ActiveRecord::Base.connection.table_exists?('minter_states'))
+          minter = ::Noid::Rails::Service.new.minter.read
+          Rails.logger.info("Morphosource::Minter seq: #{minter[:seq]}")
+          if minter[:seq] && (minter[:seq].to_i < Rails.configuration.noid_sequence_start)
+            Rails.logger.info("Morphosource::Minter initializing DB-backed minter state")
+            ActiveRecord::Base.connection.execute("UPDATE minter_states SET seq = #{Rails.configuration.noid_sequence_start} WHERE minter_states.namespace = '#{::Noid::Rails.config.namespace}' AND seq < #{Rails.configuration.noid_sequence_start}")
+          end
+        end
         minter = ::Noid::Rails::Service.new.minter.read
         Rails.logger.info("Morphosource::Minter seq: #{minter[:seq]}")
-        Rails.logger.info("Morphosource::Minter noid_sequence_start: #{Rails.configuration.noid_sequence_start}")
         if minter[:seq] && (minter[:seq].to_i < Rails.configuration.noid_sequence_start)
           (Rails.configuration.noid_sequence_start - minter[:seq].to_i).times do
             ::Noid::Rails::Service.new.minter.mint

2cbce3894a3159fa8a32e4f9d9124cb1dc141a4f
Julie Winchester <julia.m.winchester@gmail.com>
2020-06-23 14:58:13 +0000

    Migrate MS1 contributor status

--- app/jobs/batch_user_import_job.rb
+++ app/jobs/batch_user_import_job.rb
@@ -3,8 +3,16 @@ class BatchUserImportJob < ApplicationJob
   queue_as Hyrax.config.ingest_queue_name
 
   def perform(attributes)
+  	if attributes[:contributor] == "true"
+  		contributor = true
+  	else
+  		contributor = false
+  	end
+  	byebug
+  	attributes.delete(:contributor)
     u = User.new(attributes)
     u.validate!
+    u.make_contributor if contributor
     u.save!
   end
 

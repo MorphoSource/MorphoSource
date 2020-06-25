2203fa066c71d15093ec35ed77678e72538f9a5d
JocelynTriplett <jocelyn.triplett@gmail.com>
2020-06-25 15:20:44 -0400

    morphosource setup

---
+++ lib/morphosource/users/defaults.rb
@@ -0,0 +1,67 @@
+module Morphosource
+  module Users
+    module Defaults
+
+      # Settings used by morphosource.rake when creating default development user accounts
+      ADMIN = {
+        email: "admin@email.com",
+        password: 'password',
+        guest: false,
+        facebook_handle: 'admin',
+        twitter_handle: '@Admin',
+        display_name: 'AdminFirst AdminLast',
+        address: '100 Main Street',
+        department: 'Admin Department',
+        website: 'www.admin.com',
+        affiliation: 'Admin Affiliation',
+        telephone: '(888) 555-5555',
+        orcid: 'https://orcid.org/0000-0000-0000-0000',
+        state: 'NC',
+        country: 'USA',
+        postal_code: '27278',
+        terms_read: true,
+        ms1_user: false
+      }
+
+      CONTRIBUTOR = {
+        email: "contributor@email.com",
+        password: 'password',
+        guest: false,
+        facebook_handle: 'contributor',
+        twitter_handle: '@Contributor',
+        display_name: 'ContributorFirst ContributorLast',
+        address: '100 Main Street',
+        department: 'Contributor Department',
+        website: 'www.contributor.com',
+        affiliation: 'Contributor Affiliation',
+        telephone: '(888) 555-5555',
+        orcid: 'https://orcid.org/0000-0000-0000-0000',
+        state: 'NC',
+        country: 'USA',
+        postal_code: '27278',
+        terms_read: true,
+        ms1_user: false
+      }
+
+      REGISTERED = {
+        email: "registered@email.com",
+        password: 'password',
+        guest: false,
+        facebook_handle: 'registered',
+        twitter_handle: '@Registered',
+        display_name: 'RegisteredFirst RegisteredLast',
+        address: '100 Main Street',
+        department: 'Registered Department',
+        website: 'www.registered.com',
+        affiliation: 'Registered Affiliation',
+        telephone: '(888) 555-5555',
+        orcid: 'https://orcid.org/0000-0000-0000-0000',
+        state: 'NC',
+        country: 'USA',
+        postal_code: '27278',
+        terms_read: true,
+        ms1_user: false
+      }
+    end
+  end
+end

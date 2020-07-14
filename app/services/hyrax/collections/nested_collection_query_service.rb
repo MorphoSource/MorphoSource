c1e260a45c01d8a85e5ad119a714f83c12060db9
JocelynTriplett <jocelyn.triplett@gmail.com>
2020-07-14 08:55:07 -0400

    nest projects within teams

--- app/services/hyrax/collections/nested_collection_query_service.rb
+++ app/services/hyrax/collections/nested_collection_query_service.rb
@@ -35,7 +35,12 @@ module Hyrax
       def self.available_child_collections(parent:, scope:, limit_to_id: nil)
         return [] unless parent.try(:nestable?)
         return [] unless scope.can?(:deposit, parent)
-        query_solr(collection: parent, access: :read, scope: scope, limit_to_id: limit_to_id, nest_direction: :as_child).documents
+        # projects can't have child collections
+        return [] if parent.project?
+        results = query_solr(collection: parent, access: :read, scope: scope, limit_to_id: limit_to_id, nest_direction: :as_child).documents
+        # if parent is a team, return only projects without parents
+        results.select!{ |r| r["nesting_collection__parent_ids_ssim"].nil? } if parent.team?
+        results
       end
 
       # @api public
@@ -49,9 +54,12 @@ module Hyrax
       def self.available_parent_collections(child:, scope:, limit_to_id: nil)
         return [] unless child.try(:nestable?)
         return [] unless scope.can?(:read, child)
+        # teams can't have parent collections
+        return [] if child.team?
+        # projects can have only one parent
+        return [] if child.project? && child.parent?
         query_solr(collection: child, access: :deposit, scope: scope, limit_to_id: limit_to_id, nest_direction: :as_parent).documents
       end
-
       # @api public
       #
       # What collections is the given child nested within?
@@ -117,12 +125,18 @@ module Hyrax
       # @todo Consider expanding from same collection type to a lookup table that says "This collection type can have within it, these collection types"
       def self.parent_and_child_can_nest?(parent:, child:, scope:)
         return false if parent == child # Short-circuit
-        return false unless parent.collection_type_gid == child.collection_type_gid
+        if parent.collection_type_gid != child.collection_type_gid
+          # Teams are able to have child projects
+          return false unless parent.team? && child.project?
+          # Projects can have only one parent
+          return false if child.parent?
+        else
+          return false if parent.team? || parent.project?
+        end
         return false if available_parent_collections(child: child, scope: scope, limit_to_id: parent.id).none?
         return false if available_child_collections(parent: parent, scope: scope, limit_to_id: child.id).none?
         true
       end
-
       # @api public
       #
       # Does the nesting depth fall within defined limit?

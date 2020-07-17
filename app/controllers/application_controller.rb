5663aeb660cc6104c3ce95a0b68bf5324c59d742
Simon Choy <simon.choy@duke.edu>
2020-07-16 21:19:09 -0400

    removed mini profiler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,15 +14,6 @@ class ApplicationController < ActionController::Base
   # Blacklight discarding flash messages - see https://github.com/samvera/hyrax/issues/1596
   skip_after_action :discard_flash_if_xhr
 
-  # temporarily loading miniprofiler on production environment
-  # todo: remove later when not needed
-  # https://github.com/MiniProfiler/rack-mini-profiler#access-control-in-non-development-environments
-  before_action do
-    if current_user && current_user.admin?
-      Rack::MiniProfiler.authorize_request
-    end
-  end
-
   protected
 
   def after_sign_in_path_for(resource)

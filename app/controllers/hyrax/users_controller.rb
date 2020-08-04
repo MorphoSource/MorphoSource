fcfe61ca6d153bcee90831c3dfcf63d3e690fe3f
JocelynTriplett <jocelyn.triplett@gmail.com>
2020-08-04 17:22:32 -0400

    return email in user search results instead of ms_id

--- app/controllers/hyrax/users_controller.rb
+++ app/controllers/hyrax/users_controller.rb
@@ -25,7 +25,7 @@ module Hyrax
       def search(query)
         clause = query.blank? ? nil : "%" + query.downcase + "%"
         base = ::User.where(*base_query)
-        base = base.where("#{Hydra.config.user_key_field} like lower(?) OR display_name like lower(?)", clause, clause) if clause.present?
+        base = base.where("email like lower(?) OR lower(display_name) like lower(?)", clause, clause) if clause.present?
         base.registered
             .where("#{Hydra.config.user_key_field} not in (?)",
                    [::User.batch_user_key, ::User.audit_user_key])
--- app/views/hyrax/users/index.json.jbuilder
+++ app/views/hyrax/users/index.json.jbuilder
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 json.users @users do |user|
-  json.id user.id
-  json.user_key user.user_key
-  json.text user.to_s
+  json.id user.user_key
+  json.text user.email
 end

3f525186eb3629b05fae16970bd91c2719511512
Ryan Baumann <ryan.baumann@gmail.com>
2020-08-07 09:34:46 -0400

    MR-800: hook up commercial/3D use permitted fields on Media

---
+++ app/services/morphosource/commercial_use_types_service.rb
@@ -0,0 +1,18 @@
+# app/services/morphosource/series_types_service.rb
+module Morphosource
+  # Provide select options for the series types field
+  class CommercialUseTypesService < Hyrax::QaSelectService
+    def initialize(_authority_name = nil)
+      super('commercial_use_types')
+    end
+
+    def include_current_value(value, _index, render_options, html_options)
+      unless value.blank? || active?(value)
+        html_options[:class] << ' force-select'
+        render_options += [[label(value) { value }, value]]
+      end
+      [render_options, html_options]
+    end
+
+  end
+end

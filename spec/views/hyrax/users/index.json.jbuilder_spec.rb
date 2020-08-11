9f30e662fa020a5bd803b7643e435281c703279f
JocelynTriplett <jocelyn.triplett@gmail.com>
2020-08-11 11:38:04 -0400

    fix user jbuilder test

--- spec/views/hyrax/users/index.json.jbuilder_spec.rb
+++ spec/views/hyrax/users/index.json.jbuilder_spec.rb
@@ -15,9 +15,9 @@ RSpec.describe "hyrax/users/index.json.jbuilder" do
   it "lists users by ms_id and email" do
     json = JSON.parse(rendered)
     expect(json['users']).to match_array([
-      { 'id' => user1.ms_id, 'text' => user1.email },
-      { 'id' => user2.ms_id, 'text' => user2.email },
-      { 'id' => user3.ms_id, 'text' => user3.email }
+      { 'id' => user1.id, 'user_key' => user1.ms_id, 'text' => user1.email },
+      { 'id' => user2.id, 'user_key' => user2.ms_id, 'text' => user2.email },
+      { 'id' => user3.id, 'user_key' => user3.ms_id, 'text' => user3.email }
       ])
   end
 end

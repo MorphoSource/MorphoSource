62cf60797d3350bd73543149709cac6a711ecd1c
Ryan Baumann <ryan.baumann@gmail.com>
2020-08-12 15:25:48 -0400

    MR-800: set/restrict "Commercial Use Permitted" based on Copyright Statement

--- app/assets/javascripts/hyrax/ms_copyright_licensing_fields.js
+++ app/assets/javascripts/hyrax/ms_copyright_licensing_fields.js
@@ -5,7 +5,8 @@ $( document ).ready(function() {
       0: ['http://rightsstatements.org/vocab/InC-RUU/1.0/'],
       1: ['http://rightsstatements.org/vocab/InC/1.0/', 'http://rightsstatements.org/vocab/InC-OW-EU/1.0/', 'http://rightsstatements.org/vocab/InC-EDU/1.0/'],
       2: ['http://rightsstatements.org/vocab/InC-NC/1.0/'],
-      3: ['http://rightsstatements.org/vocab/NoC-CR/1.0/', 'http://rightsstatements.org/vocab/NoC-OKLR/1.0/', 'http://rightsstatements.org/vocab/NoC-NC/1.0/', 'http://rightsstatements.org/vocab/NoC-US/1.0/', 'http://rightsstatements.org/vocab/NKC/1.0/']
+      3: ['http://rightsstatements.org/vocab/NoC-CR/1.0/', 'http://rightsstatements.org/vocab/NoC-OKLR/1.0/', 'http://rightsstatements.org/vocab/NoC-US/1.0/', 'http://rightsstatements.org/vocab/NKC/1.0/'],
+      4: ['http://rightsstatements.org/vocab/NoC-NC/1.0/']
     }
 
     var conditionMatch = null;
@@ -17,18 +18,39 @@ $( document ).ready(function() {
     switch (conditionMatch) {
       case '0':
         disableLicense(media_or_organization, ['https://creativecommons.org/licenses/by/4.0/', 'https://creativecommons.org/licenses/by-sa/4.0/', 'https://creativecommons.org/licenses/by-nd/4.0/', 'https://creativecommons.org/licenses/by-nc/4.0/', 'https://creativecommons.org/licenses/by-nc-nd/4.0/', 'https://creativecommons.org/licenses/by-nc-sa/4.0/', 'http://creativecommons.org/publicdomain/zero/1.0/', 'http://creativecommons.org/publicdomain/mark/1.0/', 'http://www.morphosource.org/terms/licenseUnknown/']);
+        setCommercialUsePermitted(true);
         break;
       case '1':
         disableLicense(media_or_organization, ['http://creativecommons.org/publicdomain/mark/1.0/']);
+        setCommercialUsePermitted(true);
         break;
       case '2':
         disableLicense(media_or_organization, ['https://creativecommons.org/licenses/by/4.0/', 'https://creativecommons.org/licenses/by-sa/4.0/', 'https://creativecommons.org/licenses/by-nd/4.0/', 'http://creativecommons.org/publicdomain/zero/1.0/', 'http://creativecommons.org/publicdomain/mark/1.0/']);
+        setCommercialUsePermitted(false);
         break;
       case '3':
         disableLicense(media_or_organization, ['https://creativecommons.org/licenses/by/4.0/', 'https://creativecommons.org/licenses/by-sa/4.0/', 'https://creativecommons.org/licenses/by-nd/4.0/', 'https://creativecommons.org/licenses/by-nc/4.0/', 'https://creativecommons.org/licenses/by-nc-nd/4.0/', 'https://creativecommons.org/licenses/by-nc-sa/4.0/']);
+        setCommercialUsePermitted(true);
+        break;
+      case '4':
+        disableLicense(media_or_organization, ['https://creativecommons.org/licenses/by/4.0/', 'https://creativecommons.org/licenses/by-sa/4.0/', 'https://creativecommons.org/licenses/by-nd/4.0/', 'https://creativecommons.org/licenses/by-nc/4.0/', 'https://creativecommons.org/licenses/by-nc-nd/4.0/', 'https://creativecommons.org/licenses/by-nc-sa/4.0/']);
+        setCommercialUsePermitted(false);
         break;
       default:
         break;
+    }
+  };
+
+  var setCommercialUsePermitted = function(commercial_use_permitted) {
+    var commercial_use_permitted_option = $('select[name="media[permits_commercial_use]"] option[value="CommercialUsePermitted"]');
+    if (commercial_use_permitted) {
+      commercial_use_permitted_option.removeAttr('disabled');
+    }
+    else {
+      if ($('select[name="media[permits_commercial_use]"]').val() == commercial_use_permitted_option.val()) {
+        $('select[name="media[permits_commercial_use]"]').val('CommercialUseNotPermitted');
+      }
+      commercial_use_permitted_option.attr('disabled', 'disabled');
     }
   };
 

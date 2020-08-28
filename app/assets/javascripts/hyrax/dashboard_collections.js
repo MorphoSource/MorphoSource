9c4922620017e4a8d447d0ce4a50c35aa96bd23e
Simon Choy <simon.choy@duke.edu>
2020-08-28 10:25:24 -0400

    added datatables and column chooser

---
+++ app/assets/javascripts/hyrax/dashboard_collections.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    if ($('body.dashboard.teams').length) { // check if the page is dashboard media works
+
+      var collectionsTable = $('#collections-list-table').DataTable({
+        responsive: {
+          details: {
+            type: 'column' // hide the details and button in responsive
+          }
+        },
+        //order: [[ 2, "asc" ]],  // if sorting is needed, we might need to sort the same way in gallery view
+        columnDefs: [
+          { orderable: false, targets: 0 }, // disable sorting
+          { orderable: false, targets: -1 },
+          { responsivePriority: 1, targets: 0 },
+          { responsivePriority: 2, targets: 1 },
+          { responsivePriority: 3, targets: 2 },
+          { responsivePriority: 4, targets: -1 } // rightmost column
+        ],
+        pageLength: 10,
+        bPaginate: false,
+        bInfo: false,
+        bDestroy: true,
+        bLengthChange: false, // hide the show number of entries dropdown
+        bFilter: false // hide search box
+      });
+
+      // Toggle the visibility of table column
+      $('.choose-columns-collections .toggle-vis').on( 'click', function (e) {
+        //e.preventDefault();
+        var column = collectionsTable.column( $(this).attr('data-column') );
+        column.visible( ! column.visible() );
+      });
+
+      // keep dropdown menu open
+      $(document).on('click', '.choose-columns .dropdown-menu', function (e) {
+        e.stopPropagation();
+      });
+
+    } // / end if dashboard collections
+
+});

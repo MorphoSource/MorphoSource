14b9a25ee20b558dfcaed4313f78f56d2cada4c5
Ryan Baumann <ryan.baumann@gmail.com>
2020-09-28 11:57:52 -0400

    MR-803: redirect to Team page for Team-linked Organizations

--- app/controllers/hyrax/organizations_controller.rb
+++ app/controllers/hyrax/organizations_controller.rb
@@ -17,6 +17,18 @@ module Hyrax
     self.show_presenter = Hyrax::OrganizationPresenter
     with_themed_layout 'morphosource_1_column'
 
+    def show
+      @curation_concern ||= ActiveFedora::Base.find(params[:id])
+      if @curation_concern.team_id.present?
+        Rails.logger.info("MR-803: organization #{params[:id]} has team: #{@curation_concern.team_id.inspect}")
+        redirect_to "/teams/#{@curation_concern.team_id.first}"
+      else
+        presenter
+        query_collection_information
+        query_collection_members
+      end
+    end
+
     def update
       if update_work
         after_update_response

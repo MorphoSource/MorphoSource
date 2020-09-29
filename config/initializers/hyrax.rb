81d32c1ada8a416a4e58ea1bb1c4415ce91acca9
Simon Choy <simon.choy@duke.edu>
2020-09-29 15:46:34 -0400

    change email sender, update email template

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -39,7 +39,7 @@ Hyrax.config do |config|
   # config.rendering_predicate = ::RDF::DC.hasFormat
 
   # Email recipient of messages sent via the contact form
-  # config.contact_email = "repo-admin@example.org"
+  config.contact_email = "morphosource@duke.edu"
 
   # Text prefacing the subject entered in the contact form
   # config.subject_prefix = "Contact form:"

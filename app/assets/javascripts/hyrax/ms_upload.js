3d003ecf49d2f607d45c1de7ff6acc480384ca29
Simon Choy <simon.choy@duke.edu>
2020-10-08 16:44:18 -0400

    different content and partials when add media to collection

--- app/assets/javascripts/hyrax/ms_upload.js
+++ app/assets/javascripts/hyrax/ms_upload.js
@@ -1,14 +1,9 @@
 // Use this script to override jquery file upload options.  Add method can also be defined here
-// console.log('in ms_upload');
-
 $( document ).ready(function() {
-    // This file is the default initialization of the fileupload.  If you want to call
-    // hyraxUploader with other options (like afterSubmit), then override this file.
-
-  // Blacklight.onLoad(function() {  // for some reason, Blacklight.onLoad does not work any more
+  // This file is the default initialization of the fileupload.  If you want to call
+  // hyraxUploader with other options (like afterSubmit), then override this file.
   // Check if the file upload widget exists
   if ($('#fileupload').length) {
-
     var options = {
         maxNumberOfFiles:1,
         maxFileSize: 50000000000,
@@ -16,8 +11,6 @@ $( document ).ready(function() {
     };
     $('#fileupload').hyraxUploader(options);
     $('#fileuploadlogo').hyraxUploader({downloadTemplateId: 'logo-template-download'});
-
   }
-  //    });    
 
 });

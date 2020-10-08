7e5524321d1b99d278c7152a99c0075429e7c878
Simon Choy <simon.choy@duke.edu>
2020-10-07 20:03:55 -0400

    upload limit to 1 file

--- app/assets/javascripts/hyrax/ms_upload.js
+++ app/assets/javascripts/hyrax/ms_upload.js
@@ -2,17 +2,22 @@
 // console.log('in ms_upload');
 
 $( document ).ready(function() {
-
     // This file is the default initialization of the fileupload.  If you want to call
     // hyraxUploader with other options (like afterSubmit), then override this file.
-    Blacklight.onLoad(function() {
-      var options = {
-          maxNumberOfFiles:10,
-          maxFileSize: 50000000000,
-          acceptFileTypes: /(\.|\/)(zip|ply|stl|obj|x3d|glb|gltf|bin|wrl|png|gif|bmp|dcm|dicom|jpe?g|jpeg2000|tif?f|mtl|pdf|wmv|mov|avi|mpe?g|m4v|dng|nef|crw|cr2|cr3|iiq|arw|raw|rw2)$/i
-      };
-      $('#fileupload').hyraxUploader(options);
-      $('#fileuploadlogo').hyraxUploader({downloadTemplateId: 'logo-template-download'});
-    });    
+
+  // Blacklight.onLoad(function() {  // for some reason, Blacklight.onLoad does not work any more
+  // Check if the file upload widget exists
+  if ($('#fileupload').length) {
+
+    var options = {
+        maxNumberOfFiles:1,
+        maxFileSize: 50000000000,
+        acceptFileTypes: /(\.|\/)(zip|ply|stl|obj|x3d|glb|gltf|bin|wrl|png|gif|bmp|dcm|dicom|jpe?g|jpeg2000|tif?f|mtl|pdf|wmv|mov|avi|mpe?g|m4v|dng|nef|crw|cr2|cr3|iiq|arw|raw|rw2)$/i
+    };
+    $('#fileupload').hyraxUploader(options);
+    $('#fileuploadlogo').hyraxUploader({downloadTemplateId: 'logo-template-download'});
+
+  }
+  //    });    
 
 });

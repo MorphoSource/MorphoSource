98d2777cf0b9e6ee76e290d66d4d8c2eac93305c
Simon Choy <simon.choy@duke.edu>
2020-10-14 12:44:38 -0400

    updated application js for collection visibility status

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,8 +28,8 @@
 //= require morphosource/ms_editor
 //= require morphosource/media/upload_formats
 //= require morphosource/media/select_publication_status
-//= require morphosource/media/select_visibility_status
 //= require morphosource/media/tags
+//= require morphosource/collections/select_visibility_status
 //= require morphosource/ms_save_work_control
 //= require morphosource/ms_control
 //= require morphosource/ms_registry

80451170f562730c3381989c1ebac8f6c8023148
Julie Winchester <julia.m.winchester@gmail.com>
2020-10-23 22:08:24 +0000

    Various submission tweaks and new organization search facets

--- app/authorities/qa/authorities/find_organizations.rb
+++ app/authorities/qa/authorities/find_organizations.rb
@@ -1,5 +1,6 @@
 module Qa::Authorities
   class FindOrganizations < Qa::Authorities::FindWorks
+    self.search_builder_class = Morphosource::My::FindOrganizationsSearchBuilder
 
     def search(_q, controller)
       # The My::FindWorksSearchBuilder expects a current_user
@@ -10,7 +11,7 @@ module Qa::Authorities
       docs = response.documents
       docs.map do |doc|
         id = doc.id
-        title = doc.title
+        title = doc.institution_name.present? ? [ doc.institution_name&.first + ', ' + doc.title&.first ] : doc.title
         organization_type = doc.organization_type
         institution_code = doc.institution_code
         institution_name = doc.institution_name

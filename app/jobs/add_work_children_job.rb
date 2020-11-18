e228da7a181a70dba218e8ad1042e724cf9fa30e
Julie Winchester <julia.m.winchester@gmail.com>
2020-11-18 18:16:04 +0000

    Make add children to work jobs more efficient

--- app/jobs/add_work_children_job.rb
+++ app/jobs/add_work_children_job.rb
@@ -3,7 +3,7 @@ class AddWorkChildrenJob < ApplicationJob
 
   # @param [String] parent_id ID string of parent work
   # @param [Array/String] child_ids Array of child work IDs or string of single child ID
-  def perform(parent_id, child_ids)
+  def perform(parent_id, child_ids, delete_existing=false)
     if ::ActiveFedora::Base.exists?(parent_id)
       parent = ::ActiveFedora::Base.find(parent_id)
     else
@@ -11,14 +11,17 @@ class AddWorkChildrenJob < ApplicationJob
     end
 
     children = Array(child_ids)
+      .select { |c| !parent.member_ids.include?(c) }
       .map { |c| ::ActiveFedora::Base.exists?(c) ? ::ActiveFedora::Base.find(c) : nil }
       .compact
     return false if !children.present?
     
-    parent.members.each do |child|
-      parent.ordered_members.delete(child)
-      parent.members.delete(child)
-      parent.save!
+    if delete_existing
+      parent.members.each do |child|
+        parent.ordered_members.delete(child)
+        parent.members.delete(child)
+        parent.save!
+      end
     end
 
     parent.reload

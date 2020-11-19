48a9f83d7943e3b6123ac66dc614426aeccc606f
Julie Winchester <julia.m.winchester@gmail.com>
2020-11-19 17:22:38 +0000

    Add contextual explanation

--- config/initializers/active_fedora_general.rb
+++ config/initializers/active_fedora_general.rb
@@ -1,6 +1,7 @@
 # Monkey-patch to short circuit ActiveModel::Dirty which attempts to load the whole master files ordered list when calling nodes_will_change!
 # This leads to a stack level too deep exception when attempting to delete a master file from a media object on the manage files step.
 # See https://github.com/samvera/active_fedora/pull/1312/commits/7c8bbbefdacefd655a2ca653f5950c991e1dc999#diff-28356c4daa0d55cbaf97e4269869f510R100-R103
+# MorphoSource is using this monkey-patch in response to a bug where adding a child to a work with too many other children causes a similar stack level too deep error
 ActiveFedora::Aggregation::ListSource.class_eval do
   def attribute_will_change!(attr)
     return super unless attr == 'nodes'

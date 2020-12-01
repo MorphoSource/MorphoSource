ff7956d3ed8625c028ff09c8f6955079d505d782
Simon Choy <simon.choy@duke.edu>
2020-12-01 09:29:09 -0500

    redirect non-contributor to cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'dashboard', to: redirect('/dashboard/my/media', status: 301)
-
   scope module: :morphosource do
     scope module: :dashboard do
       post 'dashboard/collections/:id', controller: :collection_members, action: :update_members, as: 'update_members'

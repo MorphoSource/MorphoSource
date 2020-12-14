1090e45bf7f70ef45f74af812fe8f3ef266b1f4c
Julie Winchester <julia.m.winchester@gmail.com>
2020-12-14 19:52:53 +0000

    Add Laser Aided Profiling and Video to modalities

--- app/helpers/morphosource_helper.rb
+++ app/helpers/morphosource_helper.rb
@@ -252,6 +252,10 @@ module MorphosourceHelper
       'BD'
     when 'XRay'
       'XRay'
+    when 'LaserAidedProfiling'
+      'LAP'
+    when 'Video'
+      'Video'
     else
       'Etc'
     end

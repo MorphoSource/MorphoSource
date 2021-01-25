ae79860b804202181c9c07814414e8d6f67c4b27
Julie Winchester <julie.winchester@duke.edu>
2021-01-25 18:56:34 +0000

    Update more tests

--- spec/presenters/hyrax/taxonomy_presenter_spec.rb
+++ spec/presenters/hyrax/taxonomy_presenter_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Hyrax::TaxonomyPresenter do
 
-  let(:work) { Taxonomy.new(title: ["taxonomy title"]) }
+  let(:work) { Taxonomy.create(title: ["taxonomy title"]) }
   let(:taxonomy_terms) {[:taxonomy_domain, :taxonomy_kingdom, :taxonomy_phylum, :taxonomy_superclass, :taxonomy_class, :taxonomy_subclass, :taxonomy_superorder, :taxonomy_order, :taxonomy_suborder, :taxonomy_superfamily, :taxonomy_family, :taxonomy_subfamily, :taxonomy_tribe, :taxonomy_genus, :taxonomy_subgenus, :taxonomy_species, :taxonomy_subspecies, :trusted, :gbif_key]}
 
   subject { described_class.new(SolrDocument.new(work.to_solr), nil) }
@@ -16,13 +16,8 @@ RSpec.describe Hyrax::TaxonomyPresenter do
   end
 
   describe "#biological_specimens" do
-    let(:bso1) { BiologicalSpecimen.new(title: ["bso1 title"], vouchered: ["Yes"]) }
-    let(:bso2) { BiologicalSpecimen.new(title: ["bso2 title"], vouchered: ["No"]) }
-
-    before do
-      work.members << bso1 << bso2
-      work.save
-    end
+    let(:bso1) { BiologicalSpecimen.create(title: ["bso1 title"], vouchered: ["Yes"], taxonomy_id: [work.id]) }
+    let(:bso2) { BiologicalSpecimen.create(title: ["bso2 title"], vouchered: ["No"], taxonomy_id: [work.id]) }
 
     it "returns all children (which can only be biological specimens)" do
       expect(subject.biological_specimens).to match_array([bso1, bso2])

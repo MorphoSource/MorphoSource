b2674930f8a08dfac757902d11fe5ab0779c4b6e
Julie Winchester <julie.winchester@duke.edu>
2021-01-26 11:01:05 -0600

    Add new job for syncing work ordered members to members and vice-versa

---
+++ app/jobs/sync_work_ordered_members_job.rb
@@ -0,0 +1,15 @@
+class SyncWorkOrderedMembersJob < Hyrax::ApplicationJob
+
+  queue_as Hyrax.config.reindex_queue_name
+
+  def perform(object=nil)
+  	if object.present? && ( (m_len = object.members.to_a.length) != (om_len = object.ordered_members.to_a.length) )
+  		if om_len > m_len
+  			object.members = object.ordered_members
+  		elsif m_len > om_len
+  			object.ordered_members = object.members
+  		end
+  		object.save!
+  	end
+  end
+end

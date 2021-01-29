3f75a100912d2bc2397858bc04bd53f1f63f8931
Julie Winchester <julie.winchester@duke.edu>
2021-01-29 06:36:48 +0000

    Fix tests and exempt organization/collection reindexing related works from config flag

--- lib/morphosource/works/index_related_works.rb
+++ lib/morphosource/works/index_related_works.rb
@@ -3,22 +3,25 @@ module Morphosource
     module IndexRelatedWorks
 
       def index_related_works
-        return unless Hyrax.config.index_related_works
         case self
         when BiologicalSpecimen
+          return unless Hyrax.config.index_related_works
           if organization_id_changed?
             index_related(media)
           end
         when CulturalHeritageObject
+          return unless Hyrax.config.index_related_works
           if organization_id_changed?
             index_related(media)
           end
         when ImagingEvent
+          return unless Hyrax.config.index_related_works
           if ie_modality_changed?
             index_related(media)
             index_related(objects)
           end
         when Media
+          return unless Hyrax.config.index_related_works
           index_related(objects)
         when Organization
           if title_changed? || team_id_changed?
@@ -35,9 +38,11 @@ module Morphosource
             index_related_collections(team&.child_projects)
           end
         when ProcessingEvent
+          return unless Hyrax.config.index_related_works
           index_related(media)
           index_related(objects)
         when Taxonomy
+          return unless Hyrax.config.index_related_works
           index_related(objects)
           index_related(media)
         end

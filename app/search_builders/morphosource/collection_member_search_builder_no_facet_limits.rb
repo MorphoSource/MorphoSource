64140bc70341b0322d3449bfaf50183b88500234
JocelynTriplett <jocelyn.triplett@gmail.com>
2021-02-11 11:32:22 -0500

    downloaders can see private media in project

--- app/search_builders/morphosource/collection_member_search_builder_no_facet_limits.rb
+++ app/search_builders/morphosource/collection_member_search_builder_no_facet_limits.rb
@@ -5,5 +5,10 @@ module Morphosource
     def no_facet_limit(solr_parameters)
       solr_parameters["facet.limit"] = -1
     end
+
+    # override Hydra::AccessControlsEnforcement to include 'download'
+    def discovery_permissions
+      @discovery_permissions ||= ["edit","discover","download","read"]
+    end
   end
 end

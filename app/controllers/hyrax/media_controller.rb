ea2cbaef0bbdeedf856a588caa1db25d745299fa
Simon Choy <simon.choy@duke.edu>
2021-02-12 15:12:33 -0500

    re-index PO when media is deleted.  include direct IE

--- app/controllers/hyrax/media_controller.rb
+++ app/controllers/hyrax/media_controller.rb
@@ -21,52 +21,6 @@ module Hyrax
     before_action :save_fileset_visibility, only: [:update]
     before_action :set_fileset_visibility, only: [:create, :update]
 
-    def after_destroy(related_media, pe, ie)
-      if related_media.present?
-        UpdateRelatedWorksIndexJob.perform_later(related_media)
-      end
-      pe.delete if pe.present?
-      ie.delete if ie.present?
-      respond_to do |format|
-        format.js {render :js => "location.reload()"}
-        format.html do
-          flash[:notice] = 'Media deleted'
-          redirect_to '/dashboard/my/media'
-        end
-        format.json { head :no_content, location: '/dashboard/my/media' }
-      end
-    end
-
-    def after_destroy_error(id)
-      respond_to do |format|
-        format.html do
-          flash[:notice] = 'Error deleting media'
-          redirect_to '/concern/media/' + id + '/edit'
-        end
-        format.json { render json: { id: id }, status: :unprocessable_entity, location: media_showcase_path(curation_concern) }
-      end
-    end
-
-    def destroy
-      # delete the PE parent of that media 
-      # If the media has a direct or indirect IE parent and that IE parent has no other children, it should also be deleted
-      processing_event = curation_concern.parent_works.select { |w| w.class == ProcessingEvent }&.first
-      if processing_event.present?
-        imaging_event = processing_event.parent_works.select { |w| w.class == ImagingEvent }&.first
-        if imaging_event.present?
-          if imaging_event.descendants.select { |d| d.class == Media && d.id != curation_concern.id}.present?
-            imaging_event = nil
-          end
-        end
-      end
-      related_media = curation_concern.related_media
-      if curation_concern.destroy
-        after_destroy(related_media, processing_event, imaging_event)
-      else
-        after_destroy_error(curation_concern.id)
-      end
-    end
-
     # override the layout from WorksControllerBehavior
     def decide_layout
       layout = case action_name
@@ -208,6 +162,54 @@ module Hyrax
           end
           wants.json { render_json_response(response_type: :unprocessable_entity, options: { errors: curation_concern.errors }) }
         end
+      end
+    end
+
+    def after_destroy(works_to_index, works_to_delete)
+      UpdateRelatedWorksIndexJob.perform_later(works_to_index)
+      works_to_delete&.each do |w|
+        w.delete if w.present?
+      end
+      respond_to do |format|
+        format.js {render :js => "location.reload()"}
+        format.html do
+          flash[:notice] = 'Media deleted'
+          redirect_to '/dashboard/my/media'
+        end
+        format.json { head :no_content, location: '/dashboard/my/media' }
+      end
+    end
+
+    def after_destroy_error(id)
+      respond_to do |format|
+        format.html do
+          flash[:notice] = 'Error deleting media'
+          redirect_to '/concern/media/' + id + '/edit'
+        end
+        format.json { render json: { id: id }, status: :unprocessable_entity, location: media_showcase_path(curation_concern) }
+      end
+    end
+
+    def destroy
+      # delete the PE parent of that media 
+      # If the media has a direct or indirect IE parent and that IE parent has no other children, it should also be deleted
+      processing_event = curation_concern.parent_works.select { |w| w.class == ProcessingEvent }&.first
+      if processing_event.present?
+        imaging_event = processing_event.parent_works.select { |w| w.class == ImagingEvent }&.first
+      else
+        imaging_event = curation_concern.parent_works.select { |w| w.class == ImagingEvent }&.first
+      end
+      if imaging_event.present?
+        if imaging_event.descendants.select { |d| d.class == Media && d.id != curation_concern.id}.present?
+          imaging_event = nil
+        end
+      end
+      works_to_delete = [processing_event, imaging_event].compact
+      works_to_index = (curation_concern.related_media + curation_concern.physical_objects).compact
+      if curation_concern.destroy
+        after_destroy(works_to_index, works_to_delete)
+      else
+        after_destroy_error(curation_concern.id)
       end
     end
 
@@ -380,16 +382,6 @@ module Hyrax
           create_thumbnail
         end
       end
-
-
-
-    def index_objects
-  byebug
-      @objects_for_index&.each do |o|
-        o.update_index
-      end
-    end
-
 
   end
 end

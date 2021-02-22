3884be00a2e1fc2e817f3c5e7af40ce878043307
Simon Choy <simon.choy@duke.edu>
2021-02-22 15:50:26 -0500

    fixed error when adding proxy user

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -211,6 +211,7 @@ Hyrax.config do |config|
 
   # The user who runs batch jobs. Update this if you aren't using emails
   # config.batch_user_key = 'batchuser@example.com'
+  config.batch_user_key = '1' # user with key '1' (admin@email.com)
 
   # The user who runs fixity check jobs. Update this if you aren't using emails
   # config.audit_user_key = 'audituser@example.com'

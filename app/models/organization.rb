2383ed294538f0972c38270e22e18cf03f945aaa
Julie Winchester <julia.m.winchester@gmail.com>
2021-04-14 21:06:12 +0000

    Add organization model methods to return device-associated media/objects

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -54,6 +54,27 @@ class Organization < Morphosource::Works::Base
     outside_team(media)
   end
 
+  # Media associated with the organization via devices and imaging events
+  def device_media
+    Media.where(media_device_facility_organization_id_ssim: id)
+  end
+
+  def device_specimens
+    device_media.map(&:specimens).flatten.uniq { |s| s.id }
+  end
+
+  def device_cultural_heritage_objects
+    device_media.map(&:cultural_heritage_objects).flatten.uniq { |s| s.id }
+  end
+
+  def device_physical_objects
+    device_specimens + device_cultural_heritage_objects
+  end
+
+  def devices
+    members.select { |m| m.device? }
+  end
+
   def team
     return nil if team_id.empty?
 

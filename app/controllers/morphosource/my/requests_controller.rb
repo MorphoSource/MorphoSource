f28b2b8fdb5d77412655b3fd30d494d311b4562c
Simon Choy <simon.choy@duke.edu>
2021-04-21 20:44:51 -0400

    handle multiple reviewers

--- app/controllers/morphosource/my/requests_controller.rb
+++ app/controllers/morphosource/my/requests_controller.rb
@@ -41,27 +41,30 @@ module Morphosource
       def send_batch_request_messages(items)
         requestor = current_user
         # send message for each reviewer
-        reviewers = []
+        items_reviewers = []
         reviewer_items = {}
         items.each do |item|
           work = Media.find(item.work_id)
           if work.present?
-            reviewer_id = work.reviewer.first
-            reviewers << reviewer_id
+            reviewer_id = work.reviewer # this could be more than 1 reviewers
+            items_reviewers << reviewer_id
             reviewer_items[reviewer_id] = item # store the item for sending details if single item
           end
         end
-        reviewer_counts = reviewers.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
+        reviewer_counts = items_reviewers.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
         reviewer_counts.each do |reviewer_id, count|
-          reviewer = User.where(ms_id: reviewer_id).first
-          if reviewer.present?
+          reviewers = User.where(ms_id: reviewer_id)
+          if reviewers.present?
             if count == 1
               send_request_message(reviewer_items[reviewer_id])
             else
               message_to_reviewer = "<a href='mailto:#{requestor.email}'>#{requestor.name_or_email}</a> has requested to download " + count.to_s + " media.  Please review this request in your <a href='http://#{host_name}/dashboard/my/request_manager'>Manage Requests</a> dashboard."
-              deliver(email_sender, reviewer, message_to_reviewer, "You have download request to review")
+              if reviewers.count > 1
+                message_to_reviewer += "<p>This request has been sent to the media's reviewers #{user_email_link(reviewers)} who are all able to approve, deny, or clear this request.  Please coordinate your response if appropriate.</p>"
+              end              
+              deliver(email_sender, reviewers, message_to_reviewer, "You have download request to review")
 
-              message_to_requestor = "You have sent a download request to <a href='mailto:#{reviewer.email}'>#{reviewer.name_or_email}</a> for downloading " + count.to_s + " media.  You can view pending requests in your <a href='http://#{host_name}/dashboard/my/requests'>My Requests</a> dashboard."
+              message_to_requestor = "You have sent a download request to #{user_email_link(reviewers)} for downloading " + count.to_s + " media.  You can view pending requests in your <a href='http://#{host_name}/dashboard/my/requests'>My Requests</a> dashboard."
               deliver(email_sender, requestor, message_to_requestor, "You have sent a download request")
             end
           end
@@ -72,12 +75,15 @@ module Morphosource
         work = Media.find(item.work_id)
         if work.present?
           requestor = current_user
-          reviewer = User.where(ms_id:work.reviewer.first).first
-          if reviewer.present?
+          reviewers = User.where(ms_id: work.reviewer)
+          if reviewers.present?
             message_to_reviewer = "<a href='mailto:#{requestor.email}'>#{requestor.name_or_email}</a> has requested to download " + cart_item_message_content(item, work) + "Please review this request in your <a href='http://#{host_name}/dashboard/my/request_manager'>Manage Requests</a> dashboard."
-            deliver(email_sender, reviewer, message_to_reviewer, "You have a download request to review")
+            if work.reviewer.count > 1
+              message_to_reviewer += "<p>This request has been sent to the media's reviewers #{user_email_link(reviewers)} who are all able to approve, deny, or clear this request.  Please coordinate your response if appropriate.</p>"
+            end
+            deliver(email_sender, reviewers, message_to_reviewer, "You have a download request to review")
 
-            message_to_requestor = "You have sent a download request to <a href='mailto:#{reviewer.email}'>#{reviewer.name_or_email}</a> for downloading " + cart_item_message_content(item, work) + "You can view pending requests in your <a href='http://#{host_name}/dashboard/my/requests'>My Requests</a> dashboard."
+            message_to_requestor = "You have sent a download request to #{user_email_link(reviewers)} for downloading " + cart_item_message_content(item, work) + "You can view pending requests in your <a href='http://#{host_name}/dashboard/my/requests'>My Requests</a> dashboard."
             deliver(email_sender, requestor, message_to_requestor, "You have sent a download request")
           end
         end
--- lib/morphosource/cart_items/request_messages.rb
+++ lib/morphosource/cart_items/request_messages.rb
@@ -11,26 +11,32 @@ module Morphosource
         @email_sender ||= User.where(email: 'morphosource@duke.edu')&.first
       end
 
+      def user_email_link(users)
+        list = []
+        users.each do |user|
+          list << "<a href='mailto:#{user.email}'>#{user.name_or_email}</a>"
+        end
+        return list.to_sentence.html_safe
+      end
+
       def cart_item_message_content(item, work)
-        return @cart_item_message_content if @cart_item_message_content.present?
-        content = "the media <b><a href='http://#{host_name}/media/#{work.id}'>#{work.title.first}</a></b>"
+        content = "the <b><a href='http://#{host_name}/media/#{work.id}'>Media #{work.id}: #{work.title.first}</a></b>"
         object = work.objects&.first
         if object.present?
           if object.specimen?
-            content += " of " + work.physical_object_type.downcase + " <b><a href='http://#{host_name}/biological_specimens/#{object.id}'>#{object.title.first}</a>" + "</b>" + " (" + object.taxonomies_titles&.first + ")"
+            content += " of " + work.physical_object_type.downcase + " <b><a href='http://#{host_name}/biological_specimens/#{object.id}'>#{object.title.first}</a>" + "</b>" + " (<i>" + object.taxonomies_titles&.first + "</i>)"
           else
             content += " of " + work.physical_object_type.downcase + 
               " <b><a href='http://#{host_name}/cultural_heritage_objects/#{object.id}'>#{object.title.first}</a>" + "</b>" 
           end
         end
         content += " for intended use: <i>\"" + item.use + "\"</i>" if item.use.present?
-        @cart_item_message_content = content.html_safe + ".  "
-        return @cart_item_message_content
+        return content.html_safe + ".  "
       end
 
-      def deliver(sender, recipient, message, subject)
+      def deliver(sender, recipients, message, subject)
         begin
-          Hyrax::MessengerService.deliver(sender, recipient, message, subject)
+          Hyrax::MessengerService.deliver(sender, recipients, message, subject)
           # arguments passed to messenger_service: (sender, recipients, body, subject, *args)
         rescue => e
           Rails.logger.debug "Error sending message. Exception: #{ e.message }"

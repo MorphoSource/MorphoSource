b9f0c400926ad12a8a03792ed89274eb37f99605
Julie Winchester <julie.winchester@duke.edu>
2021-04-27 12:57:27 -0500

    iDigBioSearchService and PhysicalObjectSearchService limit to 100 rows by default, add count of results and max message to submission PO search

--- lib/morphosource/i_dig_bio.rb
+++ lib/morphosource/i_dig_bio.rb
@@ -8,7 +8,7 @@ module Morphosource
     API_ENDPOINT = 'https://search.idigbio.org/v2'
     ::RestClient.log = Rails.logger
 
-    def self.search(search_query, limit = 500, fields = nil)
+    def self.search(search_query, limit = 100, fields = nil)
       response = RestClient.post "#{API_ENDPOINT}/search/records/", {rq: search_query, limit: limit}.to_json, {content_type: :json, accept: :json}
       JSON.parse(response.body)['items'] if response
     end

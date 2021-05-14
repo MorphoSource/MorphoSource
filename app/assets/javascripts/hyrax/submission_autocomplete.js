3b9a2a11f11eb694fd76a237929af54f2e45c78c
Julie Winchester <julie.winchester@duke.edu>
2021-05-14 13:14:47 -0500

    Hotfix: Increase submission jQuery autocomplete timeout to from 10s to 30s

--- app/assets/javascripts/hyrax/submission_autocomplete.js
+++ app/assets/javascripts/hyrax/submission_autocomplete.js
@@ -19,7 +19,7 @@
       source: function (request, response) {
         $.ajax({
           url: url + request.term,
-          timeout: 10000,
+          timeout: 30000,
           type: 'GET',
           dataType: 'json',
           complete: function (xhr, status) {

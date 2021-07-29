1437fea7017075b90be5bf17e19714e9cf1822d6
Simon Choy <simon.choy@duke.edu>
2021-07-29 19:08:33 -0400

    allow admin to edit other user profile

--- config/routes.rb
+++ config/routes.rb
@@ -412,6 +412,8 @@ Rails.application.routes.draw do
   get 'dashboard/profiles/:id/edit_password' => 'morphosource/dashboard/profiles#edit_password', as: 'profile_edit_password'
   patch 'dashboard/profiles/:id/update_password' => 'morphosource/dashboard/profiles#update_password', as: 'profile_update_password'
 
+  get 'dashboard/profiles/:id/edit' => 'morphosource/dashboard/profiles#edit', as: 'profile_edit'
+
   # Routes for fund code charge API
   get 'fund_code_charges', to: 'fund_code_charges#index', as: 'fund_code_charges'
   get 'fund_code_charges.csv', to: 'fund_code_charges#index', as: 'fund_code_charges_csv', defaults: { format: 'csv' }

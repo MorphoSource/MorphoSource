3d321fea536a8d53cb90628ca5707432531b5c99
Simon Choy <simon.choy@duke.edu>
2021-07-29 16:38:52 -0400

    new sftp_share field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_194630) do
+ActiveRecord::Schema.define(version: 2021_07_29_201021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -670,6 +670,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_194630) do
     t.string "ms1_password_hash"
     t.boolean "ms1_user", default: false
     t.string "token"
+    t.string "sftp_share"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["ms_id"], name: "index_users_on_ms_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

1c7c0dfa2b845e78a2a1b433b676df612c678abc
Julie Winchester <julia.m.winchester@gmail.com>
2021-09-01 17:26:15 +0000

    Disable submission debug mode

--- app/assets/javascripts/submission.js
+++ app/assets/javascripts/submission.js
@@ -1538,7 +1538,7 @@ $( document ).ready(function() {
         this.setMediaPermissionFieldEvent();
 
         // Comment out for debug ability to access any step any time
-        // this.setSidebarViewFade([2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        this.setSidebarViewFade([2, 3, 4, 5, 6, 7, 8, 9, 10]);
       }
 
       setMediaPermissionFieldEvent() {

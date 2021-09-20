f2c39f780ee517f93209a2544dd54b17ae91a43c
JocelynTriplett <jocelyn.triplett@gmail.com>
2021-09-20 17:48:04 -0400

    Refactor solr searches

--- app/search_builders/morphosource/collections/media_search_builder.rb
+++ app/search_builders/morphosource/collections/media_search_builder.rb
@@ -2,6 +2,8 @@ module Morphosource
   module Collections
     class MediaSearchBuilder < Hyrax::CollectionMemberSearchBuilder
 
+      self.default_processor_chain += [:return_selected_fields]
+
       def member_of_collection(solr_parameters)
         solr_parameters[:fq] ||= []
         if collection.organization.present?
@@ -9,6 +11,10 @@ module Morphosource
         else
           solr_parameters[:fq] << "(#{collection_membership_field}:(#{collection_ids.join(' OR ')}))"
         end
+      end
+
+      def return_selected_fields(solr_parameters)
+        solr_parameters[:fl] = 'id,has_model_ssim,title_tesim,thumbnail_path_ss,part_tesim,physical_object_title_ssim,taxonomy_ssim,human_readable_media_type_tesim,date_uploaded_dtsi,fileset_accessibility_ssim'
       end
 
       def models

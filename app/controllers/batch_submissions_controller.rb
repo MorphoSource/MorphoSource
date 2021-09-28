8de5ccfacd7d343abe4f8e815e5cfdaa8f7646b2
Simon Choy <simon.choy@duke.edu>
2021-09-28 19:48:57 -0400

    parse file using roo gem

--- app/controllers/batch_submissions_controller.rb
+++ app/controllers/batch_submissions_controller.rb
@@ -1,4 +1,4 @@
-require 'creek'
+require 'roo'
 
 class BatchSubmissionsController < ApplicationController
   load_and_authorize_resource 
@@ -114,34 +114,125 @@ class BatchSubmissionsController < ApplicationController
 
   def parse_manifest(file)
     # column 1 can be ignored
-    # field names is on row 6
-    # field values start from row 7, column 2
-    creek = Creek::Book.new file.tempfile.path, with_headers: true
+    # field names is on row 7
+    # field values start from row 8, column 3    
     error_rows = {}
     error_messages = {}
-    sheet = creek.sheets[0]
-    data_rows = sheet.simple_rows.to_a.shift(6) # remove non-data rows
-    data_rows.each_with_index do |row, index|
-      if row["media.media_file"].nil?
-        error_rows[index+1] = row
-        error_messages[index+1] = "media.media_file is missing."
+    xlsx = Roo::Excelx.new(file.tempfile.path)    
+    row_index = 8
+    xlsx.each_row_streaming(offset: 7, pad_cells: true) do |row| 
+      data_row = row.drop(2) 
+      data_row.each_with_index do |cell, cell_index|
+        error_msg = error_found(field_names[cell_index], cell.value)
+        if error_msg.present?
+          error_messages[row_index] = error_msg
+          error_rows[row_index] = data_row.map { |c| c.value }
+        end
       end
-    end
-    render 'result', locals: { rows: error_rows, messages: error_messages }
-    #redirect_to main_app.batch_submissions_result_path     
-
+      row_index = row_index + 1
+    end # /xlsx.each_row_streaming
+    render 'result', locals: { rows: error_rows, messages: error_messages, field_names: field_names }
   end
 
+  def error_found(name, val)
+    error_msg = ""
+    case name
+    when "media.media_file"
+      if val.nil?
+        error_msg = "media.media_file is missing."
+      end
 
-
-
-
-
-
-
-
-
-
-
+    end
+    return error_msg
+  end
+  
+  def field_names
+    @field_names ||= 
+    ["media.media_file",
+    "media.preview_file",
+    "media.publication_status",
+    "media.media_type",
+    "media.parent_file",
+    "media.parent_ms_id",
+    "biological_specimen.ms_id",
+    "biological_specimen.idigbio_uuid",
+    "biological_specimen.occurrence_id",
+    "biological_specimen.institution_code",
+    "biological_specimen.collection_code",
+    "biological_specimen.catalog_number",
+    "media.part",
+    "media.short_description",
+    "media.side",
+    "media.description",
+    "media.creator",
+    "media.orientation",
+    "media.identifier",
+    "media.keyword",
+    "media.date_created",
+    "media.related_url",
+    "media.x_spacing",
+    "media.y_spacing",
+    "media.z_spacing",
+    "media.slice_thickness",
+    "media.series_type",
+    "media.unit",
+    "media.map_type",
+    "biological_specimen.identifier",
+    "biological_specimen.related_url",
+    "biological_specimen.date_created",
+    "biological_specimen.creator",
+    "biological_specimen.description",
+    "biological_specimen.latitude",
+    "biological_specimen.longitude",
+    "biological_specimen.numeric_time",
+    "biological_specimen.original_location",
+    "biological_specimen.periodic_time",
+    "biological_specimen.is_type_specimen",
+    "biological_specimen.sex",
+    "biological_specimen.vouchered",
+    "taxonomy.taxonomy_genus",
+    "taxonomy.taxonomy_species",
+    "taxonomy.taxonomy_subspecies",
+    "imaging_event.description",
+    "imaging_event.creator",
+    "imaging_event.software",
+    "imaging_event.date_created",
+    "imaging_event.ct.exposure_time",
+    "imaging_event.ct.flux_normalization",
+    "imaging_event.ct.geometric_calibration",
+    "imaging_event.ct.shading_correction",
+    "imaging_event.ct.filter_material",
+    "imaging_event.ct.filter_thickness",
+    "imaging_event.ct.frame_averaging",
+    "imaging_event.ct.projections",
+    "imaging_event.ct.voltage",
+    "imaging_event.ct.power",
+    "imaging_event.ct.amperage",
+    "imaging_event.ct.surrounding_material",
+    "imaging_event.ct.xray_tube_type",
+    "imaging_event.ct.target_type",
+    "imaging_event.ct.detector_type",
+    "imaging_event.ct.detector_pixels_x",
+    "imaging_event.ct.detector_pixel_size_x",
+    "imaging_event.ct.detector_pixels_y",
+    "imaging_event.ct.detector_pixel_size_y",
+    "imaging_event.ct.detector_configuration",
+    "imaging_event.ct.source_object_distance",
+    "imaging_event.ct.source_detector_distance",
+    "imaging_event.ct.target_material",
+    "imaging_event.ct.rotation_number",
+    "imaging_event.ct.phase_contrast",
+    "imaging_event.ct.optical_magnification",
+    "imaging_event.ct.acquisition_type",
+    "imaging_event.photogrammetry.focal_length_type",
+    "imaging_event.photogrammetry.background_removal",
+    "imaging_event.photography.lens_make",
+    "imaging_event.photography.lens_model",
+    "imaging_event.photography.light_source",
+    "processing_event.creator",
+    "processing_event.date_created",
+    "processing_event.software",
+    "processing_event.description"]
+  end
 
 end

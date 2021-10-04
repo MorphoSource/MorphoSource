cd03751ba954f2c5eb71d26b8c33f1cbd79558bb
JocelynTriplett <jocelyn.triplett@gmail.com>
2021-10-04 18:01:46 -0400

    remove duplicated call to collection_media

--- app/controllers/morphosource/collections/biological_specimens_controller.rb
+++ app/controllers/morphosource/collections/biological_specimens_controller.rb
@@ -20,11 +20,6 @@ module Morphosource
       end
       configure_facets
 
-      def show
-        @media_count, @object_ids = collection_media
-        super
-      end
-
       private
 
         def query_collection_counts

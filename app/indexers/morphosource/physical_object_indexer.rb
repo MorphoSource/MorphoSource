458f94084f05336056c2a81ca49ac0616c9b5a0b
JocelynTriplett <jocelyn.triplett@gmail.com>
2021-11-03 10:09:00 -0400

    reject blank values and flatten; fix tests

--- app/indexers/morphosource/physical_object_indexer.rb
+++ app/indexers/morphosource/physical_object_indexer.rb
@@ -52,11 +52,12 @@ module Morphosource
     end
 
     def media_collection_ids
-      @media_collection_ids ||= media.map(&:member_of_collection_ids).compact.uniq
+      @media_collection_ids ||= media.map(&:member_of_collection_ids).reject(&:blank?).flatten.uniq
     end
 
     def media_collection_ids_of_type(type)
       return [] if media_collection_ids.empty?
+
       qry = "(id:(#{media_collection_ids.join(' OR ')}) AND human_readable_type_tesim:#{type})"
       collections = ActiveFedora::SolrService.query(qry, rows: 999999)
       collections.map(&:id)

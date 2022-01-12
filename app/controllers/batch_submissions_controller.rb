da99a24fbe636bfbb839b23e486e4b648bdf4704
Simon Choy <simon.choy@duke.edu>
2022-01-12 16:41:11 -0500

    added Tin filter

--- app/controllers/batch_submissions_controller.rb
+++ app/controllers/batch_submissions_controller.rb
@@ -579,7 +579,7 @@ byebug
   end
 
   def valid_imaging_event_ct_filter_material
-    @valid_imaging_event_ct_filter_material ||= ['Molybdenum', 'Aluminum', 'Copper', 'Rhodium', 'Niobium', 'Europium', 'Lead']
+    @valid_imaging_event_ct_filter_material ||= ['Molybdenum', 'Aluminum', 'Copper', 'Rhodium', 'Niobium', 'Europium', 'Lead', 'Tin']
   end
 
   def valid_imaging_event_ct_target_type

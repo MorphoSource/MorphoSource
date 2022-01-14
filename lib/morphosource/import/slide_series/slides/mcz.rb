a89da9192696796d7017c6d8b12f03627781fb75
JocelynTriplett <jocelyn.triplett@gmail.com>
2022-01-14 15:18:59 -0500

    Add slide characterization

---
+++ lib/morphosource/import/slide_series/slides/mcz.rb
@@ -0,0 +1,153 @@
+module Morphosource
+  module Import
+    module SlideSeries
+      module Slides
+        module Mcz
+
+          # define organization/specific values here
+          # overrides defaults defined in Morphosource::Import::SlideSeries::Slides::Slide
+
+          # bits_per_sample
+          def bits_per_sample
+            ["8 8 8"]
+          end
+
+          # color_space
+          def color_space
+            ["YCbCr"]
+          end
+
+          # compression
+          def compression
+            ["JPEG"]
+          end
+
+          # file.create_date
+          # date_created
+          def date_created
+            @file_json["created"]
+          end
+
+          # date_modified
+          # ["2018:12:17 17:03:34"]
+          def date_modified
+            @item_json["updated"]
+          end
+
+          # file_name
+          def file_name
+            @file_json["name"]
+          end
+
+          def fileset_accessibility
+            ["open"]
+          end
+
+          # height
+          def height
+            @tiles_json["sizeY"].to_s
+          end
+
+          # identifier
+          #["5c454d3c70aaa9064404a300"]
+          def identifier
+            [@slide_json["http://rs.tdwg.org/ac/terms/accessURI"][/\/item\/(.*?)\/tiles\//,1]]
+          end
+
+          # license
+          #["http://creativecommons.org/licences/by-nc-sa/3.0/"]
+          def license
+            [@slide_json["http://ns.adobe.com/xap/1.0/rights/WebStatement"]]
+          end
+
+          def file_magnification
+            @tiles_json["magnification"]
+          end
+
+          # mime_type
+          def mime_type
+            @file_json["mimeType"]
+          end
+
+          # pixel_spacing
+          def pixel_spacing
+            [@tiles_json["mm_x"].to_s + '\\' + @tiles_json["mm_y"].to_s]
+          end
+
+          # publisher
+          #["Museum of Comparative Zoology, Harvard University"]
+          def publisher
+            [@slide_json["http://rs.tdwg.org/ac/terms/metadataProviderLiteral"]]
+          end
+
+          # related_url
+          #["http://mczbase.mcz.harvard.edu/media/1468742", "https://images.slide-atlas.org/#item/5c454d3c70aaa9064404a300"]
+          def related_url
+            [@slide_json["http://purl.org/dc/terms/identifier"], slide_atlas_url]
+          end
+
+          # rights_holder
+          #["Museum of Comparative Zoology, Harvard University"]
+          def rights_holder
+            [@slide_json["http://ns.adobe.com/xap/1.0/rights/Owner"]]
+          end
+
+          # file_size
+          def file_size
+            [@file_json["size"].to_s]
+          end
+
+          def scanning_software
+            scan_date = Date.parse(date_created)
+            if scan_date < Date.parse('2018-04-24')
+              ["MACROscan 1.26"]
+            elsif scan_date < Date.parse('2018-08-01')
+              ["MACROscan 1.28"]
+            elsif scan_date < Date.parse('2019-02-16')
+              ["MACROscan 1.31"]
+            else
+              ["MACROscan 1.32"]
+            end
+          end
+
+          # short_description
+          #["HEC-1009 Slide A"]
+          def short_description
+            [@slide_json["http://purl.org/dc/terms/description"]]
+          end
+
+          # title
+          #["HEC-1009 Slide A [Image]"]
+          def title
+            [@slide_json["http://purl.org/dc/terms/description"] + " [Image]"]
+          end
+
+          # unit
+          def unit
+            ["Mm"]
+          end
+
+          def visibility
+            'open'
+          end
+
+          # width
+          def width
+            @tiles_json["sizeX"].to_s
+          end
+
+          # x_spacing
+          def x_spacing
+            [@tiles_json["mm_x"].to_s]
+          end
+
+          # y_spacing
+          def y_spacing
+            [@tiles_json["mm_y"].to_s]
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ lib/morphosource/import/slide_series/slides/mcz_slide.rb
@@ -0,0 +1,51 @@
+module Morphosource
+  module Import
+    module SlideSeries
+      module Slides
+        class MczSlide < Morphosource::Import::SlideSeries::Slides::Slide
+
+          include Morphosource::Import::SlideSeries::Slides::Mcz
+
+          #"https://images.slide-atlas.org/api/v1/item/5c454d3c70aaa9064404a300/tiles/thumbnail
+          def slide_thumbnail_path
+            @slide_thumbnail_path ||= @slide_json["http://rs.tdwg.org/ac/terms/accessURI"].split("?").first
+          end
+
+          private
+
+            def gather_metadata
+              @import_url = import_url
+              @item_json = json(@import_url)
+              @slide_thumbnail_path = slide_thumbnail_path
+              @file_json = json(file_uri)
+              @tiles_json = json(tiles_uri)
+            end
+
+            #"https://images.slide-atlas.org/api/v1/item/5c454d3c70aaa9064404a300"
+            def import_url
+              @import_url = @slide_json["http://rs.tdwg.org/ac/terms/accessURI"].split.present? ?  @slide_json["http://rs.tdwg.org/ac/terms/accessURI"].split("/tiles/", 2).first : nil
+            end
+
+            def file_uri
+              "https://images.slide-atlas.org/api/v1/file/#{image_file_id}"
+            end
+
+            def tiles_uri
+              @import_url + "/tiles"
+            end
+
+            # file_id
+            def image_file_id
+              @item_json["largeImage"]["fileId"]
+            end
+
+            #"https://images.slide-atlas.org/#item/5c454d3c70aaa9064404a300"
+            def slide_atlas_url
+              @import_url.gsub('api/v1/','#')
+            end
+
+        end
+      end
+    end
+  end
+end

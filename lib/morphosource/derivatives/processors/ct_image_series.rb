6fd3ddada3b2e7586b9a08959e524d681ba0f03d
JocelynTriplett <jocelyn.triplett@gmail.com>
2022-02-08 09:57:18 -0500

    add micrometers as valid unit

--- lib/morphosource/derivatives/processors/ct_image_series.rb
+++ lib/morphosource/derivatives/processors/ct_image_series.rb
@@ -2,18 +2,18 @@ require 'fileutils'
 require 'zip'
 
 module Morphosource::Derivatives::Processors
-	class TimeoutError < Hydra::Derivatives::TimeoutError
-  	end
+  class TimeoutError < Hydra::Derivatives::TimeoutError
+  end
 
-	class CTImageSeries < Hydra::Derivatives::Processors::Processor
-		attr_accessor :tmp_dir_path, :img_coll, :ext
+  class CTImageSeries < Hydra::Derivatives::Processors::Processor
+    attr_accessor :tmp_dir_path, :img_coll, :ext
     attr_accessor :input_path, :scaled_path, :raw_dcm_path, :output_path, :manifest_path
     attr_accessor :raw_dcm_file_path, :output_file_path
     attr_accessor :x, :y, :z, :linear_scale_factor
     attr_accessor :slice_thickness, :unit, :x_spacing, :y_spacing, :z_spacing
     attr_accessor :derivatives_tmp_path
 
-		class_attribute :timeout
+    class_attribute :timeout
 
     def dicom_image_formats
       ['.dcm', '.dicom', '.ima', '']
@@ -23,33 +23,33 @@ module Morphosource::Derivatives::Processors
       dicom_image_formats + ['.tiff', '.tif', '.bmp', '.png', '.jpeg', '.jpg']
     end
 
-		def process
-			timeout ? process_with_timeout : create_ct_image_series_derivative
-		end
+    def process
+      timeout ? process_with_timeout : create_ct_image_series_derivative
+    end
 
-		def process_with_timeout
-			Timeout.timeout(timeout) { create_ct_image_series_derivative }
-		rescue Timeout::Error
-			raise Morphosource::Derivatives::Processors::TimeoutError, "Unable to process CT Image Series derivative\nThe command took longer than #{timeout} seconds to execute"
-		end
+    def process_with_timeout
+      Timeout.timeout(timeout) { create_ct_image_series_derivative }
+    rescue Timeout::Error
+      raise Morphosource::Derivatives::Processors::TimeoutError, "Unable to process CT Image Series derivative\nThe command took longer than #{timeout} seconds to execute"
+    end
 
-		protected
+    protected
 
-    	def create_ct_image_series_derivative
-    		@tmp_dir_path = Rails.root.join(derivatives_tmp_path, SecureRandom.uuid)
+      def create_ct_image_series_derivative
+        @tmp_dir_path = Rails.root.join(derivatives_tmp_path, SecureRandom.uuid)
         Dir.mkdir tmp_dir_path unless File.exist? tmp_dir_path
-    		@input_path = File.join(tmp_dir_path, 'input')
+        @input_path = File.join(tmp_dir_path, 'input')
         Dir.mkdir input_path unless File.exist? input_path
         @scaled_path = File.join(tmp_dir_path, 'scaled')
         Dir.mkdir scaled_path unless File.exist? scaled_path
-    		@raw_dcm_path = File.join(tmp_dir_path, 'raw_dcm')
+        @raw_dcm_path = File.join(tmp_dir_path, 'raw_dcm')
         Dir.mkdir raw_dcm_path unless File.exist? raw_dcm_path
         @raw_dcm_file_path = File.join(raw_dcm_path, 'derivative.dcm')
-    		@output_path = File.join(tmp_dir_path, 'output')
+        @output_path = File.join(tmp_dir_path, 'output')
         Dir.mkdir output_path unless File.exist? output_path
         @output_file_path = File.join(output_path, 'derivative.dcm')
 
-        
+
         @x_spacing = (directives.fetch(:x_spacing, 1).presence || 1).to_f
         @y_spacing = (directives.fetch(:y_spacing, 1).presence || 1).to_f
         @z_spacing = (directives.fetch(:z_spacing, 0).presence || 0).to_f
@@ -68,13 +68,12 @@ module Morphosource::Derivatives::Processors
         # If unit is not Mm, must convert spacing values to Mm
         @unit = directives.fetch(:unit, 'Mm').presence || 'Mm'
         correct_spacing_scale if unit != 'Mm'
-
         begin
           locate_images
           if !img_coll
             return
-          end 
-          
+          end
+
           # extract and process images
           extract_images
           uncompress_dcm if dicom_image_formats.include?(ext)
@@ -82,24 +81,28 @@ module Morphosource::Derivatives::Processors
           scale_images
           tif_to_raw_dcm
           compress_dcm
-          
-      		# place files
-      		write_files
+
+          # place files
+          write_files
         rescue StandardError => e
           raise e
         ensure
-    		  cleanup_tmp_files
+          cleanup_tmp_files
         end
-    	end
+      end
 
     def derivatives_tmp_path
       @derivatives_tmp_path = Hyrax.config.derivatives_tmp_path
     end
 
     def correct_spacing_scale
-      unit_factors = { 'Cm' => 10.0, 'M' => 1000.0, 'Km' => 1e6, 'In' => 25.4, 'Ft' => 304.8, 'Mi' => 1.609e+6 }
+      unit_factors = { 'Cm' => 10.0, 'M' => 1000.0, 'Km' => 1e6, 'In' => 25.4, 'Ft' => 304.8, 'Mi' => 1.609e+6, 'Um' => 0.001 }
       uf = unit_factors[unit]
-      [x_spacing, y_spacing, z_spacing, slice_thickness].each { |var| var = var * uf }
+
+      @x_spacing*= uf
+      @y_spacing*= uf
+      @z_spacing*= uf
+      @slice_thickness*= uf
     end
 
     def locate_images
@@ -116,7 +119,7 @@ module Morphosource::Derivatives::Processors
             end
             if !img_locs[ext].key?(loc)
               img_locs[ext][loc] = []
-            end 
+            end
             img_locs[ext][loc] << f.name
           end
         end
@@ -232,10 +235,10 @@ module Morphosource::Derivatives::Processors
       ( (vf**3) / (x.to_f * y.to_f * z.to_f) )**( 1.0/3.0 )
     end
 
-		def scale_images
+    def scale_images
       fiji = Morphosource::Derivatives::Fiji.new(input_path, scaled_path, linear_scale_factor)
       fiji.call
-		end
+    end
 
     def tif_to_raw_dcm
       new_x, new_y, new_z, new_slice_thickness = new_spacing
@@ -248,10 +251,10 @@ module Morphosource::Derivatives::Processors
       new_x = ( x.to_f * x_spacing.to_f ) / new_dim(x)
       new_y = ( y.to_f * y_spacing.to_f ) / new_dim(y)
 
-      # depth 
+      # depth
       z_total = ( z_spacing.to_f * (z.to_f - 1.0) ) + slice_thickness.to_f
       new_slice_thickness = ( slice_thickness.to_f * z.to_f) / new_dim(z)
-      new_z = ( z_total  - new_slice_thickness ) / ( new_dim(z) - 1 ) 
+      new_z = ( z_total  - new_slice_thickness ) / ( new_dim(z) - 1 )
       new_z = 0.0 if new_z < 0.0
 
       return new_x, new_y, new_z, new_slice_thickness
@@ -265,13 +268,13 @@ module Morphosource::Derivatives::Processors
       dcmcjpeg = Morphosource::Derivatives::Dcmcjpeg.new(raw_dcm_path, output_path)
       dcmcjpeg.call
     end
-    
+
     def write_files
       output_file_service.call(output_file_path, directives)
     end
 
-		def cleanup_tmp_files
-			FileUtils.remove_dir tmp_dir_path
-		end
-	end
+    def cleanup_tmp_files
+      FileUtils.remove_dir tmp_dir_path
+    end
+  end
 end
--- spec/lib/morphosource/derivatives/processors/ct_image_series_spec.rb
+++ spec/lib/morphosource/derivatives/processors/ct_image_series_spec.rb
@@ -1,42 +1,66 @@
 require 'rails_helper'
 
 describe Morphosource::Derivatives::Processors::CTImageSeries do
-	subject { described_class.new(file_name, directives) }
+  subject { described_class.new(file_name, directives) }
 
-	# let(:file_name) { File.join(fixture_path, 'bunny/bunny.ply') }
-	let(:file_name) { 'file_name' }
-	# let(:source_path) { File.join(fixture_path, 'bunny/bunny.ply') }
-	let(:directives) { { label: :dcm, format: 'dcm' } }
+  # let(:file_name) { File.join(fixture_path, 'bunny/bunny.ply') }
+  let(:file_name) { 'file_name' }
+  # let(:source_path) { File.join(fixture_path, 'bunny/bunny.ply') }
+  let(:directives) { { label: :dcm, format: 'dcm' } }
 
-	describe "#process" do
-		context "when a timeout is set" do
-			before do
-				subject.timeout = 0.1
-				allow(subject).to receive(:create_ct_image_series_derivative) { sleep 0.2 }
-			end
+  describe "#process" do
+    context "when a timeout is set" do
+      before do
+        subject.timeout = 0.1
+        allow(subject).to receive(:create_ct_image_series_derivative) { sleep 0.2 }
+      end
 
-			xit "raises a TimeoutError" do
-				expect { subject.process }.to raise_error Morphosource::Derivatives::Processors::TimeoutError
-			end
-		end
+      xit "raises a TimeoutError" do
+        expect { subject.process }.to raise_error Morphosource::Derivatives::Processors::TimeoutError
+      end
+    end
 
-		context "when a timeout is not set" do
-			before { subject.timeout = nil }
+    context "when a timeout is not set" do
+      before { subject.timeout = nil }
 
-			xit "processes without a timeout" do
-				expect(subject).to receive(:process_with_timeout).never
-				expect(subject).to receive(:create_ct_image_series_derivative).once
-				subject.process
-			end
-		end
+      xit "processes without a timeout" do
+        expect(subject).to receive(:process_with_timeout).never
+        expect(subject).to receive(:create_ct_image_series_derivative).once
+        subject.process
+      end
+    end
 
-		context "when running the complete commmand" do
-			let(:file_name) { File.join(fixture_path, 'dcm_stack.zip') }
+    context "when running the complete commmand" do
+      let(:file_name) { File.join(fixture_path, 'dcm_stack.zip') }
 
-			xit "produces the derivative dcm" do
-				expect(Hyrax::PersistDerivatives).to receive(:call).with(kind_of(String), directives)
-				subject.process
-			end
-		end
-	end
+      xit "produces the derivative dcm" do
+        expect(Hyrax::PersistDerivatives).to receive(:call).with(kind_of(String), directives)
+        subject.process
+      end
+    end
+  end
+
+  describe 'correct_spacing_scale' do
+    let(:file_name) { File.join(fixture_path, 'dcm_stack.zip') }
+
+    before do
+      subject.instance_variable_set(:@x_spacing, 1)
+      subject.instance_variable_set(:@y_spacing, 10)
+      subject.instance_variable_set(:@z_spacing, 20)
+      subject.instance_variable_set(:@slice_thickness, 30)
+    end
+    context 'unit is micrometers' do
+      before do
+        subject.instance_variable_set(:@unit, 'Um')
+      end
+      it 'returns the correct values' do
+        subject.send(:correct_spacing_scale)
+        expect(subject.x_spacing).to eq(0.001)
+        expect(subject.y_spacing).to eq(0.01)
+        expect(subject.z_spacing).to eq(0.02)
+        expect(subject.slice_thickness).to eq(0.03)
+      end
+    end
+
+  end
 end

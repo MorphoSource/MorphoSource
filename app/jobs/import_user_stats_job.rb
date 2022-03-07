80be4c5dd0d402b1fbca016eee6da7f887e26d11
Julie Winchester <julie.winchester@duke.edu>
2022-03-07 11:38:41 -0600

    Update UserStatImporter to raise failures as errors and not store UserStat from current date

--- app/jobs/import_user_stats_job.rb
+++ app/jobs/import_user_stats_job.rb
@@ -1,8 +1,8 @@
 class ImportUserStatsJob < Hyrax::ApplicationJob
   queue_as Hyrax.config.ingest_queue_name
 
-  def perform
-    importer = Morphosource::UserStatImporter.new(verbose: true, logging: true)
+  def perform(number_of_retries = 0)
+    importer = Morphosource::UserStatImporter.new(verbose: true, logging: true, number_of_retries: number_of_retries)
     importer.import
   end
 end

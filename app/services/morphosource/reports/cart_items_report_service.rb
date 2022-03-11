4f33e5049d9dfbe2fe60f018d2942d2a8f8530d6
Julie Winchester <julie.winchester@duke.edu>
2022-03-11 16:35:04 -0600

    Add unique download users and category counts to collection CSV exports

--- app/services/morphosource/reports/cart_items_report_service.rb
+++ app/services/morphosource/reports/cart_items_report_service.rb
@@ -36,7 +36,12 @@ module Morphosource
 			def format_results
 				results_for_format = attributes.present? ? results.select(attributes) : results
 				results_for_format.map do |cart_item|
-					transform_for_report(cart_item.attributes.except('id'))
+					transformed_item = transform_for_report(cart_item.attributes.except('id'))
+					if attributes.include? :user_id
+						transformed_item.merge('download_user_id' => cart_item[:user_id])
+					else
+						transformed_item
+					end
 				end
 			end
 

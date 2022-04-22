0e591e878a9ac9d172dadc6ae7ac9b4eb2703adb
Simon Choy <simon.choy@duke.edu>
2022-04-22 12:36:32 -0400

    modify include_current_value method for Side dropdown

--- app/services/morphosource/sides_service.rb
+++ app/services/morphosource/sides_service.rb
@@ -7,16 +7,24 @@ module Morphosource
     end
 
     def active?(id)
-      begin
+      if authority.find(id).key?('active')
         authority.find(id).fetch('active')
-      rescue KeyError  
-        Rails.logger.debug "Value #{id} not found in SidesService"
+      else
         return false
       end
     end
 
-
     def include_current_value(value, _index, render_options, html_options)
+byebug
+      value = value.capitalize
+      unless value.blank? || active?(value)
+        html_options[:class] << ' force-select'
+        render_options += [[label(value) { value }, value]]
+      end
+      [render_options, html_options]
+    end
+
+    def OLD_include_current_value(value, _index, render_options, html_options)
       unless value.blank? || active?(value)
         html_options[:class] << ' force-select'
         render_options += [[label(value) { value }, value]]

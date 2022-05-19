759ca12ec52333594a02e9da6d692984092fcd97
Simon Choy <simon.choy@duke.edu>
2022-05-19 13:29:16 -0400

    modal message edits, added tests

--- spec/controllers/morphosource/i_dig_bio_search_controller_spec.rb
+++ spec/controllers/morphosource/i_dig_bio_search_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Morphosource::IDigBioSearchController, type: :controller do
 
+  describe '#search_idigbio_by_occurrence_id_ajax' do
+
+		let(:params) { {:oid => 'MCZ:Mamm:44858'} }
+
+		before do
+			allow(controller).to receive(:params).and_return(params)
+		end
+
+    it 'returns JSON response' do
+      get :search_idigbio_by_occurrence_id_ajax, params: params
+      expect(JSON.parse(response.body)).to include_json(
+        occurrence_id: params[:oid]
+      )
+    end
+
+	end
+
 end

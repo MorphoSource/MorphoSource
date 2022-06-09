c6eab1cc28de359ab5100b7efabe525e2a610933
Simon Choy <simon.choy@duke.edu>
2022-06-08 22:27:48 -0400

    render project id in class attribute

--- app/assets/javascripts/hyrax/ms_helpers.js
+++ app/assets/javascripts/hyrax/ms_helpers.js
@@ -7,6 +7,13 @@ Array.prototype.uniq = function(){
   );
 }
 
+// subtract self with array a
+Array.prototype.diff = function(a) {
+  return this.filter(
+    function(i) {return a.indexOf(i) < 0;}
+  );
+};
+
 function pluralize(noun, count) {
   if (count != 1) 
     suffix = 's' 

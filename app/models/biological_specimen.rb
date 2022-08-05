4cf9ddf3e7c5968bb0efab1bb2de83b912a7175c
Julie Winchester <julie.winchester@duke.edu>
2022-08-05 17:18:12 -0500

    Refactor specimen iDigBio Update, by default only updates if MS diff from iDigBio

--- app/models/biological_specimen.rb
+++ app/models/biological_specimen.rb
@@ -2,6 +2,7 @@ class BiologicalSpecimen < Morphosource::Works::Base
 
   include ::Hyrax::WorkBehavior
   include Morphosource::PhysicalObjectBehavior
+  include Morphosource::BiologicalSpecimeniDigBioUpdateBehavior
   validates_with Morphosource::ParentChildValidator
   before_create :controlled_value_filter, :date_filter, :set_idigbio_link_origin_when_create
   before_update do
@@ -109,132 +110,7 @@ class BiologicalSpecimen < Morphosource::Works::Base
     return ((idigbio_occurrence_id_results[:status] == :success) && (idigbio_occurrence_id_results[:data].length > 0))
   end
 
-  def update_metadata_from_idigbio_occurrence_id(save_work=false, system_update=false)
-    if idigbio_match_found?
-      idigbio_occurrence = idigbio_occurrence_id_results[:data].first
-      taxonomy_params_array = []
-      taxonomy_id_array = []
-      taxonomy_gbif_key_array = []
-      canonical_taxonomy_id = nil
-      sex_field_values = Morphosource::SexFieldService.new().option_values
-
-      idb_taxonomy_param_sets = Morphosource::IDigBioSearchService.taxonomy_param_sets_from_idigbio(idigbio_occurrence['uuid'])
-      provider_params = idb_taxonomy_param_sets[:provider]
-      gbif_params = idb_taxonomy_param_sets[:gbif]
-
-      if provider_params.present?
-        prov = Morphosource::TaxonomySearchService.match_taxonomies_strict(provider_params)
-        if prov.present?
-          # Exists, link as canonical
-          canonical_taxonomy_id = prov.first.id
-          taxonomy_id_array << prov.first.id
-        else
-          # Is new, must create
-          provider_params[:canonical] = true # to be hooked in later to set canonical taxonomy ID
-          taxonomy_params_array << ActionController::Parameters.new(provider_params)
-        end
-      end
-
-      if gbif_params.present?
-        gbif = Morphosource::TaxonomySearchService.call({ gbif_key: gbif_params['gbif_key'] })
-        if gbif.present?
-          taxonomy_id_array << gbif.first.id
-        else
-          taxonomy_params_array << ActionController::Parameters.new(gbif_params)
-        end
-      end
-
-      # add new taxonomy if any
-      taxonomy_params_array.each do |taxon_params|
-        new_taxon_id = prepare_and_create_taxonomy(taxon_params)
-        taxonomy_id_array << new_taxon_id
-        if taxon_params[:canonical]
-          canonical_taxonomy_id = new_taxon_id
-        end
-      end
-
-      # now link taxonomy (new or existing) to the bso
-      if taxonomy_id_array.present?
-        old_taxonomy_id = self.taxonomy_id.to_a
-        self.taxonomy_id = (self.taxonomy_id + taxonomy_id_array).uniq
-      end
-      if canonical_taxonomy_id.present?
-        old_canonical_taxonomy = self.canonical_taxonomy.to_a
-        self.canonical_taxonomy_will_change! unless old_canonical_taxonomy.include? canonical_taxonomy_id
-        self.canonical_taxonomy = (self.canonical_taxonomy << canonical_taxonomy_id).uniq
-      end
-
-      if self.taxonomy_id_changed?
-        Rails.logger.debug "UpdateBsoFromIdigbio: BSO #{id} : taxonomy_id #{old_taxonomy_id} will be updated to '#{self.taxonomy_id.to_a}'"
-      end
-      if self.canonical_taxonomy_changed?
-        Rails.logger.debug "UpdateBsoFromIdigbio: BSO #{id} : canonical_taxonomy #{old_canonical_taxonomy} will be updated to '#{self.canonical_taxonomy.to_a}'"
-      end
-
-      # sync bso metadata
-      biospec_model_params = Morphosource::IDigBioSearchService.biological_specimen_params_from_idigbio(idigbio_occurrence['uuid'])
-      biospec_model_params.each do |key, value|
-        if key == "sex"
-          unless sex_field_values.include? value.capitalize
-            # do not update the value from iDigBio unless it matches MS control list
-            next
-          end
-        end
-        self.send("#{key}=", value.is_a?(Array) ? value : [value] )
-        field_changed = self.send("#{key}_changed?")
-        if field_changed
-          Rails.logger.debug "UpdateBsoFromIdigbio: BSO #{id} : #{key} field will be updated to '#{value}'"
-        end
-      end
-      if self.idigbio_uuid_changed?
-        if system_update == true
-          self.idigbio_link_origin = ["system_generated"]
-        else
-          self.idigbio_link_origin = ["user"]
-        end
-      end
-      self.title = [generated_title]
-      # normally saving work is done separately (e.g. in a background job, form submit)
-      # set save_work flag if needed for debugging in the console
-      self.save if save_work
-    end
-  end
-
-
   private
-
-    def generated_title
-      inst = self.institution_code&.first.presence || ''
-      coll = self.collection_code&.first.presence || ''
-      cnum = self.catalog_number&.first.presence || ''
-      case
-      when inst.present? || coll.present? || cnum.present?
-        collection_catalog_generated_title(inst, coll, cnum)
-      when self.identifier.present?
-        identifier_generated_title(self.identifier)
-      else
-        fallback_generated_title(self.vouchered, ::User.find_by_user_key(self.depositor))
-      end
-    end
-
-    def collection_catalog_generated_title(institution_code='', collection_code='', catalog_number='')
-      [institution_code, collection_code, catalog_number].keep_if { |x| x.presence } .join(':')
-    end
-
-    def identifier_generated_title(identifier)
-      identifier.sort.join(', ')
-    end
-
-    def fallback_generated_title(vouchered, user)
-      if vouchered.present? && vouchered.first == 'Yes'
-        voucher_term = 'Vouchered'
-      else
-        voucher_term = 'Unvouchered'
-      end
-      user_term = user.display_name.present? ? user.display_name : user.email
-      I18n.t('morphosource.fallback_object_title', voucher: voucher_term, user: user_term)
-    end
-
     def add_id_to_title # this is non-functional!!
       unless self.title && self.id && self.title.first.to_s.start_with?("S#{self.id.to_s}: ")
         self.title.set("S#{self.id.to_s}: #{self.title.first.to_s}")

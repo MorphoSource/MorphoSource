38aae6f767aa1570614de43281f1101cae773f7a
Simon Choy <simon.choy@duke.edu>
2022-08-05 13:40:17 -0400

    bug fix in UpdateWorkAccessGroupsJob

--- app/jobs/update_work_access_groups_job.rb
+++ app/jobs/update_work_access_groups_job.rb
@@ -2,8 +2,8 @@ class UpdateWorkAccessGroupsJob < Hyrax::ApplicationJob
   queue_as Hyrax.config.update_fast_queue_name
 
   def perform(work, read_groups=nil, edit_groups=nil)
-    work.read_groups = read_groups if read_groups.present?
-    work.edit_groups = edit_groups if edit_groups.present?
+    work.read_groups = read_groups unless read_groups.nil?
+    work.edit_groups = edit_groups unless edit_groups.nil?
     work.save!
   end
 end

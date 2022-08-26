c222117116da6e5b070ba9f6d1318df9d3fd10b6
Simon Choy <simon.choy@duke.edu>
2022-08-26 18:48:28 -0400

    move imaging events to target specimen

--- app/services/morphosource/merge_bso_service.rb
+++ app/services/morphosource/merge_bso_service.rb
@@ -2,20 +2,22 @@ module Morphosource
   class MergeBsoService
 
     def self.call(merge_to=nil, merge_from=nil)
-
       bso_to = BiologicalSpecimen.find(merge_to)
       bso_from = BiologicalSpecimen.find(merge_from)
-byebug
-
-
+	  ie_list = []
+	  media_list = []
 	  bso_from.media.each do |m|
-
         # detach media's IE, add IE under target bso, reindex media and related media
-        ie = m.ImagingEvent
+        media_list << m.id
+        ie = m.imaging_event
+        ie_list << ie.id
+		#puts "moving IE #{ie.id} from bso #{ie.physical_object_id&.first} to bso #{bso_to.id}"
         ie.physical_object_id = [bso_to.id]
-
+        ie.save
 	  end
-
+	  puts "moved media #{media_list}, IE #{ie_list} from bso #{bso_from.id} to bso #{bso_to.id}"
+	  return nil
     end
+
   end
 end

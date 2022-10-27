6cd299e4488ee9e0ac55dc2ad7a16bf675f54d7b
Julie Winchester <julie.winchester@duke.edu>
2022-10-27 16:52:45 -0500

    Users must confirm new accounts and email changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ApplicationRecord
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   MULTI_VALUE_FIELDS = {

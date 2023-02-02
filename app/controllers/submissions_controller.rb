c3ad235240f0a1a003ef5bc95ddbacdda46529ab
Simon Choy <simon.choy@duke.edu>
2023-02-02 12:12:42 -0500

    added RestClient timeout

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -146,7 +146,7 @@ class SubmissionsController < ApplicationController
       http_code = ""
     else
       begin
-        head = RestClient.head(params[:u])
+        head = RestClient::Request.execute(method: :head, url: params[:u], timeout: 15)
         http_code = head.code
         file_ext = file_extension_from_content_type(head.headers[:content_type])
         status = "success"

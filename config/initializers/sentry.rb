e4c3f7cc46c8223f2ea316b950179b5551c743d7
Julie Winchester <julie.winchester@duke.edu>
2023-02-13 17:09:52 -0600

    Change sentry environment based on SENTRY_ENV env var

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,8 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
+  config.environment = ENV['SENTRY_ENV'] || Rails.env
+
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.

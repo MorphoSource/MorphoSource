69b961b98909fc4592da33fcd389d05a1d56c8cf
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2023-02-20 16:12:37 -0500

    format links without http:

--- app/renderers/hyrax/renderers/showcase_external_link_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/showcase_external_link_attribute_renderer.rb
@@ -5,10 +5,18 @@ module Hyrax
 
         def attribute_value_to_html(value)
           markup = ''
-          return markup if value.blank? 
-          link = link_to(value, value, target: :blank)
+          return markup if value.blank?
+          link = link_to(value, format_url(value), target: :blank)
           markup = "<span class='glyphicon glyphicon-new-window'></span>&nbsp;<span class='showcase-link'>#{link}</span>"
           markup.html_safe
+        end
+
+        def format_url(value)
+          unless value[/\Ahttp:\/\//] || value[/\Ahttps:\/\//]
+            "http://#{value}"
+          else
+            value
+          end
         end
     end
   end

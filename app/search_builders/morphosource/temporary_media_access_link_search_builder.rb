f5dd0ca3a6e3309145873fa0a68dd0f3ee687db4
Julie Winchester <julie.winchester@duke.edu>
2023-02-27 14:05:57 -0600

    Code clean-up

--- app/search_builders/morphosource/temporary_media_access_link_search_builder.rb
+++
@@ -1,6 +0,0 @@
-module Morphosource
-  class TemporaryMediaAccessLinkSearchBuilder < Hyrax::SearchBuilder
-    include Hyrax::SingleResult
-    self.default_processor_chain = [:find_one]
-  end
-end

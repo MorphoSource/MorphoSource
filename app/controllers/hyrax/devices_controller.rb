d7f39b7fe3422971dd3b2f1b24dca332e2bd326f
Julie Winchester <julie.winchester@duke.edu>
2023-03-29 14:52:42 -0500

    Bugfix: Fix broken new device form after sidebar update

--- app/controllers/hyrax/devices_controller.rb
+++ app/controllers/hyrax/devices_controller.rb
@@ -8,6 +8,7 @@ module Hyrax
     include Hyrax::BreadcrumbsForWorks
     include Hyrax::ChildWorkRedirect
     self.curation_concern_type = ::Device
+    with_themed_layout 'morphosource_1_column'
 
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::DevicePresenter

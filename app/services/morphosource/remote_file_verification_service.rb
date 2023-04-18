13849a3db1633dea9bc912d94a295990d1a92089
Simon Choy <simon.choy@duke.edu>
2023-04-18 12:24:59 -0400

    report when content_length or file_size not available

--- app/services/morphosource/remote_file_verification_service.rb
+++ app/services/morphosource/remote_file_verification_service.rb
@@ -10,25 +10,31 @@ module Morphosource
     end
 
     def call
-      media = @media
-      if !media.remote_origin_url.present?
-        return nil
+      return nil unless @media.remote_origin_url.present?
+      rfi = MorphosourceHelper::RemoteFileInfo.new(@media.remote_origin_url)
+      return rfi if rfi.status != "success" # there is an error already (e.g. 404)
+      messages = []
+      if !rfi.last_modified.present?
+        messages << "last_modified not in request"        
       else
-        rfi = MorphosourceHelper::RemoteFileInfo.new(media.remote_origin_url)
-        if rfi.last_modified.present?
-          
-          #
-           
-        end
-        if (content_length = rfi.content_length).present? && 
-            (file_size = media.file_sets&.first&.file_size&.first).present?
-          byebug
-          if content_length != file_size
-            rfi.message = "content_length #{rfi.content_length} does not match existing file_size "
-          end
-        end
-        return rfi
+
+        #
+         
       end
+      if !(content_length = rfi.content_length).present? 
+        messages << "content_length not in request"
+      end
+      if !(file_size = @media.file_sets&.first&.file_size&.first).present?
+        messages << "existing file_size not available"
+      end
+      if content_length.present? && file_size.present? && content_length != file_size
+        messages << "content_length #{rfi.content_length} does not match existing file_size #{file_size}"
+      end
+
+      return nil if messages.empty?
+
+      rfi.message = messages.join('; ')
+      return rfi
     end
 
   end

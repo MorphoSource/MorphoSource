b4feb1158c4ed8e86b0d3f5272a297bd84b95127
Simon Choy <simon.choy@duke.edu>
2023-04-26 21:02:41 -0400

    revert to not check for path_hint existence

--- app/jobs/prepare_create_derivatives_job.rb
+++ app/jobs/prepare_create_derivatives_job.rb
@@ -15,10 +15,6 @@ class PrepareCreateDerivativesJob < Hyrax::ApplicationJob
 
     wrapper = JobIoWrapper.find_by(file_set_id: work.id)
     path_hint = wrapper.uploaded_file ? wrapper.uploaded_file.uploader.path : wrapper.path
-    if File.exists?(path_hint)
-      CreateDerivativesJob.perform_later(work, work.original_file.id, path_hint)
-    else
-      raise 'PrepareCreateDerivativesJob: Temp file does not exist'
-    end
+    CreateDerivativesJob.perform_later(work, work.original_file.id, path_hint)
   end
 end

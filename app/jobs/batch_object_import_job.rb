6fcdaa589d75fa37b74e779a6faccfb7ffa66e65
Simon Choy <simon.choy@duke.edu>
2023-05-01 14:57:30 -0400

    catch exceptions and retry for bso and taxonomy jobs

--- app/jobs/batch_object_import_job.rb
+++ app/jobs/batch_object_import_job.rb
@@ -3,7 +3,7 @@ class BatchObjectImportJob < Morphosource::ApplicationJobWithStatus
   queue_as Hyrax.config.mass_ingest_queue_name
 
   def perform(model, attributes, files_directory, update = false)
-    object = Importer::BatchObjectImporter.call(model, attributes, files_directory, update)
+    object = BatchSubmissionsImporter::BatchObjectImporter.call(model, attributes, files_directory, update)
     status.update(id: object.id)
   end
 

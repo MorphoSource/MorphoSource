f808857ccc8a74a71b390f197a91d63018463781
Simon Choy <simon.choy@duke.edu>
2023-05-18 16:07:46 -0400

    re-derive remote file

--- app/jobs/prepare_characterize_job.rb
+++ app/jobs/prepare_characterize_job.rb
@@ -17,16 +17,11 @@ class PrepareCharacterizeJob < Hyrax::ApplicationJob
     path_hint = wrapper.uploaded_file ? wrapper.uploaded_file.uploader.path : wrapper.path
 
     if work.is_remote_backed?
-byebug
-# check path_hint
       if path_hint && File.exist?(path_hint)      
-        byebug
         CharacterizeJob.perform_later(work, work.original_file.id, path_hint)
       else
         user = User.find_by_user_key(work.depositor)
-        operation =  Hyrax::Operation.create!(user: user,
-                                   operation_type: "Attach Remote File")
-        byebug
+        operation =  Hyrax::Operation.create!(user: user, operation_type: "Attach Remote File")
         ImportUrlJob.perform_later(work, operation, {})
         # CharacterizeJob & CreateDerivativesJob will be called later in file_actor after ImportUrlJob
       end

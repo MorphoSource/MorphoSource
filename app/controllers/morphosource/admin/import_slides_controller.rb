e69dd912abfd67d83c70ffb2ddca8730ef29dc6c
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2023-07-12 09:38:39 -0500

    Refactor slide import service to use provider profile

--- app/controllers/morphosource/admin/import_slides_controller.rb
+++ app/controllers/morphosource/admin/import_slides_controller.rb
@@ -4,29 +4,15 @@ module Morphosource
       before_action :require_admin
       with_themed_layout 'morphosource_dashboard'
 
-      def index
-      end
-
       def import_slides
-        organization_id = params["organization"]
-        service = params["service"]
-        resource_id = params["resource_id"]
-        user_email = params["user_email"]
-        list_visibility = params["list_visibility"]
-        @collection = import_service_class.new(service: service, resource_id:resource_id, user_email: user_email).call
+        occurrence_id = params["occurrence_id"]
+        @collection = Morphosource::Import::Slides::SlideSeriesService.call(occurrence_id)
 
         # this will be refactored w/ providers yml pr
         if import_error?
           redirect_to admin_import_slides_path, flash: { error: @error_message }
         else
           redirect_to sequential_section_list_path(@collection)
-        end
-      end
-
-      def import_service_class
-        case params["organization"]
-        when "MCZ"
-          Morphosource::Import::MczSlideSeriesService
         end
       end
 

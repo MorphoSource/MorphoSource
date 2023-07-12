eebd417c1ececd2a1cc899911b1cc451ec33af3a
Simon Choy <simon.choy@duke.edu>
2023-07-12 10:17:27 -0400

    remove else block for handling non archived mesh files

--- lib/morphosource/derivatives/processors/mesh.rb
+++ lib/morphosource/derivatives/processors/mesh.rb
@@ -47,8 +47,6 @@ module Morphosource::Derivatives::Processors
           extract_mesh_zip
         elsif File.extname(source_path).downcase == '.tar'
           extract_mesh_tar
-        else
-          raise "Archive file extension not valid"
         end
         create_tmp_nondraco_glb
         create_tmp_draco_glb

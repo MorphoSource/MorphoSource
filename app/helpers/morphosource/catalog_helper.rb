233565aeb65612d62cfa8211211b35d907706f6e
Julie Winchester <julie.winchester@duke.edu>
2023-07-27 16:15:21 -0500

    Add catalog controller for media lists

--- app/helpers/morphosource/catalog_helper.rb
+++ app/helpers/morphosource/catalog_helper.rb
@@ -1,24 +1,5 @@
 module Morphosource::CatalogHelper
 
-  # used by the ms_catalog_refine_search_form
-  # sets the search path to whatever the current work type is
-  def catalog_search_path
-    case controller_name
-    when "media_catalog"
-      main_app.media_search_path
-    when "organizations_catalog"
-      main_app.organization_search_path
-    when "objects_catalog"
-      main_app.object_search_path
-    when "collections_catalog"
-      main_app.collection_search_path
-    when "all_catalog"
-      main_app.all_search_path
-    else
-      search_form_action
-    end
-  end
-
   # media index metadata displays title linked to physical object
   def link_to_object(args)
     id = args[:document]["physical_object_id_tesim"].first

45fb09212763f4f3e344ca47a40a37e410c0175f
Julie Winchester <julie.winchester@duke.edu>
2023-08-25 09:40:31 -0500

    Optimize user name solr migration

--- db/migrate/20230824170251_solr_update_user_with_ownership_depositor_name_email.rb
+++ db/migrate/20230824170251_solr_update_user_with_ownership_depositor_name_email.rb
@@ -1,11 +1,12 @@
 class SolrUpdateUserWithOwnershipDepositorNameEmail < ActiveRecord::Migration[5.2]
   def up
+    users = User.where(guest: false).map { |u| [u.user_key, u] }.to_h
     media_docs = ActiveFedora::SolrService.query("has_model_ssim:Media", rows: 999_999)
     media_docs.each do |doc|
       new_doc = doc.to_h
 
       # Data manager name and email
-      user_with_ownership_user = User.find_by_user_key(doc["user_with_ownership_ssi"])
+      user_with_ownership_user = users[doc["user_with_ownership_ssi"]]
       user_with_ownership_name = user_with_ownership_user&.name || "Unknown User"
       new_doc['user_with_ownership_name_tesim'] = user_with_ownership_name
       new_doc['user_with_ownership_name_ssim'] = user_with_ownership_name
@@ -14,7 +15,7 @@ class SolrUpdateUserWithOwnershipDepositorNameEmail < ActiveRecord::Migration[5.
       new_doc['user_with_ownership_email_ssim'] = user_with_ownership_email
 
       # Depositor name and email
-      depositor_user = User.find_by_user_key(doc["depositor_ssim"]&.first)
+      depositor_user = users[doc["depositor_ssim"]&.first]
       depositor_name = depositor_user&.name || "Unknown User"
       depositor_email = depositor_user&.email
       new_doc['depositor_name_tesim'] = depositor_name
@@ -28,12 +29,13 @@ class SolrUpdateUserWithOwnershipDepositorNameEmail < ActiveRecord::Migration[5.
   end
 
   def down
+    users = User.where(guest: false).map { |u| [u.user_key, u] }.to_h
     media_docs = ActiveFedora::SolrService.query("has_model_ssim:Media", rows: 999_999)
     media_docs.each do |doc|
       new_doc = doc.to_h
 
       # Data manager name and email
-      user_with_ownership_user = User.find_by_user_key(doc["user_with_ownership_ssi"])
+      user_with_ownership_user = users[doc["user_with_ownership_ssi"]]
       user_with_ownership_name = user_with_ownership_user&.name_and_email
       new_doc['user_with_ownership_name_tesim'] = user_with_ownership_name
       new_doc['user_with_ownership_name_ssim'] = user_with_ownership_name
@@ -41,7 +43,7 @@ class SolrUpdateUserWithOwnershipDepositorNameEmail < ActiveRecord::Migration[5.
       new_doc['user_with_ownership_email_ssim'] = nil
 
       # Depositor name and email
-      depositor_user = User.find_by_user_key(doc["depositor_ssim"]&.first)
+      depositor_user = users[doc["depositor_ssim"]&.first]
       depositor_name = depositor_user&.name_and_email
       new_doc['depositor_name_tesim'] = depositor_name
       new_doc['depositor_name_ssim'] = depositor_name

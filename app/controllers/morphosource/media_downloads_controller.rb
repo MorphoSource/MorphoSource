286386caac3f0470a7b60c9d1deb68b181bdbc87
Simon Choy <simon.choy@duke.edu>
2023-08-30 16:44:16 -0400

    use methods from media_downloads_controller.rb

--- app/controllers/morphosource/media_downloads_controller.rb
+++ app/controllers/morphosource/media_downloads_controller.rb
@@ -15,17 +15,11 @@ RemoteInclusion = Struct.new(:url, :bytesize)
 module Morphosource
   class MediaDownloadsController < ApplicationController
     include Morphosource::CartItems
-    include Morphosource::RestApiBehavior
 
-    skip_before_action :verify_authenticity_token, only: [:api_generate_download, :download_from_api]
-    before_action :validate_when_download_from_api, only: [:download_from_api]
-    before_action :validate_params, only: [:show, :download_from_api]
-    before_action :validate_download_hash, only: [:show, :download_from_api]
+    before_action :validate_params, only: [:show]
+    before_action :validate_download_hash, only: [:show]
     before_action :validate_user, only: [:show]
     after_action :reset_recaptcha, only: [:show]
-
-    before_action :authenticate_api_key_required, only: [:api_generate_download]
-    before_action :validate_params_for_api, only: [:api_generate_download]
 
     def show
       prepare_all_files
@@ -39,88 +33,9 @@ module Morphosource
       end
     end
 
-    def download_from_api
-      prepare_all_files
-      if @files.present? && @all_files.present?
-        add_subsequent_download(cart_item_for_download_from_api, "API")
-        create_interval_sequence
-        send_interval_response
-      else
-        return render_json_by_http_code 400, ["There is an issue with one of the media you have attempted to download, and it is not available right now. Please try again later. If the issue persists, contact us (morphosource@duke.edu)."]
-      end
-    end
-
-    def use_statement
-      @use_statement ||= request.params[:use_statement]&.strip
-    end
-
-    def use_categories
-      @use_categories ||= request.params[:use_categories]
-    end
-
-    def use_categories_final
-      @use_categories_final ||= [use_categories, use_category_other].compact.flatten.reject(&:empty?)
-    end
-
-    def use_category_other
-      @use_category_other ||= request.params[:use_category_other]&.strip
-    end
-
-    def agreements_accepted?
-      request.params[:agreements_accepted] == true
-    end
-
-    def api_generate_download
-      @is_api_generate_download = true
-      cart_item = create_or_update_cart_item_for_link_generation
-      if cart_item.nil?
-        render_json_by_http_code 404
-      else
-        query_params = {
-          key: media.access_control_id,
-          download: download_hash,
-          usage: use_statement,
-          usage_list: use_categories_final.join(';')
-        }
-        download_url = url_for(controller: 'media_downloads', action: 'download_from_api', id: media.id, protocol: request.protocol, host: request.host_with_port, params: query_params)
-        json_obj = {            
-          "response": {
-            "media": {
-              "id": [media.id],
-              "download_url": [URI.encode(download_url)]
-            }
-          }
-        }
-        respond_to do |format|
-          format.json { render json: JSON.generate(json_obj)}
-        end
-      end
-    end
-
     def prepare_all_files
       @temp_files = []
       @all_files ||= files + standard_agreement_files + media_agreement_files + xlsx_manifest + csv_manifest
-    end
-
-    def cart_item_for_download_from_api
-      @cart_item_for_download_from_api ||= find_item_for_download_from_api(media.first.id, download_hash, current_user.ms_id)
-    end
-
-    def create_or_update_cart_item_for_link_generation
-      return nil unless (m = media_for_api).present?
-
-      if (item = find_undownloaded_approved_request_item(m.id)).present?
-        # Undownloaded approved request exists, associate download with request
-        add_link_generation(item, download_hash)
-      elsif (item = find_undownloaded_downloadable_item(m.id)).present?
-        # Undownloaded downloadable item exists (e.g., in media cart), associated download
-        add_link_generation(item, download_hash)
-      elsif user_can_download?(current_user, m)
-        item = create_cart_item_for_api(m)
-      else
-        item = nil
-      end
-      return item
     end
 
     def create_or_update_cart_items_for_download
@@ -186,44 +101,7 @@ module Morphosource
     end
 
     private
-
-      def validate_params_for_api
-        return render_json_by_http_code 401 unless user_from_authorization_header.present? 
-        return render_json_by_http_code 404 unless media_for_api.present? 
-
-        # the rest are validation failure
-        errors = [] 
-        unless use_statement.present? && use_statement.length >= 50
-          errors << "use_statement with minimum 50 characters is required" 
-        end 
-        if use_categories.present? 
-          unless use_categories.all? { |c| intended_use = Morphosource::UserProfile::CheckboxValues::INTENT.include?(c) }
-            errors << "one or more values of use_categories are not valid"
-          end
-        elsif !use_categories_final.present?
-          errors << "use_categories or use_category_other is required"
-        end
-        unless agreements_accepted?
-          errors << "agreements_accepted is not set to true"
-        end
-        if errors.present?
-          return render_json_by_http_code 400, errors
-        end
-      end
-
-      # Get user record from api key in header
-      def user_from_authorization_header
-        User.where(token: api_key).first if api_key.present?
-      end
-
-      def media_for_api
-        return nil unless Media.exists? (params[:id])
-        @media ||= Media.find(params[:id])
-      end
-
-      def api_key
-        @api_key ||= request.headers['Authorization']
-      end
+      # Validation methods
 
       def validate_params
         return head(:bad_request) unless params_valid?
@@ -235,13 +113,7 @@ module Morphosource
 
       # Get user record from token param
       def user_from_token
-        @user ||= begin
-          if @is_download_from_api
-            user_from_authorization_header
-          else
-            User.where(token: params[:token])&.first if params[:token].present?
-          end
-        end
+        @user ||= User.where(token: params[:token])&.first if params[:token].present?
       end
 
       # Get Media from keys
@@ -255,25 +127,12 @@ module Morphosource
       end
 
       def download_hash
-        @download_hash ||= begin
-          if @is_api_generate_download
-            SecureRandom.uuid
-          else
-            params[:download] if params[:download].present?
-          end
-        end
+        @download_hash ||= params[:download] if params[:download].present?
       end
 
       def validate_download_hash
         uuid_regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
         return head(:bad_request) unless uuid_regex.match?(download_hash.to_s.downcase)
-      end
-
-      def validate_when_download_from_api
-        @is_download_from_api = true
-        @current_user = user_from_token
-        return head(:unauthorized) unless user_is_valid?
-        return head(:bad_request) unless cart_item_for_download_from_api.present?
       end
 
       def validate_user

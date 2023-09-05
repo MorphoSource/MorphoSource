49f5ba5720ec70ea3a9ba907bc5ea142a6ceecd9
Simon Choy <simon.choy@duke.edu>
2023-09-05 14:52:51 -0400

    fixed indentation

--- app/jobs/update_bso_from_idigbio_job.rb
+++ app/jobs/update_bso_from_idigbio_job.rb
@@ -3,8 +3,8 @@ class UpdateBsoFromIdigbioJob < Hyrax::ApplicationJob
   queue_as Hyrax.config.update_medium_queue_name
 
   def perform(o, save_work=false, system_update=false, log_file)
-		log = log_file.present?? Logger.new(log_file) : Logger.new(STDOUT) 
-		o.update_metadata_from_idigbio_occurrence_id(false, system_update, false, log_file)
-		o.save if save_work
+    log = log_file.present?? Logger.new(log_file) : Logger.new(STDOUT) 
+    o.update_metadata_from_idigbio_occurrence_id(false, system_update, false, log_file)
+    o.save if save_work
   end
 end

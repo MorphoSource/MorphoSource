23786b2d94999940aa2893c2386154b88c707d79
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2023-09-07 15:21:52 -0400

    disable jquery sorting of page rows

--- app/assets/javascripts/morphosource/data_tables/collections_table.js
+++ app/assets/javascripts/morphosource/data_tables/collections_table.js
@@ -8,11 +8,11 @@ var createCollectionsTable = function(selector) {
   },
   columnDefs: [
     { orderable: false, targets: 0 }, // disable sorting
-    { responsivePriority: 1, targets: 0 },
-    { orderable: false, targets: 1 }, // disable sorting
-    { responsivePriority: 2, targets: 1 },
-    { responsivePriority: 3, targets: 2 },
-    { orderable: false, targets: -1 } // disable sorting,
+    { orderable: false, targets: 1 },
+    { orderable: false, targets: 2 }, // disable sorting
+    { orderable: false, targets: 3 },
+    { orderable: false, targets: 4 },
+    { orderable: false, targets: -1 } // disable sorting
   ],
   pageLength: 10,
   bPaginate: false,
@@ -22,23 +22,3 @@ var createCollectionsTable = function(selector) {
   bFilter: false // hide search box
   })
 };
-
-$(document).ready(function() {
-
-  if ($('body.dashboard.collections').length) { // check if the page is dashboard media works
-
-    // Toggle the visibility of table column
-    $('.choose-columns-collections .toggle-vis').on( 'click', function (e) {
-      //e.preventDefault();
-      var column = collectionsTable.column( $(this).attr('data-column') );
-      column.visible( ! column.visible() );
-    });
-
-    // keep dropdown menu open
-    $(document).on('click', '.choose-columns .dropdown-menu', function (e) {
-      e.stopPropagation();
-    });
-
-  } // / end if dashboard collections
-
-});

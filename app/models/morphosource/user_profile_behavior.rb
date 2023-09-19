09e343a3fa2615484d53ea51a56c1aa476dd2777
Simon Choy <simon.choy@duke.edu>
2023-09-19 15:28:04 -0400

    added label in config yaml

--- app/models/morphosource/user_profile_behavior.rb
+++ app/models/morphosource/user_profile_behavior.rb
@@ -2,7 +2,7 @@ module Morphosource
   module UserProfileBehavior
 
     def valid_profile_types
-      @valid_profile_types ||= profile_type_config.keys - [:universal]
+      @valid_profile_types ||= profile_type_config.values.map { |profile| profile['label'] }.compact
     end
 
 
@@ -15,21 +15,36 @@ module Morphosource
 
     def check_profile_type
       unless profile_type_valid?
-        errors.add(:base, "Profile type not valid")
+        add_error("Profile type not valid")
       end
       unless metadata_match_profile_type?
-        errors.add(:base, "Metadata does not match profile type")
+        add_error("Metadata does not match profile type")
       end
     end
 
+    def profile_type_by_label(value)
+      profile_type_config.each do |profile_type, data|
+        if data['label'] == value
+          return profile_type
+        end
+      end
+     return nil
+    end
+
     def metadata_match_profile_type?
-      accepted_fields = profile_type_config[profile_type]['metadata_fields']
-      unaccepted_fields = non_universal_metadata_fields.uniq - [accepted_fields]
+      mapped_profile_type = profile_type_by_label(profile_type)
+      accepted_fields = []
+      if profile_type_config[mapped_profile_type]['metadata_fields'].present?
+        accepted_fields += [ profile_type_config[mapped_profile_type]['metadata_fields'] ]
+      end
+      if profile_type_config[mapped_profile_type]['required_metadata_fields'].present?
+        accepted_fields += [ profile_type_config[mapped_profile_type]['required_metadata_fields'] ]
+      end
+      unaccepted_fields = non_universal_metadata_fields.uniq - accepted_fields
       unaccepted_fields.each do |field|
         if self.respond_to?(field)
           if self.send(field).present?
-Rails.logger.debug("#{field} cannot be present for profile type #{profile_type}")
-            errors.add(:base, "#{field} cannot be present for profile type #{profile_type}")
+            add_error("#{field} cannot be present for profile type #{profile_type}")
           end
         end
       end
@@ -50,15 +65,13 @@ Rails.logger.debug("#{field} cannot be present for profile type #{profile_type}"
       return fields
     end
 
-
-
-
-
-
-
-
     def profile_type_valid?
       valid_profile_types.include?(profile_type)
+    end
+
+    def add_error(msg)
+      Rails.logger.debug(msg)
+      errors.add(:base, msg)
     end
 
   end

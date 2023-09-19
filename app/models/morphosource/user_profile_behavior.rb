7b3a46b70de714180e4aa187ceb517fc32c65af4
Simon Choy <simon.choy@duke.edu>
2023-09-18 21:24:41 -0400

    log error

--- app/models/morphosource/user_profile_behavior.rb
+++ app/models/morphosource/user_profile_behavior.rb
@@ -16,9 +16,47 @@ module Morphosource
     def check_profile_type
       unless profile_type_valid?
         errors.add(:base, "Profile type not valid")
-      end    
+      end
+      unless metadata_match_profile_type?
+        errors.add(:base, "Metadata does not match profile type")
+      end
     end
-    
+
+    def metadata_match_profile_type?
+      accepted_fields = profile_type_config[profile_type]['metadata_fields']
+      unaccepted_fields = non_universal_metadata_fields.uniq - [accepted_fields]
+      unaccepted_fields.each do |field|
+        if self.respond_to?(field)
+          if self.send(field).present?
+Rails.logger.debug("#{field} cannot be present for profile type #{profile_type}")
+            errors.add(:base, "#{field} cannot be present for profile type #{profile_type}")
+          end
+        end
+      end
+    end
+
+    def non_universal_metadata_fields
+      fields = []
+      profile_type_config.each do |prof_type, data|
+        if prof_type != 'universal'
+          if data['metadata_fields']
+            fields += data['metadata_fields'].split(', ')
+          end
+          if data['required_metadata_fields']
+            fields += data['required_metadata_fields'].split(', ')
+          end
+        end
+      end
+      return fields
+    end
+
+
+
+
+
+
+
+
     def profile_type_valid?
       valid_profile_types.include?(profile_type)
     end

0a8515bbabf96567803df9fad848e43fbfe75d8d
Simon Choy <simon.choy@duke.edu>
2023-10-12 17:35:15 -0400

    handle demographics in registration, add permitted params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,13 +6,55 @@ class RegistrationsController < Devise::RegistrationsController
   def new 
     @all_metadata_fields = all_metadata_fields_hash
     @required_metadata_fields = required_metadata_fields_hash
+    @all_demographics_values = all_demographics_values_hash
     super
   end
 
   protected
 
   def sign_up_params
-    params.require(:user).permit(:address, :affiliation, :sftp_share, :avatar, :country, :department, :display_name, :email, :facebook_handle, :linkedin_handle, :orcid, :password, :password_confirmation, :postal_code, :remove_avatar, :state, :telephone, :terms_read, :twitter_handle, :website, demographics: [], software: [], intent: [], mesh_file_type: [], volume_file_type: [], printer_file: [], printer_model: [] )
+    # todo: remove the following fields, and clean up later
+    # Postal Code
+    # Telephone
+    # Software to view 3D
+    # File Type for 3D Mesh
+    # File Type for 3D Volume
+    # 3D Printer Machine Type
+    # 3D Printer File Type
+    existing_permit_list = [
+      :address,
+      :affiliation,
+      :sftp_share,
+      :avatar,
+      :country,
+      :department,
+      :display_name,
+      :email,
+      :facebook_handle,
+      :linkedin_handle,
+      :orcid,
+      :password,
+      :password_confirmation,
+      :postal_code,
+      :remove_avatar,
+      :state,
+      :telephone,
+      :terms_read,
+      :twitter_handle,
+      :website,
+      demographics: [],
+      software: [],
+      intent: [],
+      mesh_file_type: [],
+      volume_file_type: [],
+      printer_file: [],
+      printer_model: []
+    ]
+    # add new user profile fields from yaml
+    profile_metadata_fields.each do |field, _type|
+      existing_permit_list << field.to_sym
+    end
+    params.require(:user).permit(existing_permit_list)
   end
 
   # Don't retain empty strings from 'other' fields.

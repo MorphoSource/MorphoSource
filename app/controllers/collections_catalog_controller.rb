e78e44b16c3bbf420dbc999e7a8420578ae9a496
Julie Winchester <julie.winchester@duke.edu>
2023-11-08 14:45:39 -0600

    Improve catalog facet field names

--- app/controllers/collections_catalog_controller.rb
+++ app/controllers/collections_catalog_controller.rb
@@ -5,11 +5,9 @@ class CollectionsCatalogController < CatalogController
   configure_blacklight do |config|
     config.search_builder_class = Morphosource::Catalog::CollectionsCatalogSearchBuilder
 
-    # facets
-    # type facet (team or project)
-    config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
-    # linked organization
-    config.add_facet_field solr_name("linked_organization", :facetable), label: "Organization", limit: 5
+    # facet fields
+    config.add_facet_field "type", field: "human_readable_type_sim", label: "Project or Team", limit: 10
+    config.add_facet_field "organization", field: "linked_organization_ssim", label: "Organization", limit: 10
 
     # search result metadata
     config.add_index_field solr_name("title", :stored_searchable), label: "Title", itemprop: 'name', if: false

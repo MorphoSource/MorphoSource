ff8b14af90ae7f608d138ca8d7db2fe4461d0f9f
Simon Choy <simon.choy@duke.edu>
2024-01-25 16:56:26 -0500

    added help_text

--- app/services/morphosource/list_types_service.rb
+++ app/services/morphosource/list_types_service.rb
@@ -6,12 +6,13 @@ module Morphosource
       super('list_types')
     end
 
-    def include_current_value(value, _index, render_options, html_options)
-      unless value.blank? || active?(value)
-        html_options[:class] << ' force-select'
-        render_options += [[label(value) { value }, value]]
+    def help_text_list(value=nil)
+      options = []
+      authority.all.map do |element|
+        opt = [element[:id], authority.find(element[:id]).fetch(:help_text)]
+        options << opt
       end
-      [render_options, html_options]
+      return options
     end
 
   end

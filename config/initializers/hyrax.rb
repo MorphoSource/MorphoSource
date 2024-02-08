a53866b11e056fdcb0a60dc673fd61b6fc18a9bd
Simon Choy <simon.choy@duke.edu>
2024-02-08 15:56:01 -0500

    updated iiif_image_url_builder with format argument

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -223,8 +223,8 @@ Hyrax.config do |config|
   config.iiif_image_server = true
 
   # Returns a URL that resolves to an image provided by a IIIF image server
-  config.iiif_image_url_builder = lambda do |file_id, base_url, size|
-    Riiif::Engine.routes.url_helpers.image_url(file_id, host: base_url, size: size)
+  config.iiif_image_url_builder = lambda do |file_id, base_url, size, format|
+    Riiif::Engine.routes.url_helpers.image_url(file_id, host: base_url, size: size, format: format)
   end
   # config.iiif_image_url_builder = lambda do |file_id, base_url, size|
   #   "#{base_url}/downloads/#{file_id.split('/').first}"

f36a01b527b57c7c6e826e4db5e99f33c905781b
Julie Winchester <julie.winchester@duke.edu>
2024-02-23 11:32:00 -0600

    Add gltf mesh derivative generation pipeline

--- lib/morphosource/derivatives/blender.rb
+++ lib/morphosource/derivatives/blender.rb
@@ -34,15 +34,16 @@ module Morphosource::Derivatives
     end
 
     protected
-      def command
-        "#{command_path} --background --factory-startup --addons io_scene_gltf2 --python vendor/blender_config/scripts/blender_derive_mesh.py -- -i '#{source_path}' -o '#{out_path}' -u '#{units}'"
-      end
 
-      # Check for produced derivative file, otherwise raise Blender response as error
-      def post_process(raw_output)
-        if !File.exists?(out_path) || (File.size(out_path) == 0)
-          raise Morphosource::Derivatives::BlenderError.new("File not successfully created by derivative tool.\nTool command: \"#{command}\"\nTool output:\n\"#{raw_output}\"")
-        end
+    def command
+      "#{command_path} --background --factory-startup --addons io_scene_gltf2 --python vendor/blender_config/scripts/blender_derive_mesh.py -- -i '#{source_path}' -o '#{out_path}' -u '#{units}'"
+    end
+
+    # Check for produced derivative file, otherwise raise Blender response as error
+    def post_process(raw_output)
+      if !File.exists?(out_path) || (File.size(out_path) == 0)
+        raise Morphosource::Derivatives::BlenderError.new("File not successfully created by derivative tool.\nTool command: \"#{command}\"\nTool output:\n\"#{raw_output}\"")
       end
+    end
   end
 end
---
+++ lib/morphosource/derivatives/gltf_transform.rb
@@ -0,0 +1,65 @@
+module Morphosource::Derivatives
+  class GltfTransformError < RuntimeError
+  end
+
+  # Derivative tool to interface with gltf-transform CLI utility
+  class GltfTransform < DerivativeTool
+    attr_reader :cli_command, :source_path, :out_path, :opts
+    def initialize(cli_command:, source_path:, out_path:, opts: {})
+      if accepted_cli_commands.include? cli_command
+        @cli_command = cli_command
+      else
+        raise Morphosource::Derivatives::GltfTransformError.new("Unrecognized gltf-transform CLI command: #{cli_command}")
+      end
+
+      @source_path = source_path
+      @out_path = out_path
+      @opts = default_opts.merge(opts)
+    end
+
+    def accepted_cli_commands
+      [:optimize, :center]
+    end
+
+    def default_opts
+      opts_by_command[cli_command]
+    end
+
+    def opts_by_command
+      {
+        optimize: { max_texture_size: 1024, simplify: false, simplify_error: 0.0001 },
+        center: {} # could support pivot point center/below/above, but for now support only center
+      }
+    end
+
+    def call
+      unless File.exists?(source_path)
+        raise Morphosource::Derivatives::GltfTransformError.new("Source file: #{source_path} does not exist.")
+      end
+
+      internal_call
+    end
+
+    protected  
+
+    def command
+      case cli_command
+      when :optimize
+        "gltf-transform optimize '#{source_path}' '#{out_path}' \\
+          --compress draco \\
+          --simplify #{opts[:simplify]} \\
+          --simplify-error #{opts[:simplify_error]} \\
+          --texture-size #{opts[:max_texture_size]}"
+      when :center
+        "gltf-transform center '#{source_path}' '#{out_path}'"
+      end
+    end
+
+    # Check for produced derivative file, otherwise raise gltf-transform response as error
+    def post_process(raw_output)
+      if !File.exists?(out_path) || (File.size(out_path) == 0)
+        raise Morphosource::Derivatives::GltfTransformError.new("File not successfully created by derivative tool.\nTool command: \"#{command}\"\nTool output:\n\"#{raw_output}\"")
+      end
+    end
+  end
+end

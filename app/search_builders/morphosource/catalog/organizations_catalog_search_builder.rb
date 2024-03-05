e2ff7a92a319f4724691db9ee2a5f053f0544c67
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2024-03-05 10:19:29 -0500

    wip

--- app/search_builders/morphosource/catalog/organizations_catalog_search_builder.rb
+++ app/search_builders/morphosource/catalog/organizations_catalog_search_builder.rb
@@ -1,10 +1,5 @@
 class Morphosource::Catalog::OrganizationsCatalogSearchBuilder < Morphosource::CatalogSearchBuilder
 
-  def add_query_to_solr(solr_parameters)
-    byebug
-    super
-  end
-
   private
 
     def models

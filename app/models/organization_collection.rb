9581f595dd8c64926462fcd8244053765b0e29ad
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2024-03-08 16:19:49 -0500

    Creating an organization collection creates an example project

--- app/models/organization_collection.rb
+++ app/models/organization_collection.rb
@@ -6,6 +6,7 @@ class OrganizationCollection < Collection
   include Morphosource::LocationMetadata
 
   after_create :create_collection_groups
+  after_create :create_organization_project
 
   self.indexer = OrganizationCollectionIndexer
 
@@ -69,4 +70,21 @@ class OrganizationCollection < Collection
   def self.polymorphic_name
     "OrganizationCollection"
   end
+  private
+
+    def create_organization_project
+      project = example_organization_project
+      project.create_collection_groups
+      project.member_of_collections << self
+      project.save!
+      project
+    end
+
+    # Create a starter project for the organization
+    def example_organization_project
+      project_collection_type = Hyrax::CollectionType.where({:title => 'Project'})&.first
+      project_title = [I18n.t('morphosource.dashboard.collections.organization_collection.example_project.title', title: title.first)]
+      description = [I18n.t('morphosource.dashboard.collections.organization_collection.example_project.description')]
+      project = Collection.create(title: project_title, collection_type_gid: project_collection_type.gid, description: description, visibility: 'restricted', depositor: depositor)
+    end
 end

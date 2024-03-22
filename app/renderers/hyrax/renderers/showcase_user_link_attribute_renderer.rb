f32f35b8db0642ea5b84f1d44bd5aa95d0ab48bf
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2024-03-22 10:59:47 -0400

    user solr instead of fedora

--- app/renderers/hyrax/renderers/showcase_user_link_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/showcase_user_link_attribute_renderer.rb
@@ -4,14 +4,13 @@ module Hyrax
 
       def user_link(user)
         markup = ''
-        case user
-        when User
+        if user.is_a?(User)
           ms_id = user.ms_id
           display_text = user.name_or_email
           url = "/users/" + ms_id
-        when OrganizationCollection
+        else
           id = user.id
-          display_text = user.name
+          display_text = user.title.first
           url = "/organizations/" + id
         end
         link = link_to(display_text, "#{url}")
@@ -24,14 +23,12 @@ module Hyrax
           markup = ''
           return markup if value.blank?
 
-          user = ::User.find_by_user_key(value) || OrganizationCollection.find_by(id: value)
+          user = ::User.find_by_user_key(value) || ::SolrDocument.where("id" => value).first
           if user.present?
             link = user_link(user)
           else
-            return markup
+            markup
           end
-          markup = "<span class='showcase-link'>#{link}</span>"
-          markup.html_safe
         end
     end
   end

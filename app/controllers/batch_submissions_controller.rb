8ef43ce83f8b0ae23f494d57ebe7352063d904d2
Julie Winchester <julie.winchester@duke.edu>
2024-04-03 16:18:53 -0500

    Fix submissions so that devices tied to orgs via organization_id are included

--- app/controllers/batch_submissions_controller.rb
+++ app/controllers/batch_submissions_controller.rb
@@ -3,6 +3,7 @@ require 'roo'
 class BatchSubmissionsController < ApplicationController
   include BatchSubmissionTools::Ms2Batch::BatchSubmissionHelper
   include Hyrax::WorksControllerBehavior
+  include SubmissionsControllerBehavior
 
   load_and_authorize_resource 
   with_themed_layout 'morphosource_dashboard'
@@ -69,91 +70,7 @@ class BatchSubmissionsController < ApplicationController
       work_data: work_data
     })
 
-    @device_organizations = repository.search(device_organizations_search_builder.query)["response"]["docs"]
-    @object_organizations = repository.search(object_organizations_search_builder.query)["response"]["docs"]
-
-    @device_organizations_select2 = @device_organizations.map do |o|
-      {
-        id: o['id'],
-        text: "#{[o['institution_name_tesim']&.first, o['title_tesim']&.first].compact.join(', ')} (#{o['institution_code_tesim']&.join('/')}:#{o['collection_code_tesim']&.join('/')})",
-        organization_type: o['organization_type_tesim']&.first,
-        institution_name: o['institution_name_tesim']&.first,
-        title: o['title_tesim']&.first,
-        institution_code: o['institution_code_tesim']&.join(', '),
-        collection_code: o['collection_code_tesim']&.join(', '),
-        recordset_id: o['recordset_id_tesim']&.join(', '),
-        related_url: o['related_url_tesim']&.first,
-        address: o['address_tesim']&.first,
-        city: o['city_tesim']&.first,
-        state_province: o['state_province_tesim']&.first,
-        country: o['country_tesim']&.first,
-        postal_code: o['postal_code_tesim']&.first,
-        description: o['description_tesim']&.first,
-        contact_person: o['contact_person_tesim']&.first,
-        devices: o['member_ids_ssim']
-      }
-    end
-
-    @object_organizations_select2 = @object_organizations.map do |o|
-      {
-        id: o['id'],
-        text: "#{[o['institution_name_tesim']&.first, o['title_tesim']&.first].compact.join(', ')} (#{o['institution_code_tesim']&.join('/')}:#{o['collection_code_tesim']&.join('/')})",
-        organization_type: o['organization_type_tesim']&.first,
-        institution_name: o['institution_name_tesim']&.first,
-        title: o['title_tesim']&.first,
-        institution_code: o['institution_code_tesim']&.join(', '),
-        collection_code: o['collection_code_tesim']&.join(', '),
-        recordset_id: o['recordset_id_tesim']&.join(', '),
-        related_url: o['related_url_tesim']&.first,
-        address: o['address_tesim']&.first,
-        city: o['city_tesim']&.first,
-        state_province: o['state_province_tesim']&.first,
-        country: o['country_tesim']&.first,
-        postal_code: o['postal_code_tesim']&.first,
-        description: o['description_tesim']&.first,
-        contact_person: o['contact_person_tesim']&.first,
-        devices: o['member_ids_ssim']
-      }
-    end
-
-    if Hyrax.config.null_organization_id.present? && Organization.exists?(Hyrax.config.null_organization_id)
-      o = Organization.find(Hyrax.config.null_organization_id)
-      @null_organization = {
-        id: o.id,
-        text: "#{[o.institution_name&.first, o.title&.first].compact.join(', ')} (#{o.institution_code&.join('/')}:#{o.collection_code&.join('/')})",
-        organization_type: o.organization_type&.first,
-        institution_name: o.institution_name&.first,
-        title: o.title&.first,
-        institution_code: o.institution_code&.join(', '),
-        collection_code: o.collection_code&.join(', '),
-        related_url: o.related_url&.first,
-        address: o.address&.first,
-        city: o.city&.first,
-        state_province: o.state_province&.first,
-        country: o.country&.first,
-        postal_code: o.postal_code&.first,
-        description: o.description&.first,
-        contact_person: o.contact_person&.first,
-        devices: o.member_ids
-      }
-    else
-      @null_organization = {}
-    end
-
-    @devices = Device.all_solr
-    @devices_with_ids = @devices.map do |d|
-      [d['id'], {
-        id: d['id'],
-        text: [d['creator_tesim']&.first, d['title_tesim']&.first].compact.join(' '),
-        title: d['title_tesim']&.first,
-        creator: d['creator_tesim']&.first,
-        modality: d['modality_tesim']&.join(','),
-        description: d['description_tesim']&.first
-      }]
-    end.to_h
-    if !current_user.admin?
-      @devices_with_ids.delete(Hyrax.config.unknown_ct_scanner)
-    end
+    get_organizations_and_devices
 
     if params[:collection] && Collection.exists?(params[:collection])
       @batch_submission.collection_id = params[:collection]

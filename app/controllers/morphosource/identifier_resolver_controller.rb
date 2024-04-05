c37565c6f6b6b0ed91b7e5cfe0997a3f42585bac
Simon Choy <simon.choy@duke.edu>
2024-04-05 16:53:27 -0400

    change media id to work id

--- app/controllers/morphosource/identifier_resolver_controller.rb
+++ app/controllers/morphosource/identifier_resolver_controller.rb
@@ -2,16 +2,25 @@
 module Morphosource
   class IdentifierResolverController < ApplicationController
     def resolve_ark
+byebug  
       if !ark_path_valid?
         redirect_to '/', alert: 'ARK path is not valid or application is not configured to resolve ARKs.' and return
       end
 
-      if !ark_media_id_valid?
+      if !ark_work_id_valid?
         redirect_to '/', alert: 'Media ID is not valid or media is unavailable.' and return
       end
 
-      if ark_path_valid? && ark_media_id_valid?
-        redirect_to main_app.media_showcase_path(ms2_id) and return
+      if ark_path_valid? && resolved_path.present?
+
+
+        
+
+        redirect_to resolved_path and return
+        
+        
+
+        #redirect_to main_app.media_showcase_path(ms2_id) and return
       end
     end
 
@@ -20,11 +29,11 @@ module Morphosource
         redirect_to '/', alert: 'DOI path is not valid or application is not configured to resolve DOIs.' and return
       end
 
-      if !doi_media_id_valid?
+      if !doi_work_id_valid?
         redirect_to '/', alert: 'Media ID is not valid or media is unavailable.' and return
       end
 
-      if doi_path_valid? && doi_media_id_valid?
+      if doi_path_valid? && doi_work_id_valid?
         redirect_to main_app.media_showcase_path(ms2_id) and return
       end
     end
@@ -45,9 +54,22 @@ module Morphosource
       @ark_shoulder_split ||= ( ENV['EZID_DEFAULT_SHOULDER'] || '' ).split('/')
     end
 
-    def ark_media_id_valid?
-      @media_id = path_split[2][/\d+/]
-      @media_id.present? && Media.exists?(ms2_id)
+#    def ark_work_id_valid?
+#      @work_id = path_split[2][/\d+/]
+#      @work_id.present? && Media.exists?(ms2_id)
+#    end
+
+    def resolved_path
+byebug
+      @work_id = path_split[2][/\d+/]
+byebug
+      if @work_id.present?
+        if (work = ActiveFedora::Base.find(ms2_id)).present?
+byebug
+          return "work_path_here"
+        end
+      end
+      return nil
     end
 
     # doi methods    
@@ -64,9 +86,9 @@ module Morphosource
       @doi_shoulder_split ||= ( ENV['CROSSREF_DOI_SHOULDER'] || '' ).split('/')
     end
 
-    def doi_media_id_valid?
-      @media_id = path_split[1][/\d+/]
-      @media_id.present? && Media.exists?(ms2_id)
+    def doi_work_id_valid?
+      @work_id = path_split[1][/\d+/]
+      @work_id.present? && Media.exists?(ms2_id)
     end
 
 
@@ -76,7 +98,7 @@ module Morphosource
     end
 
     def ms2_id
-      @ms2_id ||= pad(@media_id)
+      @ms2_id ||= pad(@work_id)
     end
 
     def pad(id)

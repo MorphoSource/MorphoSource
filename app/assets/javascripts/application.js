93725a0b55328b9e765f39b7cca979d4cd7f77e2
Simon Choy <simon.choy@duke.edu>
2024-04-10 14:57:51 -0400

    added back browse_everything js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 // Required by Blacklight
 //= require jquery
 //= require jquery_ujs
+//= require browse_everything
 // Important: the order of loadng dataTables js matters
 //= require dataTables/jquery.dataTables
 //= require dataTables/extras/dataTables.responsive

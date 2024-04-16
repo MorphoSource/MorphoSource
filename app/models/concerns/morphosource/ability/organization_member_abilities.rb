858858bfd1bbdf1ce634d612f74c20a36056f5d3
Julie Winchester <julie.winchester@duke.edu>
2024-04-16 17:19:31 -0500

    Simplify organization member abilities, fix an ability sequencing bug when checking ability for multiple media sequentially

--- app/models/concerns/morphosource/ability/organization_member_abilities.rb
+++ app/models/concerns/morphosource/ability/organization_member_abilities.rb
@@ -1,7 +1,6 @@
 module Morphosource
   module Ability
     module OrganizationMemberAbilities
-
       # View Media and FileSets through organization membership
       def organization_member_abilities
 
@@ -82,132 +81,101 @@ module Morphosource
 
       private
 
-        # Users who are members of the organizations whose IDs are stored in these media fields can view the media
-        def organization_fields
-          ["media_organization_id_ssim","media_device_facility_organization_id_ssim"]
+      # returns true if the user has read or edit access to the media through the organization collection
+      def has_organizational_access_to_media?(media)
+        Rails.logger.debug("[CANCAN] Checking for individual media access through organization membership")
+        return false unless media = solr_document(media)
+
+        (organization_groups(media) & @user_groups).present?
+      end
+
+      # returns true if the user has edit access to the media through the organization collection
+      def has_organizational_edit_access_to_media?(media)
+        Rails.logger.debug("[CANCAN] Checking for individual media edit access through organization membership")
+        return false unless media = solr_document(media)
+
+        (organization_edit_groups(media) & @user_groups).present?
+      end
+
+      def has_organizational_access_to_fileset?(file_set)
+        Rails.logger.debug("[CANCAN] Checking for individual file set access through organization membership")
+        return false unless file_set = solr_document(file_set)
+
+        return false unless media_id = file_set_media_id(file_set)
+
+        has_organizational_access_to_media?(media_id)
+      end
+
+      def has_organizational_edit_access_to_fileset?(file_set)
+        Rails.logger.debug("[CANCAN] Checking for individual file set edit access through organization membership")
+        return false unless file_set = solr_document(file_set)
+
+        return false unless media_id = file_set_media_id(file_set)
+
+        has_organizational_edit_access_to_media?(media_id)
+      end
+
+      def file_set_media_id(file_set)
+        ( SolrDocument.where("file_set_ids_ssim:#{file_set.id}")&.first || {} )['id']
+      end
+
+      # return an array of organization role names corresponding to organization_fields
+      # ex: ['000012345_managers', '000012345_editors', '000012345_depositors', '000012345_downloaders', '000012345_viewers']
+      def organization_groups(media)
+        return [] unless media = solr_document(media)
+
+        object_org_id = media["media_organization_id_ssim"]&.first
+        device_org_id = media["media_device_facility_organization_id_ssim"]&.first
+        owner_org_id = (media["owner_type_ssi"] == "OrganizationCollection") ? media["owner_ssim"]&.first : nil
+
+        [object_org_id, device_org_id, owner_org_id].compact.uniq.each_with_object([]) do |id, groups|
+          OrganizationCollection::READ_GROUP_ROLES.each {|role| groups << "#{id}_#{role}"}
+        end.flatten
+      end
+
+      def organization_edit_groups(media)
+        return [] unless media = solr_document(media)
+
+        if (media["owner_type_ssi"] == "OrganizationCollection") && media["owner_ssim"]&.first.present?
+          owner_org_id = media["owner_ssim"]&.first
+          return OrganizationCollection::EDIT_GROUP_ROLES.map {|role| "#{owner_org_id}_#{role}" }
+        else
+          return []
         end
+      end
 
-        # returns true if the user has read or edit access to the media through the organization collection
-        def has_organizational_access_to_media?(media)
-          Rails.logger.debug("[CANCAN] Checking for individual media access through organization membership")
-          return false unless media = solr_document(media)
+      def user_manages_media_through_organization?(media_id)
+        return unless media = solr_document(media_id)
 
-          (organization_groups(media) & @user_groups).present?
+        if (media["owner_type_ssi"] == "OrganizationCollection") && (owner_org_id = media["owner_ssim"]&.first).present?
+          user_is_manager_of_organization?(owner_org_id)
+        else
+          false
         end
+      end
 
-        # returns true if the user has edit access to the media through the organization collection
-        def has_organizational_edit_access_to_media?(media)
-          Rails.logger.debug("[CANCAN] Checking for individual media edit access through organization membership")
-          return false unless media = solr_document(media)
+      def user_is_manager_of_organization?(organization_id)
+        return false if organization_id.nil?
 
-          (organization_edit_groups(media) & @user_groups).present?
+        current_user.groups.include? "#{organization_id}_managers"
+      end
+
+      def solr_document(obj)
+        case obj
+        when FileSet
+          return SolrDocument.find(obj.id)
+        when Media
+          return SolrDocument.find(obj.id)
+        when SolrDocument
+          return obj
+        when String
+          return SolrDocument.find(obj)
+        when Hash
+          return SolrDocument.find(obj['id'])
         end
-
-        def has_organizational_access_to_fileset?(file_set)
-          Rails.logger.debug("[CANCAN] Checking for individual file set access through organization membership")
-          return false unless file_set = solr_document(file_set)
-
-          return false unless media_id = file_set_media_id(file_set)
-
-          has_organizational_access_to_media?(media_id)
-        end
-
-        def has_organizational_edit_access_to_fileset?(file_set)
-          Rails.logger.debug("[CANCAN] Checking for individual file set edit access through organization membership")
-          return false unless file_set = solr_document(file_set)
-
-          return false unless media_id = file_set_media_id(file_set)
-
-          has_organizational_edit_access_to_media?(media_id)
-        end
-
-        def file_set_media_id(file_set)
-          @media_id ||= ( SolrDocument.where("file_set_ids_ssim:#{file_set.id}")&.first || {} )['id']
-        end
-
-        def organization_groups(media)
-          @organization_groups ||= determine_organization_groups(media)
-        end
-
-        # return an array of organization role names corresponding to organization_fields
-        # ex: ['000012345_managers', '000012345_editors', '000012345_depositors', '000012345_downloaders', '000012345_viewers']
-        def determine_organization_groups(media)
-          return [] unless media = solr_document(media)
-
-          # get the ids of the media's physical object organization and scanning organization
-          media_organization_ids = organization_fields.map{ |field| media[field]&.first }.compact.uniq
-
-          groups = media_organization_ids.each_with_object([]) do |id, groups|
-            OrganizationCollection::DEFAULT_GROUP_ROLES.each {|role| groups << "#{id}_#{role}"}
-          end
-          groups + organization_owner_groups(media)
-        end
-
-        def organization_edit_groups(media)
-          return [] unless media = solr_document(media)
-
-          return [] unless organization_owner_id(media)
-
-          # get the combination of organization_groups and manager/editor groups whose name includes the media's owner.
-          groups = organization_groups(media) + organization_owner_groups(media)
-          groups.select{ |group| (group.include?("managers") || group.include?("editors")) }.uniq
-        end
-
-        # If the media has an owner, return an array of organization role names
-        # ex: ['000012345_managers', '000012345_editors', '000012345_depositors', '000012345_downloaders', '000012345_viewers']
-        def organization_owner_groups(media)
-          return [] unless organization_owner_id(media)
-
-          @organization_owner_groups ||= OrganizationCollection::DEFAULT_GROUP_ROLES.each_with_object([]) {|role, groups| groups << "#{@organization_owner_id}_#{role}"}
-        end
-
-        # if the media is owned by an organization collection, return the collection id.
-        # otherwise return nil
-        def organization_owner_id(media)
-          @organization_owner_id ||= owner_is_organization?(media) ? media_owner_id(media) : nil
-        end
-
-        def media_owner_id(media)
-          @media_owner_id ||= media['owner_ssim']&.first
-        end
-
-        def solr_document(obj)
-          case obj
-          when FileSet
-            return SolrDocument.find(obj.id)
-          when Media
-            return SolrDocument.find(obj.id)
-          when SolrDocument
-            return obj
-          when String
-            return SolrDocument.find(obj)
-          when Hash
-            return SolrDocument.find(obj['id'])
-          end
-        rescue
-          nil
-        end
-
-        def media_owner_type(media)
-          @media_owner_type ||= (solr_document(media)&.to_h || {})['owner_type_ssi']
-        end
-
-        def owner_is_organization?(media)
-          media_owner_type(media) == 'OrganizationCollection'
-        end
-
-        def user_manages_media_through_organization?(media_id)
-          return unless media = solr_document(media_id)
-
-          user_is_manager_of_organization?(organization_owner_id(media))
-        end
-
-        def user_is_manager_of_organization?(organization_id)
-          return false if organization_id.nil?
-
-          current_user.groups.include? "#{organization_id}_managers"
-        end
-
+      rescue
+        nil
+      end
     end
   end
 end

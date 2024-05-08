d99b8d8e2224f94b05bda405d2a7de05d8c79af9
Simon Choy <simon.choy@duke.edu>
2024-05-08 12:29:17 -0400

    general detail section, delete button

--- app/presenters/hyrax/device_presenter.rb
+++ app/presenters/hyrax/device_presenter.rb
@@ -4,7 +4,16 @@ module Hyrax
   class DevicePresenter < Hyrax::WorkShowPresenter
     include Morphosource::PresenterMethods
 
-    delegate :modality, to: :solr_document
+    delegate :modality, :ark, to: :solr_document
+
+    #
+    # Modality (return one more more values)
+    #
+    # @return [String] modality
+    #
+    def device_modality
+      @device_modality ||= modality.map{ |m| Morphosource::ModalitiesService.new.label(m) }.join(', ')
+    end
 
     # methods for showcase partials
     def showcase_work_title_partial
@@ -13,6 +22,10 @@ module Hyrax
 
     def showcase_show_actions_partial
       '/hyrax/devices/showcase_show_actions'
+    end
+
+    def showcase_general_details_partial
+      'showcase_general_details'
     end
 
   end

508213ecc953764403799c69e12eaf22e317a556
Simon Choy <simon.choy@duke.edu>
2024-05-08 18:25:58 -0400

    media items block

--- app/presenters/hyrax/device_presenter.rb
+++ app/presenters/hyrax/device_presenter.rb
@@ -15,17 +15,36 @@ module Hyrax
       @device_modality ||= modality.map{ |m| Morphosource::ModalitiesService.new.label(m) }
     end
 
+    def related_media_ids
+      @related_media_ids ||= begin
+#        solr_document.related_media_ids || []
+      end
+    end
+
+    def viewable_related_media_ids
+      return related_media_ids if current_ability.current_user.admin?
+      @viewable_related_media_ids ||= related_media_ids.select { |id| current_ability.can?(:read, id) }
+    end
+
     # methods for showcase partials
     def showcase_work_title_partial
-      '/hyrax/devices/showcase_work_title'
+      'showcase_work_title'
     end
 
     def showcase_show_actions_partial
-      '/hyrax/devices/showcase_show_actions'
+      'showcase_show_actions'
     end
 
     def showcase_general_details_partial
       'showcase_general_details'
+    end
+
+    def showcase_media_items_partial
+      'showcase_media_items'
+    end
+
+    def showcase_media_items_member_partial
+      '/hyrax/physical_objects/showcase_media_items_member'
     end
 
   end

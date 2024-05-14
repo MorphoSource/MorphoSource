1ecb3fb08dfcaa5894ceb705e637b7673ed6efcb
Simon Choy <simon.choy@duke.edu>
2024-05-14 16:59:06 -0400

    update device links

--- app/renderers/hyrax/renderers/showcase_page_link_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/showcase_page_link_attribute_renderer.rb
@@ -11,26 +11,21 @@ module Hyrax
           obj = ActiveFedora::Base.find(value)
           case obj
           when Organization
-            label = obj.title.first
             link = Rails.application.routes.url_helpers.hyrax_organization_path(value)
           when OrganizationCollection
-            label = obj.title.first
             link = Rails.application.routes.url_helpers.organization_collection_path(value)
           when Device
-            label = obj.creator.first
             link = Rails.application.routes.url_helpers.hyrax_device_path(value)
           when Media
-            label = obj.title.first
             link = Rails.application.routes.url_helpers.hyrax_media_path(value)
           when BiologicalSpecimen
-            label = obj.title.first
             link = Rails.application.routes.url_helpers.hyrax_biological_specimen_path(value)
           when CulturalHeritageObject
-            label = obj.title.first
             link = Rails.application.routes.url_helpers.hyrax_cultural_heritage_object_path(value)
           else
             return markup
           end
+          label = obj.title.first
           markup << "<span class='showcase-link' style='word-break: normal;'>#{link_to(label, link)}</span>"
           markup.html_safe
         end

fa167a627c83d1dac72a8b43defc403a86602af1
Simon Choy <simon.choy@duke.edu>
2024-05-15 10:14:37 -0400

    update org device tab columns with ARK

--- app/renderers/hyrax/renderers/showcase_ark_link_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/showcase_ark_link_attribute_renderer.rb
@@ -4,13 +4,8 @@ module Hyrax
       private
 
         def attribute_value_to_html(value)
-          markup = ''
-          return markup if value.blank? 
-          # todo: move the urls (and possibly the logic) outside of renderer, e.g presenter?
-          url = 'http://n2t.net/'
-          link = link_to(value, "#{url}#{value}", target: :blank)
-          markup = "<span class='glyphicon glyphicon-new-window'></span>&nbsp;<span class='showcase-link'>#{link}</span>"
-          markup.html_safe
+          return '' if value.blank? 
+          return "<span class='glyphicon glyphicon-new-window'></span>&nbsp;<span class='showcase-link'>#{ark_link(value)}</span>".html_safe
         end
     end
   end

459be7e0f80482d2cf3fead131c91d48a5f4b9c6
Simon Choy <simon.choy@duke.edu>
2024-05-16 10:46:06 -0400

    separate get_title from generated_title method

--- app/actors/hyrax/actors/media_actor.rb
+++ app/actors/hyrax/actors/media_actor.rb
@@ -7,28 +7,28 @@ module Hyrax
       include Morphosource::LinkedTeams::LinkedTeamsManagement
 
       def create(env)
-        env.attributes['title'] = [ generated_title(env) ]
+        env.attributes['title'] = [ get_title(env) ]
         env.attributes['keyword'] = split_keywords(env)
         add_team_access(env)
         super
       end
 
       def update(env)
-        env.attributes['title'] = [ generated_title(env) ]
+        env.attributes['title'] = [ get_title(env) ]
         env.attributes['keyword'] = split_keywords(env)
         add_team_access(env)
         super
       end
 
-      def generated_title(env)
+      def get_title(env)
         attrs = env.attributes
         if attrs.key?('short_title')
-          short_title = attrs['short_title'].presence || ''
+          custom_title = attrs['short_title'].presence || ''
         else
-          short_title = env.curation_concern.short_title.presence || ''
+          custom_title = env.curation_concern.short_title.presence || ''
         end
 
-        if short_title.present?
+        if custom_title.present?
           # use custom title provided by user
           if attrs.key?('custom_title_case_sensitive')
             custom_title_case_sensitive = attrs['custom_title_case_sensitive'].presence || ''
@@ -38,60 +38,64 @@ module Hyrax
 
           if custom_title_case_sensitive.present?
             if custom_title_case_sensitive == ['1']
-              return short_title.first
+              return custom_title.first
             end
           end
-          return short_title.first.titleize
-        else
-          # use system generated title
-          if attrs.key?('id')
-            id = attrs['id'].presence || ''
-          else
-            id = env.curation_concern.id.presence || ''
-          end
-
-          if attrs.key?('part')
-            part = attrs['part'].presence || ''
-          else
-            part = env.curation_concern.part.presence || ''
-          end
-
-          if attrs.key?('media_type')
-            media_type = attrs['media_type']&.first.presence || ''
-          else
-            media_type = env.curation_concern.media_type&.first.presence || ''
-          end
-
-          # get the modality from the parent imaging event
-          ie_modality = []
-
-          if attrs['work_parents_attributes'].present?
-            # Adding or changing parents
-            attrs['work_parents_attributes'].each do |key, wp|
-              work_parent = Morphosource::Works::Base.find(wp['id'])
-              if work_parent.class.to_s == 'ImagingEvent'
-                ie_modality << work_parent.ie_modality&.first
-              elsif work_parent.class.to_s == 'ProcessingEvent'
-                ie = work_parent.imaging_event
-                ie_modality << ie.ie_modality&.first if ie.present?
-              end
-            end
-          elsif id.present? && Media.exists?(id)
-            # Updating work, not updating parents
-            imaging_event = Media.find(id).imaging_event
-            ie_modality << imaging_event.ie_modality&.first if imaging_event.present?
-          else
-            ie_modality = []
-          end
-
-          # Final formatting and backup values
-          parts = part.presence || ['Element unspecified']
-          modality_abbrevs = ie_modality.map { |m| Morphosource::ModalitiesService.abbreviation(m) }
-
-          parts.sort.join(', ').titleize +
-            (media_type.presence ? ' [' + media_type.to_s + ']' : '') +
-            (modality_abbrevs.presence ? ' [' + modality_abbrevs.join('/')+ ']' : ' [Etc]')
+          return custom_title.first.titleize
+        else 
+          return generated_title(env)
         end
+      end
+
+      def generated_title(env)
+        attrs = env.attributes
+        if attrs.key?('id')
+          id = attrs['id'].presence || ''
+        else
+          id = env.curation_concern.id.presence || ''
+        end
+
+        if attrs.key?('part')
+          part = attrs['part'].presence || ''
+        else
+          part = env.curation_concern.part.presence || ''
+        end
+
+        if attrs.key?('media_type')
+          media_type = attrs['media_type']&.first.presence || ''
+        else
+          media_type = env.curation_concern.media_type&.first.presence || ''
+        end
+
+        # get the modality from the parent imaging event
+        ie_modality = []
+
+        if attrs['work_parents_attributes'].present?
+          # Adding or changing parents
+          attrs['work_parents_attributes'].each do |key, wp|
+            work_parent = Morphosource::Works::Base.find(wp['id'])
+            if work_parent.class.to_s == 'ImagingEvent'
+              ie_modality << work_parent.ie_modality&.first
+            elsif work_parent.class.to_s == 'ProcessingEvent'
+              ie = work_parent.imaging_event
+              ie_modality << ie.ie_modality&.first if ie.present?
+            end
+          end
+        elsif id.present? && Media.exists?(id)
+          # Updating work, not updating parents
+          imaging_event = Media.find(id).imaging_event
+          ie_modality << imaging_event.ie_modality&.first if imaging_event.present?
+        else
+          ie_modality = []
+        end
+
+        # Final formatting and backup values
+        parts = part.presence || ['Element unspecified']
+        modality_abbrevs = ie_modality.map { |m| Morphosource::ModalitiesService.abbreviation(m) }
+
+        parts.sort.join(', ').titleize +
+          (media_type.presence ? ' [' + media_type.to_s + ']' : '') +
+          (modality_abbrevs.presence ? ' [' + modality_abbrevs.join('/')+ ']' : ' [Etc]')
       end
 
       private

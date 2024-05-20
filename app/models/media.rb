a8e57266010b12a4e890cd9e63d8b491982dfffb
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2024-05-20 09:56:52 -0500

    restructure add_to_organization_collection

--- app/models/media.rb
+++ app/models/media.rb
@@ -499,19 +499,19 @@ class Media < Morphosource::Works::Base
   def transfer_media_to_organization_collection(org)
     # check that organization has a valid data manager
     if org.managers&.first.present?
-      if self.organization_transfer_on_publish
-        self.organization_transfer_on_publish = false
-        self.save!
-      end
       # First, is media manager user the same as the new org data manager?
       owner_user = User.find_by(ms_id: user_with_ownership)
       proxy_user = User.find_by(ms_id: on_behalf_of)
       if owner_user.groups.include?("#{org.id}_managers") || proxy_user&.groups&.include?("#{org.id}_managers")
         # don't create transfer, but add organization as media owner and ensure no further transfers are created
-        add_to_organization_collection(org.id)
+        self.owner = org_id
       else
         create_new_organization_transfer_request(org)
       end
+      if self.organization_transfer_on_publish
+        self.organization_transfer_on_publish = false
+      end
+      self.save!
     else
       message = "Failed to transfer management of media #{id} to organization #{org&.id}"
 
@@ -524,14 +524,6 @@ class Media < Morphosource::Works::Base
     if (org = organizations&.first).present? && (team = org.team).present?
       AddCollectionMembersJob.perform_later(team.id, id)
     end
-  end
-
-  def add_to_organization_collection(org_id)
-    self.owner = org_id
-    if self.organization_transfer_on_publish
-      self.organization_transfer_on_publish = false
-    end
-    self.save!
   end
 
   def create_new_organization_transfer_request(org_data_manager, force_update=false)

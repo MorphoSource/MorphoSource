87191221acfe8f2e0cc922213af28b11d75da5d1
Julie Winchester <julie.winchester@duke.edu>
2024-05-21 16:43:31 -0500

    Add production Puma config

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,5 +1,17 @@
+before_fork do
+  PumaWorkerKiller.config do |config|
+    config.ram           = ( ENV.fetch("PUMA_WORKER_MEMORY_LIMIT") { 10240 } ).to_i # MiB, default is 10 GiB
+    config.frequency     = 300 # seconds
+    config.percent_usage = 0.99
+    config.rolling_restart_frequency = ( ENV.fetch("PUMA_WORKER_RESTART_HOURS") { 12 } ).to_i.hours
+  end
+  PumaWorkerKiller.start
+end
+
+worker_timeout 3600 # 1 hour timeout, to match Apache timeout
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
-bind "tcp://0.0.0.0:9292"
+bind 'tcp://127.0.0.1:9292'
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
+activate_control_app 'tcp://127.0.0.1:9293', { no_token: true }

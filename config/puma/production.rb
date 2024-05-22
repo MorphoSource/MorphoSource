9b1fa8ad00f93db5f6ff806773e956d5f1cf7ca9
Julie Winchester <julie.winchester@duke.edu>
2024-05-22 14:39:08 -0500

    Switch PumaWorkerKiller to rolling restarts due to running in container

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,11 +1,15 @@
 before_fork do
   PumaWorkerKiller.config do |config|
-    config.ram           = ( ENV.fetch("PUMA_WORKER_MEMORY_LIMIT") { 10240 } ).to_i # MiB, default is 10 GiB
-    config.frequency     = 300 # seconds
-    config.percent_usage = 0.99
+    # config.ram           = ( ENV.fetch("PUMA_WORKER_MEMORY_LIMIT") { 10240 } ).to_i # MiB, default is 10 GiB
+    # config.frequency     = 300 # seconds
+    # config.percent_usage = 0.99
     config.rolling_restart_frequency = ( ENV.fetch("PUMA_WORKER_RESTART_HOURS") { 12 } ).to_i.hours
   end
-  PumaWorkerKiller.start
+  # checks memory usage and kill processes as needed
+  # PumaWorkerKiller.start
+  
+  # but checking memory usage in a container is dubious! so using rolling restarts for now
+  PumaWorkerKiller.enable_rolling_restart
 end
 
 worker_timeout 3600 # 1 hour timeout, to match Apache timeout

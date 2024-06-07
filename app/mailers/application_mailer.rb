d6284f565122cfb4cb155e3b82522d28a148cb7f
Simon Choy <simon.choy@duke.edu>
2024-06-07 14:44:38 -0400

    changed sender to morphosource.do.not.reply@duke.edu

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'do.not.reply@morphosource.org'
+  default from: 'morphosource.do.not.reply@duke.edu'
   layout 'mailer'
 
   def send_email_with_attachment(email_address, subject, body, attachment_path)

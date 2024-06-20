2feaa70fcac7e86e1e3e4b7622c90f177b656602
Julie Winchester <julie.winchester@duke.edu>
2024-06-20 18:27:41 -0500

    Change solr scripts

--- bin/solrcloud-assign-configset.sh
+++ bin/solrcloud-assign-configset.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 COUNTER=0;
 
-# if [ "$SOLR_ADMIN_USER" ]; then
-#   solr_user_settings="--user $SOLR_ADMIN_USER:$SOLR_ADMIN_PASSWORD"
-# fi
+if [ "$SOLR_ADMIN_USER" ]; then
+  solr_user_settings="--user $SOLR_ADMIN_USER:$SOLR_ADMIN_PASSWORD"
+fi
 
 solr_config_name="${SOLR_CONFIGSET_NAME:-solrconfig}"
 solr_collection_name="${SOLR_COLLECTION_NAME:-hyrax}"
@@ -14,15 +14,15 @@ solr_collection_modify_url="$SOLR_HOST:$SOLR_PORT/solr/admin/collections?action=
 
 while [ $COUNTER -lt 30 ]; do
   if nc -z "${SOLR_HOST}" "${SOLR_PORT}"; then
-    if curl --silent "$solr_collection_list_url" | grep -q "$solr_collection_name"; then
+    if curl --silent $solr_user_settings "$solr_collection_list_url" | grep -q "$solr_collection_name"; then
       echo "-- Collection ${solr_collection_name} exists; setting ${solr_config_name} ConfigSet ..."
       echo $solr_collection_modify_url
-      curl "$solr_collection_modify_url"
+      curl $solr_user_settings "$solr_collection_modify_url"
       exit
     else
       echo "-- Collection ${solr_collection_name} does not exist; creating and setting ${solr_config_name} ConfigSet ..."
       solr_collection_create_url="$SOLR_HOST:$SOLR_PORT/solr/admin/collections?action=CREATE&name=${solr_collection_name}&collection.configName=${solr_config_name}&numShards=1"
-      curl "$solr_collection_create_url"
+      curl $solr_user_settings "$solr_collection_create_url"
       exit
     fi
   fi

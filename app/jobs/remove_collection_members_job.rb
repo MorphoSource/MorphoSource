6f2d25242f1d8808439b20b261641743b11570a5
Simon Choy <simon.choy@duke.edu>
2024-07-02 12:55:48 -0400

    re-index physical object in model after save, get PO id from solr doc

--- app/jobs/remove_collection_members_job.rb
+++ app/jobs/remove_collection_members_job.rb
@@ -23,14 +23,7 @@ class RemoveCollectionMembersJob < Hyrax::ApplicationJob
       end
     else
       c.remove_member_objects member_ids
-      update_physical_object_index member_ids.first
     end
-  end
-
-  def update_physical_object_index(id)
-    member = Media.find(id)
-    object_id = member.physical_object_id
-    ActiveFedora::Base.where(id: object_id).first.try(:update_index) if object_id.present?
   end
 
 end

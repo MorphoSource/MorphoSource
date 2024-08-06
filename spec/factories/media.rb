a8c07cc4d503c5bcca4d0a9eb7cf33122041c44f
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2024-08-06 17:02:24 -0500

    add factorybehavior

--- spec/factories/media.rb
+++ spec/factories/media.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :media, aliases: [:private_media] do
+    # MorphoSource FactoryBehavior methods
+    # see config/initializers/factory_bot.rb
+    after_create_work # provides find methods for work
+
     title { ["example media title"] }
     visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE }
     fileset_visibility { ["restricted"] }
@@ -8,12 +12,20 @@ FactoryBot.define do
   end
 
   factory :public_media, aliases: [:open_media], :parent => :media do
+    # MorphoSource FactoryBehavior methods
+    # see config/initializers/factory_bot.rb
+    after_create_work # provides find methods for work
+
     visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
     fileset_visibility { ["open"] }
     fileset_accessibility { ["open"] }
   end
 
   factory :restricted_media, :parent => :media do
+    # MorphoSource FactoryBehavior methods
+    # see config/initializers/factory_bot.rb
+    after_create_work # provides find methods for work
+
     visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
     fileset_visibility { ["open"] }
     fileset_accessibility { ["restricted_download"] }

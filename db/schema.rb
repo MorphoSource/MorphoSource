ed481994a3f38afa760976cc2095a92f80d31914
Julie Winchester <julie.winchester@duke.edu>
2024-09-01 15:06:22 -0500

    Change UploadedFiles total_file_size from integer to bigint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_27_221403) do
+ActiveRecord::Schema.define(version: 2024_09_01_195803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -683,7 +683,7 @@ ActiveRecord::Schema.define(version: 2024_08_27_221403) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "upload_hash"
-    t.integer "total_file_size"
+    t.bigint "total_file_size"
     t.index ["file_set_uri"], name: "index_uploaded_files_on_file_set_uri"
     t.index ["user_id"], name: "index_uploaded_files_on_user_id"
   end

29e6faa7a36b28b034a779b9f3e51fdf91295e62
Julie Winchester <julie.winchester@duke.edu>
2024-09-01 16:43:29 -0500

    Set chunk size at 50M

--- app/assets/javascripts/hyrax/ms_upload.js
+++ app/assets/javascripts/hyrax/ms_upload.js
@@ -34,7 +34,7 @@ $( document ).ready(function() {
     });
 
     var options = {
-      maxChunkSize: 10000000,
+      maxChunkSize: 50000000,
       maxFileSize: 100000000000,
       maxNumberOfFiles: 1,
       maxRetries: 5,

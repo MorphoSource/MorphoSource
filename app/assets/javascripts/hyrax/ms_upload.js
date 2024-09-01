8c7d9d91da0572929b3abe919719ad1c525ca53a
Julie Winchester <julie.winchester@duke.edu>
2024-09-01 16:09:54 -0500

    Reduce upload chunk size to 10 megabytes

--- app/assets/javascripts/hyrax/ms_upload.js
+++ app/assets/javascripts/hyrax/ms_upload.js
@@ -34,7 +34,7 @@ $( document ).ready(function() {
     });
 
     var options = {
-      maxChunkSize: 100000000,
+      maxChunkSize: 10000000,
       maxFileSize: 100000000000,
       maxNumberOfFiles: 1,
       maxRetries: 5,

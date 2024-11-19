b155f97f6b7c66e14fa6626d387cbf75947fdf0a
Julie Winchester <julie.winchester@duke.edu>
2024-11-19 15:51:34 -0600

    Update database and migrations

--- db/migrate/20190916185140_remove_media_cart_references.rb
+++ db/migrate/20190916185140_remove_media_cart_references.rb
@@ -1,6 +1,6 @@
 class RemoveMediaCartReferences < ActiveRecord::Migration[5.1]
   def change
-    remove_column :users, :media_cart_id, :integer
+    remove_column :users, :media_cart_id, :integer, if_exists: true
     remove_column :cart_items, :media_cart_id, :integer
     add_column :cart_items, :user_id, :integer
   end

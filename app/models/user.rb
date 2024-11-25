01055d977e7d3239955c55fa2c5fb8170425097e
Julie Winchester <julie.winchester@duke.edu>
2024-11-25 16:40:16 -0600

    update_attributes is deprecated, replaced with update

--- app/models/user.rb
+++ app/models/user.rb
@@ -283,11 +283,11 @@ class User < ApplicationRecord
   end
 
   def make_active
-    update_attributes(active: true)
+    update(active: true)
   end
 
   def make_inactive
-    update_attributes(active: false)
+    update(active: false)
   end
 
   # true if user has download access or an approved cart item

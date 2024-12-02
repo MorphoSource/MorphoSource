66bc6403c9ace43bc5d1e2915fa289050f3f868b
Julie Winchester <julie.winchester@duke.edu>
2024-12-02 16:48:55 -0600

    Fix spec/search_builders/morphosource/my/collections/media_lists_search_builder_spec.rb

--- spec/search_builders/morphosource/my/collections/media_lists_search_builder_spec.rb
+++ spec/search_builders/morphosource/my/collections/media_lists_search_builder_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Morphosource::My::Collections::MediaListsSearchBuilder do
-  let(:scope)                       { double('Scope') }
-  let(:builder)                     { described_class.new(scope: scope) }
+  let(:scope)   { double('Scope', blacklight_config: CatalogController.blacklight_config) }
+  let(:builder) { described_class.new(scope) }
 
   describe 'collection_types' do
     it { expect(builder.collection_types).to match_array([media_list_collection_type]) }

a8926e169cf3cd7473e9989ccb8d5c92fa603c59
Simon Choy <simon.choy@duke.edu>
2024-12-06 16:12:47 -0500

    fixed tests

--- spec/services/morphosource/cw_attachment_service_spec.rb
+++ spec/services/morphosource/cw_attachment_service_spec.rb
@@ -1,31 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Morphosource::CwAttachmentService do
-  let(:model) { ProcessingEvent.create }
-  let(:file) { Rack::Test::UploadedFile.new('spec/fixtures/text/text.txt', 'text/plain') }
-  let(:field_name) { "processing_event_attachment" }
 
-  describe '.create' do
-    it 'creates an attachment' do
-byebug
-      url = Morphosource::CwAttachmentService.create(model, field_name, file)
-      expect(model.reload.processing_event_attachment).to eq(url)
+  describe 'ProcessingEvent attachment' do
+    let(:pe) { ProcessingEvent.create }
+    let(:file) { Rack::Test::UploadedFile.new('spec/fixtures/text/text.txt', 'text/plain') }
+    let(:field_name) { "processing_event_attachment" }
+    let(:expected_url) { "/uploads/processing_event/processing_event_attachment/text.txt" }
+
+    describe '.create' do
+      it 'creates an attachment' do
+        Morphosource::CwAttachmentService.create(pe, field_name, file, Morphosource.attachment_formats, true)
+        expect(pe.processing_event_attachment).to eq(expected_url)
+      end
+
+      it 'raises an error for unsupported file formats' do
+        invalid_file = Rack::Test::UploadedFile.new('spec/fixtures/ms.zip', 'application/zip')
+        expect {
+          Morphosource::CwAttachmentService.create(pe, field_name, invalid_file, Morphosource.attachment_formats, true)
+        }.to raise_error(ArgumentError, /Invalid file format/)
+      end
     end
 
-    it 'raises an error for unsupported file formats' do
-      invalid_file = Rack::Test::UploadedFile.new('spec/fixtures/ms.zip', 'application/zip')
-      expect {
-        Morphosource::CwAttachmentService.create(model, field_name, invalid_file, ['.txt'])
-      }.to raise_error(ArgumentError, /Invalid file format/)
+    describe '.delete' do
+      it 'deletes an attachment' do
+        Morphosource::CwAttachmentService.create(pe, field_name, file)
+        expect {
+          Morphosource::CwAttachmentService.delete(pe, field_name)
+        }.to change { pe.processing_event_attachment }.to(nil)
+      end
     end
   end
 
-  describe '.delete' do
-    it 'deletes an attachment' do
-      Morphosource::CwAttachmentService.create(model, field_name, file)
-      expect {
-        Morphosource::CwAttachmentService.delete(model, field_name)
-      }.to change { model.reload.processing_event_attachment }.to(nil)
-    end
-  end
 end

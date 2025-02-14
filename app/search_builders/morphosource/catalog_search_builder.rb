6328843593b9337e68d75590dcc46444556f0fb7
Simon Choy <simon.choy@duke.edu>
2025-02-14 17:03:09 -0500

    handle rights_statement, license with contains_title param

--- app/search_builders/morphosource/catalog_search_builder.rb
+++ app/search_builders/morphosource/catalog_search_builder.rb
@@ -10,40 +10,49 @@ module Morphosource
       facet_config = blacklight_config.facet_fields[facet]
       contains = blacklight_params[request_keys[:contains]]
       contains_title = blacklight_params[:'facet.containsTitle']
-  
       if contains_title.present?
+        # for certain facet keys (e.g. license, rights_statement), lookup an existing source (e.g. Yaml file) to find matching IDs
         case facet_config.key
-        when "license"
-          query_values = mapped_values( Hyrax.config.license_service_class.new, contains_title)
-        when "rights_statement"
-          query_values = mapped_values( Hyrax.config.rights_statement_service_class.new, contains_title)
-        when "device"
+        when 'license'
+          custom_list = YAML.load_file(Rails.root.join('config', 'authorities', 'licenses.yml'))
+          matching_terms = custom_list['terms'].select { |term| term['term'].downcase.include?(contains_title.downcase) }
+          matching_ids = matching_terms.map { |term| term['id'] }
+          if matching_ids.any?
+            solr_params[:fq] ||= []
+            solr_params[:fq] << "{!terms f=#{facet_config.field}}#{matching_ids.join(',')}"
+          else
+            solr_params[:fq] ||= []
+            solr_params[:fq] << "{!frange l=1 u=0}1"
+          end
+        when 'rights_statement'
+          rights_statements = YAML.load_file(Rails.root.join('config', 'authorities', 'rights_statements.yml'))
+          matching_terms = rights_statements['terms'].select { |term| term['term'].downcase.include?(contains_title.downcase) }
+          matching_ids = matching_terms.map { |term| term['id'] }
+          if matching_ids.any?
+            solr_params[:fq] ||= []
+            solr_params[:fq] << "{!terms f=#{facet_config.field}}#{matching_ids.join(',')}"
+          else
+            solr_params[:fq] ||= []
+            solr_params[:fq] << "{!frange l=1 u=0}1"
+          end          
+        else
           # Perform a lookup on the Solr title field to find matching IDs
           response = fetch_ids_by_title_and_model(contains_title, facet_config.key)
-          query_values = response['response']['docs'].map { |doc| doc['id'] }
-        else
-          # unknown facet key
-          query_values = nil
-        end
-        if query_values.any?
-          # facet.contains does not support multiple values.  Use filter query (fq) instead
-          solr_params[:fq] ||= []
-          solr_params[:fq] << "{!terms f=#{facet_config.field}}#{query_values.join(',')}"
-        else
-          # If no query_values, add an always-false filter query to ensure no results are returned
-          solr_params[:fq] ||= []
-          solr_params[:fq] << "{!frange l=1 u=0}1"
+          matching_ids = response['response']['docs'].map { |doc| doc['id'] }
+          if matching_ids.any?
+            # facet.contains does not support multiple values.  Use filter query (fq) instead
+            solr_params[:fq] ||= []
+            solr_params[:fq] << "{!terms f=#{facet_config.field}}#{matching_ids.join(',')}"
+          else
+            # If no matching_ids, add an always-false filter query to ensure no results are returned
+            solr_params[:fq] ||= []
+            solr_params[:fq] << "{!frange l=1 u=0}1"
+          end
         end
       elsif contains.present?
         solr_params[:"f.#{facet_config.field}.facet.contains"] = contains
         solr_params[:"f.#{facet_config.field}.facet.contains.ignoreCase"] = true
       end
-    end
-
-    def mapped_values(select_service, contains_value) 
-      options = select_service.select_active_options
-      matching_terms = options.select { |term| term[0].downcase.include?(contains_value.downcase) }
-      matching_ids = matching_terms.map { |term| term[1] }
     end
 
     private

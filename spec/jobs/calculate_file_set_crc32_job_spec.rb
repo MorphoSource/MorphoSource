cca2c84ae2444ba302df52624f1db7c50ccd3c40
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2025-04-08 10:11:27 -0400

    prevent test from failing if remote file is unavailable

--- spec/jobs/calculate_file_set_crc32_job_spec.rb
+++ spec/jobs/calculate_file_set_crc32_job_spec.rb
@@ -7,17 +7,21 @@ RSpec.describe CalculateFileSetCrc32Job do
 
   describe 'perform' do
     context 'file is remote' do
-      # image from duke digital collections: https://repository.duke.edu/dc/hmp/hmpgp21657
-      let(:file_path)  { "https://repository.duke.edu/iiif/ark:%2F87924%2Fr3ft8h245/full/200,/0/default.jpg" }
-      let(:file_set)   { FileSet.create(import_url: file_path) }
+      # thumbnail for https://www.morphosource.org/concern/media/000530653?locale=en
+      let(:file_path) { "https://www.morphosource.org/downloads/000530688853?file=thumbnail&t=1686760977"}
+      let(:file_set)  { FileSet.create(import_url: file_path) }
 
       before do
         Hydra::Works::AddExternalFileToFileSet.call(file_set, file_set.import_url, :original_file, update_existing: true, versioning: false)
       end
 
+      # Don't fail if the file is not available
       it 'returns the correct crc32' do
         described_class.perform_now(file_set.id)
-        expect(file_set.reload.crc32).to eq([1662178079])
+        expect(file_set.reload.crc32).to eq([1579858954])
+      rescue OpenURI::HTTPError => e
+        pending("Error accessing remote file: #{file_path}")
+        raise e
       end
     end
 

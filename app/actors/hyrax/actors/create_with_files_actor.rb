fc0027fb74fc1460a6dce8a535ff782ea7bcd0be
Simon Choy <simon.choy@duke.edu>
2025-04-21 18:14:55 -0400

    migrated 5.0.1 changes to CreateWithFilesActor

--- app/actors/hyrax/actors/create_with_files_actor.rb
+++ app/actors/hyrax/actors/create_with_files_actor.rb
@@ -7,7 +7,12 @@ module Hyrax
       def create(env)
         uploaded_file_ids = filter_file_ids(env.attributes.delete(:uploaded_files))
         files = uploaded_files(uploaded_file_ids)
-        validate_files(files, env) && next_actor.create(env) && attach_files(files, env)
+        # get a current copy of attributes, to protect against future mutations
+        attributes        = env.attributes.clone
+
+        validate_files(files, env) &&
+          next_actor.create(env) &&
+          attach_files(files, env.curation_concern, attributes)
       end
 
       # @param [Hyrax::Actors::Environment] env
@@ -15,7 +20,12 @@ module Hyrax
       def update(env)
         uploaded_file_ids = filter_file_ids(env.attributes.delete(:uploaded_files))
         files = uploaded_files(uploaded_file_ids)
-        validate_files(files, env) && next_actor.update(env) && attach_files(files, env)
+        # get a current copy of attributes, to protect against future mutations
+        attributes        = env.attributes.clone
+
+        validate_files(files, env) &&
+          next_actor.update(env) &&
+          attach_files(files, env.curation_concern, attributes)
       end
 
       private
@@ -37,13 +47,13 @@ module Hyrax
         end
 
         # @return [TrueClass]
-        def attach_files(files, env)
-          if env.curation_concern.media? && env.curation_concern.is_remote_backed?
+        def attach_files(files, curation_concern, attributes)
+            if curation_concern.media? && curation_concern.is_remote_backed?
             # if remote file url is empty or same as the current one, no need to call AttachFilesToWorkJob
             if (
-              !env.curation_concern.remote_origin_url.present? ||
-              (env.curation_concern.remote_origin_url == env.curation_concern.file_sets&.first&.import_url) ||
-              env.curation_concern.remote_manifest_url.present?
+              !curation_concern.remote_origin_url.present? ||
+              (curation_concern.remote_origin_url == curation_concern.file_sets&.first&.import_url) ||
+              curation_concern.remote_manifest_url.present?
             )
               return true
             end
@@ -51,7 +61,7 @@ module Hyrax
             # this block is for other work type (e.g. PE) and media that is not remote-backed (which includes local/cloud upload)
             return true if files.blank?
           end
-          AttachFilesToWorkJob.perform_later(env.curation_concern, files, env.attributes.to_h.symbolize_keys)
+          AttachFilesToWorkJob.perform_later(curation_concern, files, **attributes.to_h.symbolize_keys)
           true
         end
 

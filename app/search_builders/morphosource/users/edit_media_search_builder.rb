e2c594233b2da898681150ff4e9bd700411e2787
Simon Choy <simon.choy@duke.edu>
2025-05-29 14:00:06 -0400

    call SearchBuilderBehavior in other search builders

--- app/search_builders/morphosource/users/edit_media_search_builder.rb
+++ app/search_builders/morphosource/users/edit_media_search_builder.rb
@@ -2,8 +2,8 @@
 module Morphosource
   module Users
     class EditMediaSearchBuilder < Hyrax::WorksSearchBuilder
-      include Hyrax::My::SearchBuilderBehavior
       include Morphosource::SearchBuilderBehavior
+      include Hyrax::My::SearchBuilderBehavior
       # enable f.field facet format
       include Morphosource::Facets::SearchBuilderFacetParamsBehavior
 
--- app/search_builders/morphosource/users/my_media_search_builder.rb
+++ app/search_builders/morphosource/users/my_media_search_builder.rb
@@ -2,11 +2,11 @@
 module Morphosource
   module Users
     class MyMediaSearchBuilder < Hyrax::WorksSearchBuilder
+      include Morphosource::SearchBuilderBehavior
       # filter_collection_facet_for_access
       include Morphosource::Facets::CollectionsSearchBuilderBehavior
       # enable f.field facet format
       include Morphosource::Facets::SearchBuilderFacetParamsBehavior
-      include Morphosource::SearchBuilderBehavior
 
       self.default_processor_chain -= [:add_access_controls_to_solr_params]
       self.default_processor_chain += [:apply_read_edit_filters, :filter_collection_facet_for_access]

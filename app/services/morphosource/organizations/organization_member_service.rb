a9c062ca7ad1218d83af03178929cb42e6523353
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2025-06-10 10:03:57 -0500

    update repository references

--- app/services/morphosource/organizations/organization_member_service.rb
+++ app/services/morphosource/organizations/organization_member_service.rb
@@ -4,7 +4,7 @@ module Morphosource
       include Blacklight::AccessControls::Catalog
       include Blacklight::Base
       include SolrHelper
-    
+
       attr_reader :solr
 
       copy_blacklight_config_from(::CatalogController)
@@ -50,18 +50,18 @@ module Morphosource
           "(organization_id_ssim:#{@organization.id})"
         end
       end
-  
+
       private
 
         def available_member_works_filter_query(fq_params: [], object_model: nil)
           query_solr_with_fq(
-            query_builder: works_search_builder, 
-            query_params: @params[:cq], 
+            query_builder: works_search_builder,
+            query_params: @params[:cq],
             fq_params: fq_params,
-            rows: (rows = rows_param(object_model)), 
+            rows: (rows = rows_param(object_model)),
             start: start_param(rows)
           )
-        end  
+        end
 
         def query_solr_with_fq(query_builder:, query_params:, fq_params:, rows: 10, start: 0)
           initial_q = query_builder[:q]
@@ -74,7 +74,7 @@ module Morphosource
             query_builder.merge('facet.limit' => -1)
             query_builder.rows = rows
             query_builder.start = start
-            repository.search(query_builder.query)
+            blacklight_config.repository.search(query_builder.query)
           ensure
             query_builder.merge(q: initial_q)
             query_builder.merge(fq: initial_fq)

2412c1d2e819892942c7aefaff19491bf8003656
Julie Winchester <julia.m.winchester@gmail.com>
2025-06-16 12:15:58 -0500

    Apply suggestions from code review

--- app/indexers/organization_collection_indexer.rb
+++ app/indexers/organization_collection_indexer.rb
@@ -5,7 +5,7 @@ class OrganizationCollectionIndexer < Hyrax::CollectionWithBasicMetadataIndexer
       solr_doc['media_ownership_transfer_bsi'] = object.media_ownership_transfer
       solr_doc['generic_type_sim'] = ['Collection']
       solr_doc['title_ssi'] = object.title.first
-      solr_doc['date_modified_dtsi'] = object.modified_date if object.modified_date.present?
+      solr_doc['date_modified_dtsi'] = object.modified_date
       solr_doc['display_name_ssi'] = object.display_name
       solr_doc['ark_tesim'] = object.ark
       solr_doc['ark_ssim'] = object.ark

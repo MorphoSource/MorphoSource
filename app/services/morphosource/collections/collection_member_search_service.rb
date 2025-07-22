f4a774b08dd108dfeb14e01e1988ad7990713fed
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2025-07-22 15:55:59 -0500

    remove query_solr_for_media_with_fq

--- app/services/morphosource/collections/collection_member_search_service.rb
+++ app/services/morphosource/collections/collection_member_search_service.rb
@@ -5,27 +5,6 @@ module Morphosource
 
       private
 
-      def query_solr_for_media_with_fq(query_builder:, query_params:, fq_params:, core_fq:, rows: 10, start: 0)
-        initial_q = query_builder[:q]
-        initial_fq = query_builder[:fq]
-        initial_rows = query_builder.rows
-        initial_start = query_builder.start
-        begin
-          query_builder.merge(q: query_params)
-          query_builder.merge(fq: prepare_media_query_fq_param(initial_fq, fq_params, core_fq))
-          query_builder.merge('facet.limit' => -1)
-          query_builder.rows = rows
-          query_builder.start = start
-          blacklight_config.repository.search(query_builder.query)
-        ensure
-          query_builder.merge(q: initial_q)
-          query_builder.merge(fq: initial_fq)
-          query_builder.merge('facet.limit' => -1)
-          query_builder.rows = initial_rows
-          query_builder.start = initial_start
-        end
-      end
-
       def prepare_media_query_fq_param(initial, new, new_core)
         ((initial.map { |x| x == std_core_fq && new_core.present? ? new_core : x }) + new).uniq
       end

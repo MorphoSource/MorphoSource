53b58f3b135cc46dc8d5712c8758a20499afd44b
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2025-07-22 15:56:54 -0500

    remove prepare_media_query_fq_param

--- app/services/morphosource/collections/collection_member_search_service.rb
+++ app/services/morphosource/collections/collection_member_search_service.rb
@@ -5,10 +5,6 @@ module Morphosource
 
       private
 
-      def prepare_media_query_fq_param(initial, new, new_core)
-        ((initial.map { |x| x == std_core_fq && new_core.present? ? new_core : x }) + new).uniq
-      end
-
       def std_core_fq
         "#{ActiveFedora.index_field_mapper.solr_name('member_of_collection_ids', :symbol)}:#{collection.id}"
       end

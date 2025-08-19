197fef8ba9e384ab5ab4ff34c59606d42ce780ec
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2025-08-19 11:15:54 -0400

    remove extra user groups

--- spec/factories/solr/collections/media_list_document.rb
+++ spec/factories/solr/collections/media_list_document.rb
@@ -10,7 +10,7 @@ FactoryBot.define do
     sequence(:id, 110000) { |n| n.to_s.rjust(9, "0") } # sequence ids starting at '000110000'
     initialize_with { new(MEDIA_LIST_DOC_ATTRIBUTES.merge({'id': id,collection_type_gid_ssim: [Hyrax::CollectionType.find_or_create_by(Morphosource::CollectionTypes::MediaLists::SETTINGS).to_global_id.uri],
                                                         'edit_access_group_ssim': ["#{id}_managers", 'admin'],
-                                                        'read_access_group_ssim': ["#{id}_viewers", "#{id}_editors", "#{id}_downloaders", "#{id}_depositors", 'public']
+                                                        'read_access_group_ssim': ["#{id}_viewers", 'public']
                                                        }).merge(attributes)) }
     to_create { |instance| ActiveFedora::SolrService.add(instance.to_h, softCommit: true)}
   end

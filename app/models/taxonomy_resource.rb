41b153238ed7f803cb96f3f42ba30d045ae193a2
Julie Winchester <julie.winchester@duke.edu>
2025-09-25 14:01:00 -0500

    Add Taxonomy model methods to TaxonomyResource

--- app/models/taxonomy_resource.rb
+++ app/models/taxonomy_resource.rb
@@ -5,4 +5,32 @@
 class TaxonomyResource < Hyrax::Work
   include Hyrax::Schema(:basic_metadata)
   include Hyrax::Schema(:taxonomy_resource)
+
+  def short_title
+    ranks = [:taxonomy_genus, :taxonomy_subgenus, :taxonomy_species, :taxonomy_subspecies]
+    title = ranks.map { |rank| self.send(rank).first }.compact.join(' ')
+    return title unless title.blank?
+    self.title.first
+  end
+
+  def contributing_user
+    user = ::User.find_by_user_key(depositor)
+  end
+
+  def gbif_ranks
+    [:taxonomy_kingdom, :taxonomy_phylum, :taxonomy_class, :taxonomy_order, :taxonomy_family, :taxonomy_genus, :taxonomy_species, :taxonomy_subspecies]
+  end
+
+  def gbif_taxonomy_array
+    gbif_ranks.map { |rank| self.send(rank)&.first }.compact
+  end
+
+  # ToDoValk: Replace with persister method when BiologicalSpecimen gets Valkyrized
+  def objects
+    BiologicalSpecimen.where(taxonomy_id: id).to_a
+  end
+
+  def media
+    objects.map(&:media).flatten
+  end
 end

3090f68b0f84b0ed745adc84dc2a9b11d8409fc2
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2025-10-10 12:35:04 -0400

    wip

--- app/models/media.rb
+++ app/models/media.rb
@@ -356,42 +356,42 @@ class Media < Morphosource::Works::Base
   # Persistent identifier methods
   #
 
-  # def mint_doi(target_url)
-  #   if self.doi.empty?
-  #     depositor_user_or_org = User.find_by(ms_id: self.user_with_ownership) ||
-  #       OrganizationCollection.where(id: self.user_with_ownership)&.first
-  #     if !depositor_user_or_org.present?
-  #       Rails.logger.error "Failed to mint DOI for media #{self.id} because depositor user or organization not found"
-  #     end
+  def mint_doi(target_url)
+    if self.doi.empty?
+      depositor_user_or_org = User.find_by(ms_id: self.user_with_ownership) ||
+        OrganizationCollection.where(id: self.user_with_ownership)&.first
+      if !depositor_user_or_org.present?
+        Rails.logger.error "Failed to mint DOI for media #{self.id} because depositor user or organization not found"
+      end
 
-  #     depositor_params = if depositor_user_or_org.is_a?(User)
-  #       depositor_user_name_components = depositor_user_or_org.display_name.split(' ')
-  #       {
-  #         'author_first' => depositor_user_name_components.first,
-  #         'author_last' => depositor_user_name_components.drop(1).join(' ')
-  #       }
-  #     elsif depositor_user_or_org.is_a?(OrganizationCollection)
-  #       { 'organization' => depositor_user_or_org.display_name }
-  #     else
-  #       { }
-  #     end
+      depositor_params = if depositor_user_or_org.is_a?(User)
+        depositor_user_name_components = depositor_user_or_org.display_name.split(' ')
+        {
+          'author_first' => depositor_user_name_components.first,
+          'author_last' => depositor_user_name_components.drop(1).join(' ')
+        }
+      elsif depositor_user_or_org.is_a?(OrganizationCollection)
+        { 'organization' => depositor_user_or_org.display_name }
+      else
+        { }
+      end
 
-  #     minted_doi = Morphosource::CrossrefDoiMinter.mint_doi( self.id,
-  #                                                           {
-  #                                                             'title' => self.title.first,
-  #                                                             'url' => target_url,
-  #                                                             'resource_type' => self.media_type.first
-  #                                                           }.merge(depositor_params) )
-  #     if minted_doi.present?
-  #       # minted_doi may be an exception if mint_doi failed
-  #       unless minted_doi.respond_to?(:message)
-  #         self.doi = [minted_doi]
-  #         self.save
-  #       end
-  #     end
-  #     return minted_doi
-  #   end
-  # end
+      minted_doi = Morphosource::CrossrefDoiMinter.mint_doi( self.id,
+                                                            {
+                                                              'title' => self.title.first,
+                                                              'url' => target_url,
+                                                              'resource_type' => self.media_type.first
+                                                            }.merge(depositor_params) )
+      if minted_doi.present?
+        # minted_doi may be an exception if mint_doi failed
+        unless minted_doi.respond_to?(:message)
+          self.doi = [minted_doi]
+          self.save
+        end
+      end
+      return minted_doi
+    end
+  end
 
   #
   # Methods for persisting and monitoring save around work updates

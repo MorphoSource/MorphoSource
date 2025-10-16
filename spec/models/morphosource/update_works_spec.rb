7e18f65e2a8465e54823ca20f6aba30c1c02826c
Julie Winchester <julie.winchester@duke.edu>
2025-10-16 12:10:56 -0500

    Fix and update tests

--- spec/models/morphosource/update_works_spec.rb
+++ spec/models/morphosource/update_works_spec.rb
@@ -3,8 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe Morphosource::Works::Base do
+  let(:user)                      { User.create(email: 'email@email.com', password: 'password') }
+  let(:contributors)              { Role.create(name: 'contributor') }
 
-  include_context 'update works'
+  let(:organization)              { Organization.create(title: ['old title']) }
+  let(:taxonomy)                  { valkyrie_create(:taxonomy_resource, title: ['old title']) }
+  let(:specimen)                  { BiologicalSpecimen.create(title: ['old title'], vouchered: ['Yes'], organization_id: [organization.id], taxonomy_id: [taxonomy.id.to_s]) }
+  let(:device)                    { Device.create(title: ['device'], modality: ["MagneticResonanceImaging"])}
+  let(:imaging_event)             { ImagingEvent.create(title: ['old title'], ie_modality: device.modality, device_id: [device.id], physical_object_id: [specimen.id]) }
+  let(:media1)                    { Media.create(title: ['old title']) }
+  let(:processing_event)          { ProcessingEvent.create(title: ['old title']) }
+  let(:media2)                    { Media.create(title: ['old title']) }
+  let(:specimen_works)            { [imaging_event, media1, processing_event, media2] }
+
+  let(:old_organization_doc)      { SolrDocument.find(organization.id) }
+  let(:old_taxonomy_doc)          { SolrDocument.find(taxonomy.id.to_s) }
+  let(:old_specimen_doc)          { SolrDocument.find(specimen.id) }
+  let(:old_imaging_event_doc)     { SolrDocument.find(imaging_event.id) }
+  let(:old_media1_doc)            { SolrDocument.find(media1.id) }
+  let(:old_processing_event_doc)  { SolrDocument.find(processing_event.id) }
+  let(:old_media2_doc)            { SolrDocument.find(media2.id) }
+
+  let(:old_docs)                  { [old_organization_doc, old_taxonomy_doc, old_specimen_doc, old_imaging_event_doc, old_media1_doc, old_processing_event_doc, old_media2_doc] }
+
+  let(:new_organization_doc)      { SolrDocument.find(organization.id) }
+  let(:new_taxonomy_doc)          { SolrDocument.find(taxonomy.id.to_s) }
+  let(:new_specimen_doc)          { SolrDocument.find(specimen.id) }
+  let(:new_imaging_event_doc)     { SolrDocument.find(imaging_event.id) }
+  let(:new_media1_doc)            { SolrDocument.find(media1.id) }
+  let(:new_processing_event_doc)  { SolrDocument.find(processing_event.id) }
+  let(:new_media2_doc)            { SolrDocument.find(media2.id) }
+
+  before do
+    imaging_event.members << media1
+    media1.members << processing_event
+    processing_event.members << media2
+    specimen_works.each(&:save)
+  end
 
   describe 'updating the specimen index record' do
 
--- spec/models/taxonomy_spec.rb
+++ spec/models/taxonomy_spec.rb
@@ -2,78 +2,127 @@
 #  `rails generate hyrax:work Taxonomy`
 require 'rails_helper'
 
+# @deprecated Taxonomy spec tests should return errors.
 RSpec.describe Taxonomy do
 
   describe "valid work relationships" do
 
-    it "has no valid parents" do
-      expect(subject.valid_parent_concerns).to match_array([])
+    it "has no valid parents @deprecated ReadOnlyRecord" do
+      # expect(subject.valid_parent_concerns).to match_array([])
+      expect { subject.valid_parent_concerns }.to raise_error(ActiveFedora::ReadOnlyRecord)
     end
 
-    it "has no valid child concerns" do
-      expect(subject.valid_child_concerns).to match_array([])
+    it "has no valid child concerns @deprecated ReadOnlyRecord" do
+      # expect(subject.valid_child_concerns).to match_array([])
+      expect{ subject.valid_child_concerns }.to raise_error(ActiveFedora::ReadOnlyRecord)
     end
   end
 
   describe "instance" do
-    subject { described_class.new }
+    it "is valid with valid attributes @deprecated ReadOnlyRecord" do
+      # subject.title = ["title"]
+      # subject.taxonomy_domain = ["domain"]
+      # subject.taxonomy_kingdom = ["kingdom"]
+      # subject.taxonomy_phylum = ["phylum"]
+      # subject.taxonomy_superclass = ["superclass"]
+      # subject.taxonomy_class = ["class"]
+      # subject.taxonomy_subclass = ["subclass"]
+      # subject.taxonomy_superorder = ["superorder"]
+      # subject.taxonomy_order = ["order"]
+      # subject.taxonomy_suborder = ["suborder"]
+      # subject.taxonomy_superfamily = ["superfamily"]
+      # subject.taxonomy_family = ["family"]
+      # subject.taxonomy_subfamily = ["subfamily"]
+      # subject.taxonomy_tribe = ["tribe"]
+      # subject.taxonomy_genus = ["genus"]
+      # subject.taxonomy_species = ["species"]
+      # subject.taxonomy_subspecies = ["subspecies"]
+      # subject.source = ["some source"]
+      # subject.trusted = ["true"]
+      # subject.gbif_key = ['111111']
+      # expect(subject).to be_valid
 
-    it "is valid with valid attributes" do
-      subject.title = ["title"]
-      subject.taxonomy_domain = ["domain"]
-      subject.taxonomy_kingdom = ["kingdom"]
-      subject.taxonomy_phylum = ["phylum"]
-      subject.taxonomy_superclass = ["superclass"]
-      subject.taxonomy_class = ["class"]
-      subject.taxonomy_subclass = ["subclass"]
-      subject.taxonomy_superorder = ["superorder"]
-      subject.taxonomy_order = ["order"]
-      subject.taxonomy_suborder = ["suborder"]
-      subject.taxonomy_superfamily = ["superfamily"]
-      subject.taxonomy_family = ["family"]
-      subject.taxonomy_subfamily = ["subfamily"]
-      subject.taxonomy_tribe = ["tribe"]
-      subject.taxonomy_genus = ["genus"]
-      subject.taxonomy_species = ["species"]
-      subject.taxonomy_subspecies = ["subspecies"]
-      subject.source = ["some source"]
-      subject.trusted = ["true"]
-      subject.gbif_key = ['111111']
-      expect(subject).to be_valid
+      expect {
+        subject.title = ["title"]
+        subject.taxonomy_domain = ["domain"]
+        subject.taxonomy_kingdom = ["kingdom"]
+        subject.taxonomy_phylum = ["phylum"]
+        subject.taxonomy_superclass = ["superclass"]
+        subject.taxonomy_class = ["class"]
+        subject.taxonomy_subclass = ["subclass"]
+        subject.taxonomy_superorder = ["superorder"]
+        subject.taxonomy_order = ["order"]
+        subject.taxonomy_suborder = ["suborder"]
+        subject.taxonomy_superfamily = ["superfamily"]
+        subject.taxonomy_family = ["family"]
+        subject.taxonomy_subfamily = ["subfamily"]
+        subject.taxonomy_tribe = ["tribe"]
+        subject.taxonomy_genus = ["genus"]
+        subject.taxonomy_species = ["species"]
+        subject.taxonomy_subspecies = ["subspecies"]
+        subject.source = ["some source"]
+        subject.trusted = ["true"]
+        subject.gbif_key = ['111111']
+      }.to raise_error(ActiveFedora::ReadOnlyRecord)
     end
 
-    it "is not valid without required title field" do
-      subject.title = []
-      subject.taxonomy_domain = ["domain"]
-      subject.taxonomy_kingdom = []
-      subject.taxonomy_phylum = ["phylum"]
-      subject.taxonomy_superclass = ["superclass"]
-      subject.taxonomy_class = []
-      subject.taxonomy_subclass = ["subclass"]
-      subject.taxonomy_superorder = ["superorder"]
-      subject.taxonomy_order = []
-      subject.taxonomy_suborder = ["suborder"]
-      subject.taxonomy_superfamily = ["superfamily"]
-      subject.taxonomy_family = ["family"]
-      subject.taxonomy_subfamily = ["subfamily"]
-      subject.taxonomy_tribe = []
-      subject.taxonomy_genus = []
-      subject.taxonomy_species = ["species"]
-      subject.taxonomy_subspecies = ["subspecies"]
-      subject.source = ["some source"]
-      subject.trusted = ["true"]
-      subject.gbif_key = ['111111']
-      expect(subject).to_not be_valid
+    it "is not valid without required title field @deprecated ReadOnlyRecord" do
+      # subject.title = []
+      # subject.taxonomy_domain = ["domain"]
+      # subject.taxonomy_kingdom = []
+      # subject.taxonomy_phylum = ["phylum"]
+      # subject.taxonomy_superclass = ["superclass"]
+      # subject.taxonomy_class = []
+      # subject.taxonomy_subclass = ["subclass"]
+      # subject.taxonomy_superorder = ["superorder"]
+      # subject.taxonomy_order = []
+      # subject.taxonomy_suborder = ["suborder"]
+      # subject.taxonomy_superfamily = ["superfamily"]
+      # subject.taxonomy_family = ["family"]
+      # subject.taxonomy_subfamily = ["subfamily"]
+      # subject.taxonomy_tribe = []
+      # subject.taxonomy_genus = []
+      # subject.taxonomy_species = ["species"]
+      # subject.taxonomy_subspecies = ["subspecies"]
+      # subject.source = ["some source"]
+      # subject.trusted = ["true"]
+      # subject.gbif_key = ['111111']
+      # expect(subject).to_not be_valid
+
+      expect {
+        subject.title = []
+        subject.taxonomy_domain = ["domain"]
+        subject.taxonomy_kingdom = []
+        subject.taxonomy_phylum = ["phylum"]
+        subject.taxonomy_superclass = ["superclass"]
+        subject.taxonomy_class = []
+        subject.taxonomy_subclass = ["subclass"]
+        subject.taxonomy_superorder = ["superorder"]
+        subject.taxonomy_order = []
+        subject.taxonomy_suborder = ["suborder"]
+        subject.taxonomy_superfamily = ["superfamily"]
+        subject.taxonomy_family = ["family"]
+        subject.taxonomy_subfamily = ["subfamily"]
+        subject.taxonomy_tribe = []
+        subject.taxonomy_genus = []
+        subject.taxonomy_species = ["species"]
+        subject.taxonomy_subspecies = ["subspecies"]
+        subject.source = ["some source"]
+        subject.trusted = ["true"]
+        subject.gbif_key = ['111111']
+      }.to raise_error(ActiveFedora::ReadOnlyRecord)
     end
 
     describe "valid work relationships" do
 
-      it "has no valid parents" do
-        expect(subject.valid_parent_concerns).to match_array([])
+      it "has no valid parents @deprecated ReadOnlyRecord" do
+        # expect(subject.valid_parent_concerns).to match_array([])
+        expect{ subject.valid_parent_concerns }.to raise_error(ActiveFedora::ReadOnlyRecord)
       end
 
-      it "has no valid child concerns" do
-        expect(subject.valid_child_concerns).to match_array([])
+      it "has no valid child concerns @deprecated ReadOnlyRecord" do
+        # expect(subject.valid_child_concerns).to match_array([])
+        expect{ subject.valid_child_concerns }.to raise_error(ActiveFedora::ReadOnlyRecord)
       end
 
     end
@@ -92,16 +141,18 @@ RSpec.describe Taxonomy do
     let(:media3)        { Media.create(title: ['media3']) }
 
     before do
-      imaging_event.ordered_members << media1
-      media1.ordered_members << pe1
-      pe1.ordered_members << media2
-      media2.ordered_members << pe3
-      pe3.ordered_members << media3
-      [imaging_event, pe1, media1, pe2, media2, pe3, media3].each(&:save)
+      # Not needed when creating taxonomy returns ReadOnlyRecord error
+      # imaging_event.ordered_members << media1
+      # media1.ordered_members << pe1
+      # pe1.ordered_members << media2
+      # media2.ordered_members << pe3
+      # pe3.ordered_members << media3
+      # [imaging_event, pe1, media1, pe2, media2, pe3, media3].each(&:save)
     end
 
-    it 'returns all descendant media' do
-      expect(taxonomy.media).to match_array([media1, media2, media3])
+    it 'returns all descendant media @deprecated ReadOnlyRecord' do
+      # expect(taxonomy.media).to match_array([media1, media2, media3])
+      expect{ taxonomy.media }.to raise_error(ActiveFedora::ReadOnlyRecord)
     end
   end
 end

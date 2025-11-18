f3be441633b4a2d930a552bce46aa4583dd8c06b
Simon Choy <simon.choy@duke.edu>
2025-11-18 10:31:26 -0500

    fixed test in destroy_proxy_deposit_requests_listener_spec.rb

--- spec/services/morphosource/listeners/destroy_proxy_deposit_requests_listener_spec.rb
+++ spec/services/morphosource/listeners/destroy_proxy_deposit_requests_listener_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Morphosource::Listeners::DestroyProxyDepositRequestsListener do
   describe '#on_object_deleted' do
     context 'when the deleted object is a Media' do
       let!(:media) { FactoryBot.create(:media) }
+      let(:event)  { Dry::Events::Event.new(:on_object_deleted, object: media) }
 
       before do
         # create some proxy deposit requests tied to this media
@@ -21,7 +22,7 @@ RSpec.describe Morphosource::Listeners::DestroyProxyDepositRequestsListener do
       it 'destroys all proxy deposit requests for that media' do
         expect(ProxyDepositRequest.where(work_id: media.id).count).to eq(2)
 
-        listener.on_object_deleted(object: media)
+        listener.on_object_deleted(event)
 
         expect(ProxyDepositRequest.where(work_id: media.id).count).to eq(0)
       end
@@ -29,13 +30,14 @@ RSpec.describe Morphosource::Listeners::DestroyProxyDepositRequestsListener do
       it 'is tolerant when there are no requests' do
         ProxyDepositRequest.where(work_id: media.id).destroy_all
 
-        expect { listener.on_object_deleted(object: media) }.not_to raise_error
+        expect { listener.on_object_deleted(event) }.not_to raise_error
       end
     end
 
     context 'when the deleted object is not a Media' do
       let!(:media) { FactoryBot.create(:media) }
       let!(:specimen) { FactoryBot.create(:biological_specimen) }
+      let(:event) { Dry::Events::Event.new(:on_object_deleted, object: specimen) }
 
       before do
         ProxyDepositRequest.create!(work_id: media.id, sending_user: depositor, receiving_user: receiver, status: 'pending')
@@ -44,7 +46,7 @@ RSpec.describe Morphosource::Listeners::DestroyProxyDepositRequestsListener do
       it 'does not destroy requests for other works' do
         expect(ProxyDepositRequest.count).to eq(1)
 
-        listener.on_object_deleted(object: specimen)
+        listener.on_object_deleted(event)
 
         expect(ProxyDepositRequest.count).to eq(1)
       end

d620388c878df308b0c4ac7513e65103cc83973f
Simon Choy <simon.choy@duke.edu>
2025-12-05 12:22:42 -0500

    added custom fields in device resource indexer

--- app/indexers/device_resource_indexer.rb
+++ app/indexers/device_resource_indexer.rb
@@ -6,11 +6,20 @@ class DeviceResourceIndexer < Hyrax::Indexers::PcdmObjectIndexer(DeviceResource)
   include Hyrax::Indexer(:basic_metadata)
   include Hyrax::Indexer(:device_resource)
 
-  # Uncomment this block if you want to add custom indexing behavior:
-  #  def to_solr
-  #    super.tap do |index_document|
-  #      index_document[:my_field_tesim]   = resource.my_field.map(&:to_s)
-  #      index_document[:other_field_ssim] = resource.other_field
-  #    end
-  #  end
+  def to_solr
+    super.tap do |solr_doc|
+      solr_doc['creator_ssim'] = Array(resource.creator)
+      solr_doc['ark_tesim'] = Array(resource.ark)
+      solr_doc['ark_ssim'] = Array(resource.ark)
+
+      if (device_organization = resource.organization).present?
+        solr_doc['device_organization_id_tesim'] = device_organization.id
+        solr_doc['device_organization_id_ssim'] = device_organization.id
+        solr_doc['device_organization_title_tesim'] = device_organization.title
+        solr_doc['device_organization_title_ssi'] = device_organization.title
+        solr_doc['device_organization_institution_name_tesim'] = device_organization.institution_name
+        solr_doc['device_organization_institution_name_ssim'] = device_organization.institution_name
+      end
+    end
+  end
 end

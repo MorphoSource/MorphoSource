d7e9803b7e38dd2a903db4cdb4fec12ef964959b
Simon Choy <simon.choy@duke.edu>
2025-12-05 14:16:22 -0500

    fixed tests

--- spec/services/morphosource/listeners/destroy_proxy_deposit_requests_listener_spec.rb
+++ spec/services/morphosource/listeners/destroy_proxy_deposit_requests_listener_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe Morphosource::Listeners::DestroyProxyDepositRequestsListener do
   describe '#on_object_deleted' do
     context 'when the deleted object is a Media' do
       let!(:media) { FactoryBot.create(:media) }
-      let(:event)  { Dry::Events::Event.new(:on_object_deleted, object: media) }
 
       before do
         # create some proxy deposit requests tied to this media
@@ -22,6 +21,9 @@ RSpec.describe Morphosource::Listeners::DestroyProxyDepositRequestsListener do
       it 'destroys all proxy deposit requests for that media' do
         expect(ProxyDepositRequest.where(work_id: media.id).count).to eq(2)
 
+        event = double('event')
+        allow(event).to receive(:payload).and_return({ object: media })
+        allow(event).to receive(:[]).with(:object).and_return(media)
         listener.on_object_deleted(event)
 
         expect(ProxyDepositRequest.where(work_id: media.id).count).to eq(0)
@@ -30,6 +32,9 @@ RSpec.describe Morphosource::Listeners::DestroyProxyDepositRequestsListener do
       it 'is tolerant when there are no requests' do
         ProxyDepositRequest.where(work_id: media.id).destroy_all
 
+        event = double('event')
+        allow(event).to receive(:payload).and_return({ object: media })
+        allow(event).to receive(:[]).with(:object).and_return(media)
         expect { listener.on_object_deleted(event) }.not_to raise_error
       end
     end
@@ -37,7 +42,6 @@ RSpec.describe Morphosource::Listeners::DestroyProxyDepositRequestsListener do
     context 'when the deleted object is not a Media' do
       let!(:media) { FactoryBot.create(:media) }
       let!(:specimen) { FactoryBot.create(:biological_specimen) }
-      let(:event) { Dry::Events::Event.new(:on_object_deleted, object: specimen) }
 
       before do
         ProxyDepositRequest.create!(work_id: media.id, sending_user: depositor, receiving_user: receiver, status: 'pending')
@@ -46,6 +50,9 @@ RSpec.describe Morphosource::Listeners::DestroyProxyDepositRequestsListener do
       it 'does not destroy requests for other works' do
         expect(ProxyDepositRequest.count).to eq(1)
 
+        event = double('event')
+        allow(event).to receive(:payload).and_return({ object: specimen })
+        allow(event).to receive(:[]).with(:object).and_return(specimen)
         listener.on_object_deleted(event)
 
         expect(ProxyDepositRequest.count).to eq(1)

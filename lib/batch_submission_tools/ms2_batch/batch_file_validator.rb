53035e0d63b50ad5cf41bf98373710c8e291f1f8
Simon Choy <simon.choy@duke.edu>
2026-01-22 08:32:01 -0500

    set self.valkyrie_klass to DeviceResource

--- lib/batch_submission_tools/ms2_batch/batch_file_validator.rb
+++ lib/batch_submission_tools/ms2_batch/batch_file_validator.rb
@@ -850,6 +850,7 @@ module BatchSubmissionTools
         else
           device_id = pad(cell_value(row_num, field_column("experimental.device_id")))
         end
+byebug
 
         return nil if !device_id.present?
 

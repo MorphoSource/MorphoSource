69454043282226c98556367a9485b1f2f580f96c
Julie Winchester <julie.winchester@duke.edu>
2026-01-23 10:26:57 -0600

    Add test for ValkyrieAssociation

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -1,7 +1,6 @@
 # @deprecated Use TaxonomyResource instead.
 class Taxonomy < Morphosource::Works::Base
   include ::Hyrax::WorkBehavior
-  include ::Morphosource::Works::ValkyrieAssociation # must include after Hyrax::WorkBehavior
   validates_with Morphosource::ParentChildValidator
 
   self.work_requires_files = false

77a3f12e10b951d808dfe22127c9bd2a68b9e660
Simon Choy <simon.choy@duke.edu>
2026-02-16 09:49:51 -0500

    Fix reload-safe MorphoSource listener registration

--- config/initializers/listeners.rb
+++ config/initializers/listeners.rb
@@ -1,5 +1,22 @@
-Hyrax.publisher.subscribe(Morphosource::Listeners::DeleteDeviceReservedArkListener.new)
-Hyrax.publisher.subscribe(Morphosource::Listeners::DestroyProxyDepositRequestsListener.new)
-Hyrax.publisher.subscribe(Morphosource::Listeners::IndexRelatedWorksListener.new)
-Hyrax.publisher.subscribe(Morphosource::Listeners::MintDeviceArkListener.new)
-Hyrax.publisher.subscribe(Morphosource::Listeners::UpdateDeviceArkStatusListener.new)
+# frozen_string_literal: true
+
+Rails.application.config.to_prepare do
+  publisher = Hyrax.publisher
+  stored_listeners = Rails.application.config.x.morphosource_event_listeners ||= []
+
+  # Clear previously registered listener instances before reloading classes.
+  stored_listeners.each { |listener| publisher.unsubscribe(listener) }
+
+  listener_classes = [
+    Morphosource::Listeners::DeleteDeviceReservedArkListener,
+    Morphosource::Listeners::DestroyProxyDepositRequestsListener,
+    Morphosource::Listeners::IndexRelatedWorksListener,
+    Morphosource::Listeners::MintDeviceArkListener,
+    Morphosource::Listeners::UpdateDeviceArkStatusListener
+  ]
+
+  Rails.application.config.x.morphosource_event_listeners =
+    listener_classes.map do |listener_class|
+      listener_class.new.tap { |listener| publisher.subscribe(listener) }
+    end
+end

3df195d634f0184e123a735a3fb075fa38fee89f
Simon Choy <simon.choy@duke.edu>
2026-03-12 10:51:06 -0400

    added comments, change DeviceResource to Valkyrie::Resource

--- app/jobs/update_work_ark_status_job.rb
+++ app/jobs/update_work_ark_status_job.rb
@@ -4,11 +4,11 @@ class UpdateWorkArkStatusJob < Hyrax::ApplicationJob
   queue_as Hyrax.config.update_slow_queue_name
 
   # Update ARK status for an existing work.
-  #
+  # This only works for Valkyrie resources and isn't called by AF works.
   # @param work_id [String]
   def perform(work_id)
     resource = find_resource(work_id)
-    return unless resource.is_a?(DeviceResource)
+    return unless resource.is_a?(Valkyrie::Resource)
     return if resource.ark.blank?
 
     resource.update_ark_status

65b62017da54e4ac3704bfb9b314ac28c9a92938
Simon Choy <simon.choy@duke.edu>
2026-03-18 12:42:43 -0400

    removed test

--- spec/forms/device_resource_form_spec.rb
+++ spec/forms/device_resource_form_spec.rb
@@ -12,9 +12,4 @@ RSpec.describe DeviceResourceForm do
     )
   end
 
-  it "validates title presence with a custom message" do
-    validators = subject.validators_on(:title).grep(ActiveModel::Validations::PresenceValidator)
-    messages = validators.map { |validator| validator.options[:message] }
-    expect(messages).to include('Your device must have a model name.')
-  end
 end

f9518e54652492d3dbff9840a5a98bd512ae547d
Jocelyn Triplett <JocelynTriplett@users.noreply.github.com>
2026-04-10 18:31:31 -0400

    Skip tests if API is down

--- spec/indexers/cultural_heritage_object_indexer_spec.rb
+++ spec/indexers/cultural_heritage_object_indexer_spec.rb
@@ -1,37 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe CulturalHeritageObjectIndexer do
-  subject(:solr_document) { CulturalHeritageObjectIndexer.new(object).generate_solr_document }
-  let(:object)            { CulturalHeritageObject.create(title: ['Cultural Heritage Object'], vouchered: ['Yes']) }
+  context 'AAT API' do
+    api_test_url = 'http://vocab.getty.edu/aat/300222484.json'
+    skip_message = "Unable to access #{api_test_url}"
 
-  describe 'custom fields' do
-    aat_type_ids = ['http://vocab.getty.edu/aat/300222484', # finger puppets
-                    'http://vocab.getty.edu/aat/300033340', # Dutch-door bolts
-                    'http://vocab.getty.edu/aat/300424453', # dental handles
-                    'http://vocab.getty.edu/aat/300420882'] # batter bowls
+    if ApiHelpers.external_api_is_up?(api_test_url)
 
-    aat_material_ids = ['http://vocab.getty.edu/aat/300011666', # Alberene stone
-                        'http://vocab.getty.edu/aat/300448599', # cement-stabilized clay
-                        'http://vocab.getty.edu/aat/300014346'] # flooring sand
+      context 'is available' do
+        subject(:solr_document) { CulturalHeritageObjectIndexer.new(object).generate_solr_document }
 
-    let(:field_values) { {
-      aat_type: aat_type_ids.map { |id| Morphosource::ControlledVocabularies::Getty::Aat.new(::RDF::URI(id)) },
-      aat_material: aat_material_ids.map { |id| Morphosource::ControlledVocabularies::Getty::Aat.new(::RDF::URI(id)) },
-      cho_type: ["punch bowl", "dog toy", "airplane"],
-      material: ["ceramic", "plastic", "metal"],
-      vouchered: ["Yes"]
-    } }
+        let(:object)            { CulturalHeritageObject.create(title: ['Cultural Heritage Object'], vouchered: ['Yes']) }
 
-    before do
-      field_values.each do |k,v|
-        allow(object).to receive(k).and_return(v)
+        describe 'custom fields' do
+          aat_type_ids = ['http://vocab.getty.edu/aat/300222484', # finger puppets
+                          'http://vocab.getty.edu/aat/300033340', # Dutch-door bolts
+                          'http://vocab.getty.edu/aat/300424453', # dental handles
+                          'http://vocab.getty.edu/aat/300420882'] # batter bowls
+
+          aat_material_ids = ['http://vocab.getty.edu/aat/300011666', # Alberene stone
+                              'http://vocab.getty.edu/aat/300448599', # cement-stabilized clay
+                              'http://vocab.getty.edu/aat/300014346'] # flooring sand
+
+          let(:field_values) { {
+            aat_type: aat_type_ids.map { |id| Morphosource::ControlledVocabularies::Getty::Aat.new(::RDF::URI(id)) },
+            aat_material: aat_material_ids.map { |id| Morphosource::ControlledVocabularies::Getty::Aat.new(::RDF::URI(id)) },
+            cho_type: ["punch bowl", "dog toy", "airplane"],
+            material: ["ceramic", "plastic", "metal"],
+            vouchered: ["Yes"]
+          } }
+
+          before do
+            field_values.each do |k,v|
+              allow(object).to receive(k).and_return(v)
+            end
+          end
+
+          it 'indexes custom fields' do
+            expect(subject['material_si']).to eq("alberene stone")
+            expect(subject['cho_type_si']).to eq("airplane")
+            expect(subject['vouchered_si']).to eq("yes")
+          end
+        end
       end
-    end
-
-    it 'indexes custom fields' do
-      expect(subject['material_si']).to eq("alberene stone")
-      expect(subject['cho_type_si']).to eq("airplane")
-      expect(subject['vouchered_si']).to eq("yes")
+    else
+      context 'is unavailable', skip: "#{skip_message}" do
+        it {}
+      end
     end
   end
 end
--- spec/lib/morphosource/controlled_vocabularies/getty_spec.rb
+++ spec/lib/morphosource/controlled_vocabularies/getty_spec.rb
@@ -1,25 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Morphosource::ControlledVocabularies::Getty do
-  let(:aat_id)   { '300172528' }
-  let(:aat_term) { Morphosource::ControlledVocabularies::Getty::Aat.new(::RDF::URI("http://vocab.getty.edu/aat/#{aat_id}")) }
-  let(:non_id)  { '000000' }
-  let(:non_term) { Morphosource::ControlledVocabularies::Getty::Aat.new(::RDF::URI("http://vocab.getty.edu/aat/#{non_id}")) }
+  let(:aat_id)    { '300172528' }
+  let(:aat_term)  { Morphosource::ControlledVocabularies::Getty::Aat.new(::RDF::URI("http://vocab.getty.edu/aat/#{aat_id}")) }
+  let(:non_id)    { '000000' }
+  let(:non_term)  { Morphosource::ControlledVocabularies::Getty::Aat.new(::RDF::URI("http://vocab.getty.edu/aat/#{non_id}")) }
 
-  before(:each) do
-    Rails.cache.clear
-  end
+  describe 'AAT API' do
+    api_test_url = 'http://vocab.getty.edu/aat/300172528.json'
+    skip_message = "Unable to access #{api_test_url}"
 
-  describe 'rdf_label' do
-    context 'fetching label is successful' do
-      it 'returns the label and caches it' do
-        expect(aat_term.rdf_label).to eq(["dog's-paw feet"])
-        expect(Rails.cache.fetch(aat_term.send(:cache_key))).to eq(["dog's-paw feet"])
+    if ApiHelpers.external_api_is_up?(api_test_url)
+      context 'is available' do
+        before(:each) do
+          Rails.cache.clear
+        end
+
+        describe 'rdf_label' do
+          context 'fetching label is successful' do
+            it 'returns the label and caches it' do
+              expect(aat_term.rdf_label).to eq(["dog's-paw feet"])
+              expect(Rails.cache.fetch(aat_term.send(:cache_key))).to eq(["dog's-paw feet"])
+            end
+          end
+          context 'fetching label is not successful' do
+            it 'returns an error message' do
+              expect(non_term.rdf_label).to eq(["Error fetching #{non_id}"])
+            end
+          end
+        end
       end
-    end
-    context 'fetching label is not successful' do
-      it 'returns an error message' do
-        expect(non_term.rdf_label).to eq(["Error fetching #{non_id}"])
+    else
+      context 'is unavailable', skip: "#{skip_message}" do
+        it {}
       end
     end
   end

027d35dbf2884fe17780a6cee0a2d43f2ffea0ad
Julie Winchester <julie.winchester@duke.edu>
2026-05-18 16:17:55 -0500

    Improve listener initializer

--- config/initializers/listeners.rb
+++ config/initializers/listeners.rb
@@ -1,28 +1,37 @@
 # frozen_string_literal: true
 
-# Wrapped in to_prepare so listener instances are re-registered after each
-# Rails code reload in development, preventing stale instances from raising:
+# Hyrax 5.0.5 subscribes its default listeners in a plain initializer, outside
+# of any to_prepare block. The latest Hyrax fixed this by moving the
+# subscription into to_prepare. We replicate that fix here: undo Hyrax's
+# boot-time subscription so that to_prepare below has sole ownership of all
+# listener subscriptions from this point forward.
+#
+# Without this, listener instances are not refreshed after a Rails code reload,
+# causing:
 #   ArgumentError: A copy of <Listener> has been removed from the module tree
 #                  but is still active!
-# Instances are stored in config.x so they can be unsubscribed on the next
-# prepare cycle before fresh ones are created.
+Hyrax.publisher.default_listeners.each { |l| Hyrax.publisher.unsubscribe(l) }
+Hyrax.publisher.instance_variable_set(:@default_listeners, nil)
+
+# On every prepare cycle (once at boot, then after each code reload), unsubscribe
+# the previous instances, reset @default_listeners so fresh class objects are
+# used, and re-subscribe everything.
 Rails.application.config.to_prepare do
   publisher = Hyrax.publisher
-  stored_listeners = Rails.application.config.x.morphosource_event_listeners ||= []
 
-  # Clear previously registered listener instances before reloading classes.
-  stored_listeners.each { |listener| publisher.unsubscribe(listener) }
+  (Rails.application.config.x.all_event_listeners ||= []).each { |l| publisher.unsubscribe(l) }
 
-  listener_classes = [
+  publisher.instance_variable_set(:@default_listeners, nil)
+  hyrax_listeners = publisher.default_listeners
+  hyrax_listeners.each { |l| publisher.subscribe(l) }
+
+  morphosource_listeners = [
     Morphosource::Listeners::DeleteReservedArkListener,
     Morphosource::Listeners::DestroyProxyDepositRequestsListener,
     Morphosource::Listeners::IndexRelatedWorksListener,
     Morphosource::Listeners::MintArkListener,
     Morphosource::Listeners::UpdateArkStatusListener
-  ]
+  ].map { |klass| klass.new.tap { |l| publisher.subscribe(l) } }
 
-  Rails.application.config.x.morphosource_event_listeners =
-    listener_classes.map do |listener_class|
-      listener_class.new.tap { |listener| publisher.subscribe(listener) }
-    end
+  Rails.application.config.x.all_event_listeners = hyrax_listeners.to_a + morphosource_listeners
 end

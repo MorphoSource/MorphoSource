878f97eafd61f35feeb11d973a651f8463bbbce8
Julie Winchester <julie.winchester@duke.edu>
2026-05-19 11:30:58 -0500

    Fix Valkyrie FileSet member presenter for media presenter

--- app/presenters/hyrax/media_member_presenter_factory.rb
+++ app/presenters/hyrax/media_member_presenter_factory.rb
@@ -1,5 +1,15 @@
 module Hyrax
-	class MediaMemberPresenterFactory < Hyrax::MemberPresenterFactory
-		self.file_presenter_class = MediaFileSetPresenter
-	end
+  class MediaMemberPresenterFactory < Hyrax::MemberPresenterFactory
+    self.file_presenter_class = MediaFileSetPresenter
+
+    def ordered_ids
+      @ordered_ids ||= Array(@work.fetch('member_ids_ssim', []))
+    end
+
+    private
+
+    def file_set_ids
+      @file_set_ids ||= Array(@work.fetch('file_set_ids_ssim', []))
+    end
+  end
 end

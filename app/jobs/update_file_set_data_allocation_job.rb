b9994ab6f390e5da9c50b4da168ff14802b597e1
Simon Choy <simon.choy@duke.edu>
2026-06-29 12:46:14 -0400

    Use media.active_fund_code_association to simplify UpdateFileSetDataAllocationJob

--- app/jobs/update_file_set_data_allocation_job.rb
+++ app/jobs/update_file_set_data_allocation_job.rb
@@ -5,11 +5,7 @@ class UpdateFileSetDataAllocationJob < Hyrax::ApplicationJob
     media = file_set.parent
     return unless media.is_a?(Media)
 
-    fcma = FundCodeMediaAssociation.where(media: media.id, active: true).first
-    return unless fcma
-
-    return unless fcma.fund_code
-    data_allocation = fcma.fund_code.data_allocation
+    data_allocation = media.active_fund_code_association&.fund_code&.data_allocation
     return unless data_allocation
 
     UpdateDataAllocationStorageJob.perform_later(data_allocation)

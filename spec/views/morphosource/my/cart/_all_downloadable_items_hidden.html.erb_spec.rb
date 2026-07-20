e1bc33083d31e18323e0ebe4ae0207b87ba83d8f
Simon Choy <simon.choy@duke.edu>
2026-07-20 10:47:26 -0400

    Batch Solr lookups for off-page cart items instead of one query per item

--- spec/views/morphosource/my/cart/_all_downloadable_items_hidden.html.erb_spec.rb
+++ spec/views/morphosource/my/cart/_all_downloadable_items_hidden.html.erb_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe 'morphosource/my/cart/_all_downloadable_items_hidden.html.erb', t
   let(:on_page_item)  { CartItem.new(id: 1, work_id: 'onpage1') }
   let(:off_page_item) { CartItem.new(id: 2, work_id: 'offpage1') }
 
-  let(:on_page_doc) do
-    SolrDocument.new(id: 'onpage1', title_tesim: ['On Page'], has_model_ssim: ['Media'])
-  end
   let(:off_page_doc) do
     SolrDocument.new(id: 'offpage1', title_tesim: ['Off Page'], has_model_ssim: ['Media'])
   end
@@ -14,8 +11,7 @@ RSpec.describe 'morphosource/my/cart/_all_downloadable_items_hidden.html.erb', t
   let(:page) { Capybara::Node::Simple.new(rendered) }
 
   before do
-    allow(SolrDocument).to receive(:find).with('onpage1').and_return(on_page_doc)
-    allow(SolrDocument).to receive(:find).with('offpage1').and_return(off_page_doc)
+    allow(view).to receive(:solr_docs_find).with(['offpage1']).and_return('offpage1' => off_page_doc)
     allow(view).to receive(:current_ability).and_return(double)
 
     assign(:unrestricted_items, [on_page_item, off_page_item])
@@ -34,5 +30,9 @@ RSpec.describe 'morphosource/my/cart/_all_downloadable_items_hidden.html.erb', t
 
   it 'renders inside a hidden wrapper so it never displays visually' do
     expect(page).to have_selector('table#all-downloadable-items-hidden.hide')
+  end
+
+  it 'looks up off-page documents in a single batched call, not one per item' do
+    expect(view).to have_received(:solr_docs_find).with(['offpage1']).once
   end
 end

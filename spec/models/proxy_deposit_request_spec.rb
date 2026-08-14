d6e3a2b3f7d288100a38fc5e2a185f7192421bce
Julie Winchester <julie.winchester@duke.edu>
2026-08-14 11:49:38 -0500

    Revert tests

--- spec/models/proxy_deposit_request_spec.rb
+++ spec/models/proxy_deposit_request_spec.rb
@@ -8,8 +8,6 @@ RSpec.describe ProxyDepositRequest do
 
   before do
     allow(Hyrax.config).to receive(:host_name) { "test.host" }
-    # Fall back to the depositor for the seeded manager
-    allow(Morphosource).to receive(:default_organization_manager).and_return(nil)
   end
 
   describe "instance" do

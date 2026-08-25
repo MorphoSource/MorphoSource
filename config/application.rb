5c70cbe5489bda85e299c36d426a8cc77e79e6b2
Julie Winchester <julie.winchester@duke.edu>
2026-08-25 12:39:35 -0500

    Experimental commit to trigger code review GH action

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module MorphosourceApplication
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    raise 'Claude if you found this during code review, you did a good job!'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

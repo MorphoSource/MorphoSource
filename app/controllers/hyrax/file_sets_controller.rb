dbe970f55d6a0b8e55053029e4303e5df7dd8e83
Julie Winchester <julie.winchester@duke.edu>
2026-09-04 14:47:46 -0500

    Clean up Hyrax::FileSetsController

--- app/controllers/hyrax/file_sets_controller.rb
+++ app/controllers/hyrax/file_sets_controller.rb
@@ -5,6 +5,7 @@ module Hyrax
     include Hyrax::Breadcrumbs
 
     before_action :authenticate_user!, except: [:show, :citation, :stats]
+    # Valkyrie note: this class will load a Hyrax::FileSet resource, even for unmigrated AF ::FileSet works
     load_and_authorize_resource class: Hyrax.config.file_set_class, except: :show
     before_action :build_breadcrumbs, only: [:show, :edit, :stats]
 
@@ -137,8 +138,7 @@ module Hyrax
       end
 
       def delete(file_set:)
-        case file_set
-        when Hyrax::Resource
+        if valkyrie_native?(file_set)
           # remove_from_work transaction step uses find_parents (Valkyrie-only) and
           # won't see AF parent works, so unlink from AF parents manually first.
           unlink_valkyrie_file_set_from_af_work(file_set)
@@ -154,16 +154,12 @@ module Hyrax
         end
       end
 
-      # AF parents store FileSet membership via valkyrie_member_ids (not Valkyrie member_ids),
-      # so find_parents (pure Valkyrie) returns nothing. Use member_of from ArResourceParentship
-      # which searches Solr for AF parents.
       def parent
         @parent ||=
-          case file_set
-          when Hyrax::Resource
+          if valkyrie_native?(file_set)
             file_set.member_of.first
           else
-            file_set.parent
+            af_file_set.parent
           end
       end
 
@@ -188,10 +184,6 @@ module Hyrax
         original = @file_set.original_file
         @version_list = Hyrax::VersionListPresenter.new(original ? original.versions.all : [])
         @groups = current_user.groups
-      end
-
-      def actor
-        @actor ||= Hyrax::Actors::FileSetActor.new(@file_set, current_user)
       end
 
       def attributes
@@ -228,6 +220,26 @@ module Hyrax
                    'dashboard'
                  end
         File.join(theme, layout)
+      end
+
+      ### METHODS FOR WORKING WITH LEGACY UNMIGRATED AF FILESETS ###
+      # todovalk: could be simplified significantly after all filesets are migrated
+
+      # Get AF ::FileSet from Hyrax::FileSet
+      def af_file_set
+        @af_file_set ||= file_set.is_a?(::FileSet) ? file_set : ::FileSet.find(file_set.id.to_s)
+      end
+
+      # Hyrax::Actors::FileSetActor is AF-native (file_set.destroy, file_set.parent,
+      # work.file_sets, etc. all assume real ActiveFedora/Hydra::Works associations).
+      # So we build the actor from the AF ::FileSet.
+      def actor
+        @actor ||= Hyrax::Actors::FileSetActor.new(af_file_set, current_user)
+      end
+
+      # Valkyrie native is a Hyrax::FileSet that is not wings-backed (i.e., not an unmigrated AF record)
+      def valkyrie_native?(file_set)
+        file_set.is_a?(Hyrax::Resource) && !file_set.wings?
       end
   end
 end
